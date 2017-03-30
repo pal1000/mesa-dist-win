d55a0657b8e6f537c21e322a76c13f657d0cdcc2
prodea2 <prodea2>
2017-03-30 12:23:04 +0300

    Buildscript: Initial Ninja build system support Needed to build LLVM with backward compatibility toolset. Began work on toolset matching validation before building Mesa.

--- buildscript/build.cmd
+++ buildscript/build.cmd
@@ -10,15 +10,16 @@
 @if %abi%==x64 set minabi=64
 @set vsenv="%ProgramFiles%
 @if NOT "%ProgramW6432%"=="" set vsenv=%vsenv% (x86)
-@set vsenv=%vsenv%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars%minabi%.bat"
-@set toolchain=Visual Studio 15 2017
-@if NOT EXIST %vsenv% (
-@set vsenv="%VS140COMNTOOLS%..\..\VC\bin\vcvars%minabi%.bat"
-@set toolchain=Visual Studio 14
-)
+@set vsenv15=%vsenv%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars%minabi%.bat"
+@set vsenv14="%VS140COMNTOOLS%..\..\VC\bin\vcvars%minabi%.bat"
 @set gcc=%mesa%mingw-w64-%abi%\mingw%minabi%\bin
 @set vsenvloaded=0
 @set dxtnbuilt=0
+@set toolset=14
+@set /p toolset=<toolset.ini
+@echo.
+@set vsenv=%vsenv14%
+@if %toolset%==15 @set vsenv=%vsenv15%
 
 :build_llvm
 @set /p buildllvm=Begin LLVM build. Only needs to run once for each ABI and version. Proceed (y/n):
@@ -31,19 +32,35 @@
 @echo.
 @md cmake-%abi%
 @cd cmake-%abi%
+@set toolchain=Visual Studio 14
+@set vs2017toolset=n
+@if EXIST %vsenv15% @set /p vs2017toolset=Build with MSVC 2017 toolset, not recommended (y/n):
+@echo.
+@if /I %vs2017toolset%==y (
+@set toolchain=Visual Studio 15 2017
+@set toolset=15
+)
+@if %abi%==x64 @set toolchain=%toolchain% Win64
+@set ninja=n
+@if EXIST "%mesa%ninja" set ninja=1
+@if EXIST "%mesa%ninja" set PATH=%mesa%ninja\;%PATH%
+@if %ninja%==1 set /p ninja=Use Ninja build system, required if building LLVM with MSVC 2015 toolset on Visual Studio 2017 (y/n):
+@echo.
+@if /I NOT %ninja%==n set toolchain=Ninja
+@set vsenv=%vsenv14%
+@if %toolset%==15 @set vsenv=%vsenv15%
 @call %vsenv%
 @set vsenvloaded=1
-@if %abi%==x64 @set toolchain=%toolchain% Win64
 @set PATH=%mesa%cmake\%abi%\bin\;%PATH%
 @echo.
-@cmake -G "%toolchain%" -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_ENABLE_RTTI=1 -DLLVM_USE_CRT_RELEASE=MT -DLLVM_ENABLE_TERMINFO=OFF -DCMAKE_INSTALL_PREFIX=%mesa:\=/%llvm/%abi% ..
+@cmake -G "%toolchain%" -DLLVM_TARGETS_TO_BUILD=X86 -DCMAKE_BUILD_TYPE=Release -DLLVM_USE_CRT_RELEASE=MT -DLLVM_ENABLE_RTTI=1 -DLLVM_ENABLE_TERMINFO=OFF -DCMAKE_INSTALL_PREFIX=%mesa:\=/%llvm/%abi% ..
 @echo.
 @pause
 @echo.
-@msbuild /p:Configuration=Release INSTALL.vcxproj
+@if NOT %toolchain%==Ninja msbuild /p:Configuration=Release INSTALL.vcxproj
+@if %toolchain%==Ninja ninja install
 @echo.
-@pause
-@echo.
+@echo %toolset% > "%mesa%toolset.ini"
 
 :build_dxtn
 @if NOT EXIST "%gcc%" GOTO build_mesa
