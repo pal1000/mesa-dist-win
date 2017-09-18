06d1bdd96fb65bb894c7c6089f82a772246eb17f
Albert <liviuprodea@yahoo.com>
2017-09-18 20:55:13 +0300

    Deployment utility: drop 32-bit OpenSWR It is unsupported per [https://bugs.freedesktop.org/show_bug.cgi?id=102564#c5](bug 102564 comment 5). Since Mesa 17.2.0 32-bit OpenSWR crashes with everything.

--- bin/quickdeploy.cmd
+++ bin/quickdeploy.cmd
@@ -63,8 +63,8 @@ if '%errorlevel%' NEQ '0' (
 :finish
 @set mesaloc=%~dp0
 @mklink "%dir%\opengl32.dll" "%mesaloc%%mesadll%\opengl32.dll"
-@mklink "%dir%\swrAVX.dll" "%mesaloc%%mesadll%\swrAVX.dll"
-@mklink "%dir%\swrAVX2.dll" "%mesaloc%%mesadll%\swrAVX2.dll"
+@if %mesadll%==x64 mklink "%dir%\swrAVX.dll" "%mesaloc%x64\swrAVX.dll"
+@if %mesadll%==x64 mklink "%dir%\swrAVX2.dll" "%mesaloc%x64\swrAVX2.dll"
 @echo.
 @set s3tc=n
 @set /p s3tc=Do you need S3TC (y/n):
