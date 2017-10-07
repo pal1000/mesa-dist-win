a72673fcd4740254d8ee019d589c655c7f6f567b
Albert <liviuprodea@yahoo.com>
2017-10-07 11:58:58 +0300

    Build script: Support building S3TC with MSYS2 Mingw-w64 GCC in addition to windows native Mingw-w64 GCC. MSYS2 Mingw-w64 GCC appears it has fixed its problem with 32-bit binaries with the upgrade to GCC 7.2.0. - Drop Mesa MSYS2 and Cygwin experiments. Terribly unsupported platforms. Totally not worth it. S3TC builds with Cygwin are also very unstable. - Fixed Mesa build fallback to softpipe. Regression since d0ac2b6.

--- buildscript/build.cmd
+++ buildscript/build.cmd
@@ -11,14 +11,20 @@
 @set /p pythonloc=<pyupd.ini
 @set makoloc="%pythonloc:python.exe=%Lib\site-packages\mako"
 @set sconsloc="%pythonloc:python.exe=%Scripts\scons.py"
-@if NOT EXIST %makoloc% set pyupd=y
+@if NOT EXIST %makoloc% (
+@python -m pip install -U pip
+@python -m pip install -U wheel
+@python -m pip install -U scons
+@python -m pip install -U MarkupSafe
+@python -m pip install -U mako
+)
 @if EXIST %makoloc% set /p pyupd=Install/update python modules (y/n):
 @if /I "%pyupd%"=="y" (
 @python -m pip install -U pip
 @python -m pip install -U wheel
-@python -m pip install -U scons
-@python -m pip install -U mako
-@python -m pip install -U MarkupSafe
+@python -m pip freeze > requirements.txt
+@python -m pip install -r requirements.txt --upgrade
+@del requirements.txt
 @echo.
 )
 @set abi=x86
@@ -120,15 +126,16 @@
 @if NOT EXIST mesa echo Warning: Mesa3D source code not found.
 @if NOT EXIST mesa set prepfail=%prepfail%2
 @if %prepfail%==12 echo Fatal: Both Mesa code and Git are missing. At least one is required. Execution halted.
-@if %prepfail%==12 GOTO exit
+@if %prepfail%==12 GOTO build_dxtn
 @if NOT EXIST mesa set /p haltmesabuild=Press Y to abort execution. Press any other key to download Mesa via Git:
-@if /I "%haltmesabuild%"=="y" GOTO exit
+@if /I "%haltmesabuild%"=="y" GOTO build_dxtn
 @if NOT EXIST mesa set /p branch=Enter Mesa source code branch name - defaults to master:
 @if "%branch%"=="" set branch=master
 @if "%branch%"=="none" set branch=master
 @if NOT EXIST mesa echo.
 @if NOT EXIST mesa git clone --depth=1 --branch=%branch% git://anongit.freedesktop.org/mesa/mesa mesa
 @cd mesa
+@set LLVM=%mesa%\llvm\%abi%
 @set /p mesaver=<VERSION
 @set branch=%mesaver:~0,4%
 @if EXIST mesapatched.ini GOTO build_mesa
@@ -138,18 +145,17 @@
 @set mesapatched=1
 @echo %mesapatched% > mesapatched.ini
 @echo.
-
-:build_mesa
-@set /p buildmesa=Begin mesa build. Proceed (y/n):
-@if /i NOT "%buildmesa%"=="y" GOTO build_dxtn
-@echo.
-@set LLVM=%mesa%\llvm\%abi%
 @if NOT EXIST %LLVM% (
 @echo Could not find LLVM, aborting mesa build.
 @echo.
 @pause
 @GOTO build_dxtn
 )
+
+:build_mesa
+@set /p buildmesa=Begin mesa build. Proceed (y/n):
+@if /i NOT "%buildmesa%"=="y" GOTO build_dxtn
+@echo.
 @cd %mesa%\mesa
 @set openswr=n
 @set sconscmd=python %sconsloc% build=release platform=windows machine=%longabi% libgl-gdi
@@ -162,66 +168,53 @@
 @set /p graw=Do you want to build graw library (y/n):
 @echo.
 @if /I "%graw%"=="y" set sconscmd=%sconscmd% graw-gdi
-@GOTO build_with_vs
-
-:build_with_cygwin
-@set mingw=n
-@if %abi%==x86 GOTO build_with_vs
-@set cygwin=%mesa%\cygwin64
-@if EXIST %cygwin% set /p mingw=Do you want to build with Cygwin instead of Visual Studio (y=yes):
-@if /I NOT "%mingw%"=="y" GOTO build_with_vs
-@set ERRORLEVEL=0
-@where /q python2.7.exe
-@IF ERRORLEVEL 1 set PATH=%cygwin%\bin\;%PATH%
-@set ERRORLEVEL=0
-@set sconscmd=scons build=release platform=cygwin machine=%longabi% llvm=no
-@rem copy %gcc%\%altabi%-w64-mingw32-gcc-ar.exe %gcc%\%altabi%-w64-mingw32-ar.exe
-@rem copy %gcc%\%altabi%-w64-mingw32-gcc-ranlib.exe %gcc%\%altabi%-w64-mingw32-ranlib.exe
-@set cleanbuild=n
-@if EXIST build\cygwin-%longabi% set /p cleanbuild=Do you want to clean build (y/n):
-@if EXIST build\cygwin-%longabi% echo.
-@if /I "%cleanbuild%"=="y" RD /S /Q build\cygwin-%longabi%
-@cd %mesa%\mesa
-@python2.7 -m pip install -U pip
-@python2.7 -m pip install -U wheel
-@python2.7 -m pip install -U scons
-@python2.7 -m pip install -U mako
-@python2.7 -m pip install -U MarkupSafe
-@%cygwin%\bin\bash %mesa%\mesa-dist-win\buildscript\cygwin.sh
-@GOTO build_mesa_exec
 
 :build_with_vs
 @where /q python.exe
 @IF ERRORLEVEL 1 set PATH=%mesa%\Python\;%mesa%\Python\Scripts\;%PATH%
 @set ERRORLEVEL=0
 @set PATH=%mesa%\flexbison\;%PATH%
+@cd %mesa%\mesa
+
+:build_mesa_exec
 @set cleanbuild=n
 @if EXIST build\windows-%longabi% set /p cleanbuild=Do you want to clean build (y/n):
 @if EXIST build\windows-%longabi% echo.
 @if /I "%cleanbuild%"=="y" RD /S /Q build\windows-%longabi%
-@cd %mesa%\mesa
+@if NOT EXIST %mesa%\mesa\build md %mesa%\mesa\build
+@if NOT EXIST %mesa%\mesa\build\windows-%abi%\git_sha1.h (
+@md %mesa%\mesa\build\windows-%longabi%
+@echo 0 > %mesa%\mesa\build\windows-%longabi%\git_sha1.h
+)
 @echo.
-
-:build_mesa_exec
-@if /I "%mingw%"=="y" GOTO build_dxtn
-@set dummygitver=#define MESA_GIT_SHA1 "git-"
-@if NOT EXIST %mesa%\mesa\build\windows-%abi%\git_sha1.h echo %dummygitver% > %mesa%\mesa\build\windows-%abi%\git_sha1.h
-@if /I NOT "%mingw%"=="y" %sconscmd%
+@%sconscmd%
 @echo.
 
 :build_dxtn
-@set gcc=%mesa%\mingw-w64\%abi%\mingw%minabi%\bin
-@if NOT EXIST %gcc% GOTO distcreate
+@if NOT EXIST %mesa%\mesa GOTO exit
+@set PATH=%oldpath%
+@set gcchost=native
 @if NOT EXIST %mesa%\dxtn GOTO distcreate
 @if "%mesaver:~-5%"=="devel" GOTO distcreate
+@set gcc=%mesa%\mingw-w64\%abi%\mingw%minabi%\bin
+@if NOT EXIST %gcc% set gcchost=msys2
+@if NOT EXIST %gcc% set gcc=%mesa%\msys64
+@if NOT EXIST %gcc% set gcc=%mesa%\msys32
+@if NOT EXIST %gcc% set gcchost=false
+@set gccpath=
+@if %gcchost%==false GOTO distcreate
+@if %gcchost%==native set gccpath=%gcc%\;
+@if %gcchost%==msys2 set gccpath=%gcc%\mingw%minabi%\bin\;%gcc%\usr\bin\;
+@if %gcchost%==msys2 pacman -Syu
 @set /p builddxtn=Do you want to build S3 texture compression library? (y/n):
+@if /i NOT "%builddxtn%"=="y" set gcchost=false
 @if /i NOT "%builddxtn%"=="y" GOTO distcreate
-@set PATH=%gcc%\;%PATH%
+@set PATH=%gccpath%%PATH%
 @cd %mesa%\dxtn
 @echo.
 @if EXIST %abi% RD /S /Q %abi%
 @MD %abi%
-@set dxtn=gcc -shared -m%minabi% -v *.c *.h -I ..\mesa\include -Wl,--dll,--dynamicbase,--enable-auto-image-base,--nxcompat -o %abi%\dxtn.dll
+@set dxtn=gcc -shared -m%minabi% -v *.c *.h -I ../mesa/include -Wl,--dll,--dynamicbase,--enable-auto-image-base,--nxcompat -o %abi%/dxtn.dll
 @%dxtn%
 @echo.
 
--- relnotes.md
+++ relnotes.md
@@ -1,5 +1,6 @@
+Built S3TC with MSYS2 Mingw-w64 GCC 7.2.0.
 # Build script
 - Drop S3TC build if Mesa master source code is detected. S3TC is now built-in. Texture cache enabling patch is still needed though.
-- Python modules updating: use pip install -U <module-name> explicitly for all modules. It is more comprehensive than pip freeze.
+- Python modules updating: use both pip install -U <module-name> explicitly and pip freeze in a hybrid approach for most optimal behavior.
 - Improved PATH cleanning.
-- Revived some cygwin/Msys2 experiments.
+- Support building S3TC with SYS2 Mingw-W64 GCC. They fixed their problem with 32-bit binaries when they upgraded to GCC 7.2.0.
