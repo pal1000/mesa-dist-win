d2f5895f898bd73033b35e49190af68cb655843f
pal1000 <liviuprodea@yahoo.com>
2018-07-16 18:18:17 +0300

    Meson test debug script: obtain Mesa3D if missing.

--- debug/mesa3dmesontest.cmd
+++ debug/mesa3dmesontest.cmd
@@ -1,8 +1,9 @@
 @set mesa=C:\Software\Development\projects\mesa
 @SET PATH=C:\Software\Development\Git\cmd\;%mesa%\Python\;%mesa%\flexbison\;%mesa%\ninja\;%mesa%\pkgconfig\;%PATH%
 @call "%ProgramFiles% (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-@cd %mesa%\mesa
-@set meson==%mesa%\Py3\python.exe %mesa%\Py3\Scripts\meson.py
+@cd %mesa%
+@IF NOT EXIST mesa git clone --recurse-submodules https://gitlab.freedesktop.org/dbaker/mesa.git mesa
+@set meson=%mesa%\Py3\python.exe %mesa%\Py3\Scripts\meson.py
 @set buildcmd=%meson% .\build\windows-x86_64 -Dosmesa=gallium
-@RD /S /Q %mesa%\mesa\build\windows-x86_64
+@IF EXIST %mesa%\mesa\build\windows-x86_64 RD /S /Q %mesa%\mesa\build\windows-x86_64
 @cmd
