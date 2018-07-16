0157129ed760d26d47cefac5d359084f5dd950f4
pal1000 <liviuprodea@yahoo.com>
2018-07-16 23:41:17 +0300

    Meson build draft: Attempt LLVM use

--- debug/mesa3dmesontest.cmd
+++ debug/mesa3dmesontest.cmd
@@ -1,9 +1,13 @@
 @set mesa=C:\Software\Development\projects\mesa
+@set abi=x64
 @SET PATH=C:\Software\Development\Git\cmd\;%mesa%\Python\;%mesa%\flexbison\;%mesa%\ninja\;%mesa%\pkgconfig\;%PATH%
 @call "%ProgramFiles% (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 @cd %mesa%
-@IF NOT EXIST mesa git clone --recurse-submodules https://gitlab.freedesktop.org/dbaker/mesa.git mesa
+@IF NOT EXIST mesa git clone --recurse-submodules --branch=meson-windows https://gitlab.freedesktop.org/dbaker/mesa.git mesa
 @set meson=%mesa%\Py3\python.exe %mesa%\Py3\Scripts\meson.py
-@set buildcmd=%meson% .\build\windows-x86_64 -Dosmesa=gallium
-@IF EXIST %mesa%\mesa\build\windows-x86_64 RD /S /Q %mesa%\mesa\build\windows-x86_64
+@set buildconf=%meson% .\%abi%
+@set buildcmd=ninja -C .\%abi%
+@IF EXIST %mesa%\mesa\%abi% RD /S /Q %mesa%\mesa\%abi%
+@cd mesa
+@IF EXIST %mesa%\llvm\%abi% SET PATH=%mesa%\llvm\%abi%\;%PATH%
 @cmd
