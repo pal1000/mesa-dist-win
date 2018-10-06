pal1000/mesa-dist-win

commit f86d93b860293a0ea58d9972c343e546f169ffdd

Release 18.2.2 Update distribution maker to collect headers and libraries - fixes #14. Debug: Remove early, obsoleted and forgotten Meson build draft. llvm-config-output debug tool: Make it behave like llvm-wrap generator. Add a patch to get swr to build with LLVM 7.0 Adjust osmesa dual pass build hack to occur with swr as well.

buildscript/modules/mesa3d.cmd
@@ -53,12 +53,18 @@ GOTO skipmesa
 @echo 1 > mesapatched.ini
 @echo.
 
-@rem Apply a patch that disables osmesa gallium and cuts off GLES from osmesa classic when building with Scons
-@rem when GLES is enabled due to build failure - https://bugs.freedesktop.org/show_bug.cgi?id=106843
-@rem We'll do a 2-pass build in this case. Build everything requested without GLES, then build everything again with GLES
-@rem having osmesa gallium disabled via patch.
+@rem Apply a patch that disables osmesa when building GLES or swr drver when using Scons
+@rem GLES linking with osmesa is disabled due to build failure - https://bugs.freedesktop.org/show_bug.cgi?id=106843
+@rem Now do the same for swr as well.
+@rem We'll do a 2-pass build in this case. Build everything requested without GLES and swr, then build everything again
+@rem with GLES and swr.
 @IF %pythonver%==2 git apply -v ..\mesa-dist-win\patches\osmesa.patch
 @IF %pythonver%==2 echo.
+
+@rem Apply a patch that fixes swr build with LLVM 7.0. It incorporates https://patchwork.freedesktop.org/patch/252354/
+@rem and Scons part I did myself.
+@git apply -v ..\mesa-dist-win\patches\swr-llvm7.patch
+@echo.
 
 :configmesabuild
 @rem Configure Mesa build.
@@ -94,14 +100,16 @@ GOTO skipmesa
 @if %pythonver%==2 if /I "%openmp%"=="y" set buildcmd=%buildcmd% openmp=1
 
 @set swrdrv=n
+@set disableosmesa=0
 @if /I NOT "%llvmless%"=="y" if %abi%==x64 if EXIST %LLVM% set /p swrdrv=Do you want to build swr drivers? (y=yes):
 @if /I NOT "%llvmless%"=="y" if %abi%==x64 if EXIST %LLVM% echo.
-@if %pythonver%==2 if /I "%swrdrv%"=="y" set buildcmd=%buildcmd% swr=1
+@if %pythonver%==2 if /I "%swrdrv%"=="y" set swrdrv=1
+@if %pythonver%==2 if /I "%swrdrv%"=="1" set disableosmesa=1
 @if %pythonver% GEQ 3 if /I "%swrdrv%"=="y" set buildconf=%buildconf% -Dgallium-drivers=swrast,swr
 
 @set /p gles=Do you want to build GLAPI shared library and GLES support (y/n):
 @echo.
-@if %pythonver%==2 if /I "%gles%"=="y" set gles=y
+@if %pythonver%==2 if /I "%gles%"=="y" set disableosmesa=1
 @if %pythonver%==2 if /I NOT "%gles%"=="y" set gles=0
 @if %pythonver% GEQ 3 if /I NOT "%gles%"=="y" set buildconf=%buildconf% -Dgles1=false -Dgles2=false
 
@@ -151,19 +159,19 @@ GOTO skipmesa
 @if %pythonver% GEQ 3 echo Build configuration command stored in buildconf variable.
 @if %pythonver% GEQ 3 echo.
 @if %pythonver% GEQ 3 cmd
-@if %pythonver%==2 IF /I "%osmesa%"=="y" IF /I "%gles%"=="y" (
-echo Build command: %buildcmd% gles=0 %mesatargets%
+@if %pythonver%==2 IF /I "%osmesa%"=="y" IF %disableosmesa%==1 (
+echo Build command: %buildcmd% gles=0 swr=0 %mesatargets%
 @echo.
-@%buildcmd% gles=0 %mesatargets%
+@%buildcmd% gles=0 swr=0 %mesatargets%
 @echo.
 @pause
 @echo Beginning 2nd build pass
 @echo.
 )
 @if %pythonver%==2 (
-@echo Build command: %buildcmd% gles=%gles% %mesatargets%
+@echo Build command: %buildcmd% gles=%gles% swr=%swrdrv% %mesatargets%
 @echo.
-@%buildcmd% gles=%gles% %mesatargets%
+@%buildcmd% gles=%gles% swr=%swrdrv% %mesatargets%
 @echo.
 )
 

.gitignore
@@ -4,3 +4,5 @@ bin/x86-bk/
 bin/x64-bk/
 bin/*.exe
 bin/*.tmp
+lib/
+include/

buildscript/modules/dist.cmd
@@ -7,20 +7,33 @@
 @if NOT EXIST mesa-dist-win MD mesa-dist-win
 @cd mesa-dist-win
 @if NOT EXIST bin MD bin
+@if NOT EXIST lib MD lib
+@if NOT EXIST include MD include
 @cd bin
 @if EXIST %abi% RD /S /Q %abi%
 @MD %abi%
 @cd %abi%
 @MD osmesa-gallium
 @MD osmesa-swrast
+@cd ..\..\lib
+@if EXIST %abi% RD /S /Q %abi%
+@MD %abi%
+@cd ..\include
+@if EXIST %abi% RD /S /Q %abi%
+@MD %abi%
 @IF %pythonver% GEQ 3 GOTO mesondist
 
 :sconsdist
+@rem Copy binaries
 @IF EXIST %mesa%\mesa\build\windows-%longabi%\bin RD /S /Q %mesa%\mesa\build\windows-%longabi%\bin
 @forfiles /p %mesa%\mesa\build\windows-%longabi% /s /m *.dll /c "cmd /c IF NOT @file==0x22osmesa.dll0x22 IF NOT @file==0x22graw.dll0x22 copy @path %mesa%\mesa-dist-win\bin\%abi%"
-@IF EXIST %mesa%\mesa\build\windows-%longabi%\mesa\drivers\osmesa\osmesa.dll copy %mesa%\mesa\build\windows-%longabi%\mesa\drivers\osmesa\osmesa.dll osmesa-swrast\osmesa.dll
-@IF EXIST %mesa%\mesa\build\windows-%longabi%\gallium\targets\osmesa\osmesa.dll copy %mesa%\mesa\build\windows-%longabi%\gallium\targets\osmesa\osmesa.dll osmesa-gallium\osmesa.dll
-@IF EXIST %mesa%\mesa\build\windows-%longabi%\gallium\targets\graw-gdi\graw.dll copy %mesa%\mesa\build\windows-%longabi%\gallium\targets\graw-gdi\graw.dll graw.dll
+@IF EXIST %mesa%\mesa\build\windows-%longabi%\mesa\drivers\osmesa\osmesa.dll copy %mesa%\mesa\build\windows-%longabi%\mesa\drivers\osmesa\osmesa.dll %mesa%\mesa-dist-win\bin\%abi%\osmesa-swrast\osmesa.dll
+@IF EXIST %mesa%\mesa\build\windows-%longabi%\gallium\targets\osmesa\osmesa.dll copy %mesa%\mesa\build\windows-%longabi%\gallium\targets\osmesa\osmesa.dll %mesa%\mesa-dist-win\bin\%abi%\osmesa-gallium\osmesa.dll
+@IF EXIST %mesa%\mesa\build\windows-%longabi%\gallium\targets\graw-gdi\graw.dll copy %mesa%\mesa\build\windows-%longabi%\gallium\targets\graw-gdi\graw.dll %mesa%\mesa-dist-win\bin\%abi%\graw.dll
+
+@rem Copy build development artifacts
+@xcopy %mesa%\mesa\build\windows-%longabi%\*.lib %mesa%\mesa-dist-win\lib\%abi% /E /I /G
+@xcopy %mesa%\mesa\build\windows-%longabi%\*.h %mesa%\mesa-dist-win\include\%abi% /E /I /G
 @echo.
 @GOTO exit
 

debug/llvm-config.cmd
@@ -1,30 +1,31 @@
 @cd ../../
+@set llvmlink=mt
+@set llvmmeson=bitwriter engine mcdisassembler mcjit
+@set llvmscons=engine mcjit bitwriter x86asmprinter irreader
+@set llvm7=bitwriter engine mcdisassembler mcjit ipo objcarcopts
 @for %%a in ("%cd%") do @set mesa=%%~sa
 @IF NOT EXIST llvm GOTO error
 @cd llvm
-@if EXIST x64 (
-@cd x64
-@GOTO writedebugoutput
-)
-@if EXIST x86 (
-@cd x86
-@GOTO writedebugoutput
-)
-@GOTO error
+@if NOT EXIST x64-%llvmlink% if NOT EXIST x86-%llvmlink% GOTO error
+@if EXIST x64-%llvmlink% cd x64-%llvmlink%
+@if EXIST x86-%llvmlink% cd x86-%llvmlink%
 
 :writedebugoutput
-@IF EXIST bin (
+@IF NOT EXIST bin GOTO error
 @cd bin
 @if EXIST %mesa%\mesa-dist-win\debug\llvm-config-old.txt del %mesa%\mesa-dist-win\debug\llvm-config-old.txt
-@if EXIST %mesa%\mesa-dist-win\debug\llvm-config.txt REN %mesa%\mesa-dist-win\debug\llvm-config.txt %mesa%\mesa-dist-win\debug\llvm-config-old.txt
-@llvm-config --libs engine mcjit bitwriter x86asmprinter irreader > %mesa%\mesa-dist-win\debug\llvm-config.txt
+@if EXIST %mesa%\mesa-dist-win\debug\llvm-config.txt REN %mesa%\mesa-dist-win\debug\llvm-config.txt llvm-config-old.txt
+@FOR /F "tokens=* USEBACKQ" %%b IN (`llvm-config --libnames %llvm7%`) DO @SET llvmlibs=%%~b
+@set llvmlibs=%llvmlibs:.lib=%
+@set llvmlibs='%llvmlibs: =', '%'
 @echo LLVM config output updated.
+@echo %llvmlibs% > %mesa%\mesa-dist-win\debug\llvm-config.txt
 @GOTO finished
-)
 
 :error
 @echo LLVM is not built yet. Cannot determine required libraries for requested configuration.
 
 :finished
 @pause
+@%mesa%\mesa-dist-win\debug\llvm-config.txt
 @exit

debug/mesa3dmesontest.cmd
@@ -1,13 +1,0 @@
-@set mesa=C:\Software\Development\projects\mesa
-@set abi=x64
-@SET PATH=C:\Software\Development\Git\cmd\;%mesa%\Python\;%mesa%\flexbison\;%mesa%\ninja\;%mesa%\pkgconfig\;%PATH%
-@call "%ProgramFiles% (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-@cd %mesa%
-@IF NOT EXIST mesa git clone --recurse-submodules --branch=meson-windows https://gitlab.freedesktop.org/dbaker/mesa.git mesa
-@set meson=%mesa%\Py3\python.exe %mesa%\Py3\Scripts\meson.py
-@set buildconf=%meson% .\%abi%
-@set buildcmd=ninja -C .\%abi%
-@IF EXIST %mesa%\mesa\%abi% RD /S /Q %mesa%\mesa\%abi%
-@cd mesa
-@rem IF EXIST %mesa%\llvm\%abi% SET PATH=%mesa%\llvm\%abi%\bin\;%PATH%
-@cmd

mesa3d.iss
@@ -2,7 +2,7 @@
 ; SEE THE DOCUMENTATION FOR DETAILS ON CREATING INNO SETUP SCRIPT FILES!
 
 #define MyAppName "Mesa3D software rendering drivers for Windows"
-#define MyAppVersion "18.2.1"
+#define MyAppVersion "18.2.2"
 #define MyAppPublisher "Pal100x"
 #define MyAppURL "https://github.com/pal1000/mesa-dist-win"
 #define MyAppExeName "mesa-{#MyAppVersion}-setup.exe"

patches/osmesa.patch
@@ -1,40 +1,26 @@
-diff --git a/src/gallium/state_trackers/osmesa/SConscript b/src/gallium/state_trackers/osmesa/SConscript
-index f5519f1..8e0da94 100644
---- a/src/gallium/state_trackers/osmesa/SConscript
-+++ b/src/gallium/state_trackers/osmesa/SConscript
-@@ -15,7 +15,7 @@ if env['platform'] == 'windows':
-         'BUILD_GL32', # declare gl* as __declspec(dllexport) in Mesa headers
-         'WIN32_LEAN_AND_MEAN', # http://msdn2.microsoft.com/en-us/library/6dwk3a1z.aspx
-     ])
--    if not env['gles']:
-+    if 1==1:
-         # prevent _glapi_* from being declared __declspec(dllimport)
-         env.Append(CPPDEFINES = ['_GLAPI_NO_EXPORTS'])
+diff --git a/src/gallium/SConscript b/src/gallium/SConscript
+index 358c31e..c71ccfd 100644
+--- a/src/gallium/SConscript
++++ b/src/gallium/SConscript
+@@ -58,7 +58,7 @@ SConscript([
+     'targets/graw-null/SConscript',
+ ])
  
-diff --git a/src/gallium/targets/osmesa/SConscript b/src/gallium/targets/osmesa/SConscript
-index 3df5c50..5fb9a30 100644
---- a/src/gallium/targets/osmesa/SConscript
-+++ b/src/gallium/targets/osmesa/SConscript
-@@ -2,6 +2,9 @@ Import('*')
+-if not env['embedded']:
++if not env['embedded'] and not env['gles'] and not env['swr']:
+     SConscript([
+         'state_trackers/osmesa/SConscript',
+         'targets/osmesa/SConscript',
+diff --git a/src/mesa/drivers/SConscript b/src/mesa/drivers/SConscript
+index f549910..165619a 100644
+--- a/src/mesa/drivers/SConscript
++++ b/src/mesa/drivers/SConscript
+@@ -1,6 +1,6 @@
+ Import('*')
+-
+-SConscript('osmesa/SConscript')
++if not env['gles'] and not env['swr']:
++    SConscript('osmesa/SConscript')
  
- env = env.Clone()
- 
-+if env['gles']:
-+     Return()
-+
- env.Prepend(CPPPATH = [
-     '#src/mapi',
-     '#src/mesa',
-diff --git a/src/mesa/drivers/osmesa/SConscript b/src/mesa/drivers/osmesa/SConscript
-index 064f3da..a918dc5 100644
---- a/src/mesa/drivers/osmesa/SConscript
-+++ b/src/mesa/drivers/osmesa/SConscript
-@@ -29,7 +29,7 @@ if env['platform'] == 'windows':
-         '_GDI32_', # prevent wgl* being declared __declspec(dllimport)
-         'BUILD_GL32', # declare gl* as __declspec(dllexport) in Mesa headers
-     ])
--    if not env['gles']:
-+    if 1==1:
-         # prevent _glapi_* from being declared __declspec(dllimport)
-         env.Append(CPPDEFINES = ['_GLAPI_NO_EXPORTS'])
- 
+ if env['x11']:
+     SConscript('x11/SConscript')

patches/swr-llvm7.patch
@@ -1,0 +1,145 @@
+diff --git a/configure.ac b/configure.ac
+index 864dcae8e3..9fd68d4929 100644
+--- a/configure.ac
++++ b/configure.ac
+@@ -2719,6 +2719,11 @@ if test -n "$with_gallium_drivers"; then
+             ;;
+         xswr)
+             llvm_require_version $LLVM_REQUIRED_SWR "swr"
++            llvm_add_default_components "swr"
++            if test $LLVM_VERSION_MAJOR -ge 7; then
++                llvm_add_component "ipo" "swr"
++                llvm_add_component "ObjCARCOpts" "swr"
++            fi
+ 
+             if test "x$HAVE_CXX11" != "xyes"; then
+                 AC_MSG_ERROR([swr requires c++11 support])
+diff --git a/src/gallium/drivers/swr/rasterizer/jitter/blend_jit.cpp b/src/gallium/drivers/swr/rasterizer/jitter/blend_jit.cpp
+index f89c502db7..1d6fb405dd 100644
+--- a/src/gallium/drivers/swr/rasterizer/jitter/blend_jit.cpp
++++ b/src/gallium/drivers/swr/rasterizer/jitter/blend_jit.cpp
+@@ -870,7 +870,11 @@ struct BlendJit : public Builder
+         passes.add(createCFGSimplificationPass());
+         passes.add(createEarlyCSEPass());
+         passes.add(createInstructionCombiningPass());
++#if LLVM_VERSION_MAJOR >= 7
++        passes.add(createInstSimplifyLegacyPass());
++#else
+         passes.add(createInstructionSimplifierPass());
++#endif
+         passes.add(createConstantPropagationPass());
+         passes.add(createSCCPPass());
+         passes.add(createAggressiveDCEPass());
+diff --git a/src/gallium/drivers/swr/rasterizer/jitter/fetch_jit.cpp b/src/gallium/drivers/swr/rasterizer/jitter/fetch_jit.cpp
+index b4d326ebdc..26972cddc2 100644
+--- a/src/gallium/drivers/swr/rasterizer/jitter/fetch_jit.cpp
++++ b/src/gallium/drivers/swr/rasterizer/jitter/fetch_jit.cpp
+@@ -294,7 +294,11 @@ Function* FetchJit::Create(const FETCH_COMPILE_STATE& fetchState)
+     optPasses.add(createCFGSimplificationPass());
+     optPasses.add(createEarlyCSEPass());
+     optPasses.add(createInstructionCombiningPass());
++#if LLVM_VERSION_MAJOR >= 7
++    optPasses.add(createInstSimplifyLegacyPass());
++#else
+     optPasses.add(createInstructionSimplifierPass());
++#endif
+     optPasses.add(createConstantPropagationPass());
+     optPasses.add(createSCCPPass());
+     optPasses.add(createAggressiveDCEPass());
+diff --git a/src/gallium/drivers/swr/rasterizer/jitter/functionpasses/lower_x86.cpp b/src/gallium/drivers/swr/rasterizer/jitter/functionpasses/lower_x86.cpp
+index 7605823c04..c58a7552a3 100644
+--- a/src/gallium/drivers/swr/rasterizer/jitter/functionpasses/lower_x86.cpp
++++ b/src/gallium/drivers/swr/rasterizer/jitter/functionpasses/lower_x86.cpp
+@@ -76,7 +76,11 @@ namespace SwrJit
+         {"meta.intrinsic.VCVTPS2PH", Intrinsic::x86_vcvtps2ph_256},
+         {"meta.intrinsic.VPTESTC", Intrinsic::x86_avx_ptestc_256},
+         {"meta.intrinsic.VPTESTZ", Intrinsic::x86_avx_ptestz_256},
++#if LLVM_VERSION_MAJOR >= 7
++        {"meta.intrinsic.VFMADDPS", Intrinsic::fma},
++#else
+         {"meta.intrinsic.VFMADDPS", Intrinsic::x86_fma_vfmadd_ps_256},
++#endif
+         {"meta.intrinsic.VPHADDD", Intrinsic::x86_avx2_phadd_d},
+         {"meta.intrinsic.PDEP32", Intrinsic::x86_bmi_pdep_32},
+         {"meta.intrinsic.RDTSC", Intrinsic::x86_rdtsc},
+diff --git a/src/gallium/drivers/swr/rasterizer/jitter/jit_pch.hpp b/src/gallium/drivers/swr/rasterizer/jitter/jit_pch.hpp
+index 47f717bfc2..e58de783ab 100644
+--- a/src/gallium/drivers/swr/rasterizer/jitter/jit_pch.hpp
++++ b/src/gallium/drivers/swr/rasterizer/jitter/jit_pch.hpp
+@@ -70,6 +70,7 @@ using PassManager         = llvm::legacy::PassManager;
+ #if LLVM_VERSION_MAJOR >= 7
+ #include "llvm/Transforms/Utils.h"
+ #include "llvm/Transforms/InstCombine/InstCombine.h"
++#include "llvm/LinkAllPasses.h"
+ #endif
+ #include "llvm/Support/Host.h"
+ #include "llvm/Support/DynamicLibrary.h"
+diff --git a/src/gallium/drivers/swr/rasterizer/jitter/streamout_jit.cpp b/src/gallium/drivers/swr/rasterizer/jitter/streamout_jit.cpp
+index 8f86af2a4b..1b78c8296e 100644
+--- a/src/gallium/drivers/swr/rasterizer/jitter/streamout_jit.cpp
++++ b/src/gallium/drivers/swr/rasterizer/jitter/streamout_jit.cpp
+@@ -306,7 +306,11 @@ struct StreamOutJit : public Builder
+         passes.add(createCFGSimplificationPass());
+         passes.add(createEarlyCSEPass());
+         passes.add(createInstructionCombiningPass());
+-        passes.add(createInstructionSimplifierPass());
++#if LLVM_VERSION_MAJOR >= 7
++    passes.add(createInstSimplifyLegacyPass());
++#else
++    passes.add(createInstructionSimplifierPass());
++#endif
+         passes.add(createConstantPropagationPass());
+         passes.add(createSCCPPass());
+         passes.add(createAggressiveDCEPass());
+diff --git a/scons/llvm.py b/scons/llvm.py
+index a34edfb..6cb607d 100644
+--- a/scons/llvm.py
++++ b/scons/llvm.py
+@@ -103,8 +103,9 @@ def generate(env):
+             'HAVE_STDINT_H',
+         ])
+         env.Prepend(LIBPATH = [os.path.join(llvm_dir, 'lib')])
+-        # LIBS should match the output of `llvm-config --libs engine mcjit bitwriter x86asmprinter irreader`
+-        if llvm_version >= distutils.version.LooseVersion('5.0'):
++        # LIBS should match the output of `llvm-config --libs engine mcjit bitwriter x86asmprinter irreader` for LLVM < 7.0
++        # and `llvm-config --libs bitwriter engine mcdisassembler mcjit ipo objcarcopts` for LLVM >= 7.0
++        if llvm_version >= distutils.version.LooseVersion('7.0'):
+             env.Prepend(LIBS = [
+                 'LLVMX86Disassembler', 'LLVMX86AsmParser',
+                 'LLVMX86CodeGen', 'LLVMSelectionDAG', 'LLVMAsmPrinter',
+@@ -122,9 +123,34 @@ def generate(env):
+                 'LLVMIRReader', 'LLVMAsmParser',
+                 'LLVMDemangle', 'LLVMGlobalISel', 'LLVMDebugInfoMSF',
+                 'LLVMBinaryFormat',
++                'LLVMObjCARCOpts', 'LLVMipo', 'LLVMVectorize',
++                'LLVMLinker', 'LLVMAggressiveInstCombine',
+             ])
+             if env['platform'] == 'windows' and env['crosscompile']:
+-                # LLVM 5.0 requires MinGW w/ pthreads due to use of std::thread and friends.
++                # LLVM 5.0 and newer require MinGW w/ pthreads due to use of std::thread and friends.
++                assert env['gcc']
++                env['CXX'] = env['CXX'] + '-posix'
++        elif llvm_version >= distutils.version.LooseVersion('5.0'):
++            env.Prepend(LIBS = [
++                'LLVMX86Disassembler', 'LLVMX86AsmParser',
++                'LLVMX86CodeGen', 'LLVMSelectionDAG', 'LLVMAsmPrinter',
++                'LLVMDebugInfoCodeView', 'LLVMCodeGen',
++                'LLVMScalarOpts', 'LLVMInstCombine',
++                'LLVMTransformUtils',
++                'LLVMBitWriter', 'LLVMX86Desc',
++                'LLVMMCDisassembler', 'LLVMX86Info',
++                'LLVMX86AsmPrinter', 'LLVMX86Utils',
++                'LLVMMCJIT', 'LLVMExecutionEngine', 'LLVMTarget',
++                'LLVMAnalysis', 'LLVMProfileData',
++                'LLVMRuntimeDyld', 'LLVMObject', 'LLVMMCParser',
++                'LLVMBitReader', 'LLVMMC', 'LLVMCore',
++                'LLVMSupport',
++                'LLVMIRReader', 'LLVMAsmParser',
++                'LLVMDemangle', 'LLVMGlobalISel', 'LLVMDebugInfoMSF',
++                'LLVMBinaryFormat',
++            ])
++            if env['platform'] == 'windows' and env['crosscompile']:
++                # LLVM 5.0 and newer require MinGW w/ pthreads due to use of std::thread and friends.
+                 assert env['gcc']
+                 env['CXX'] = env['CXX'] + '-posix'
+         elif llvm_version >= distutils.version.LooseVersion('4.0'):

commit 11eaca48603366fa6a1dea6c63e1b687c6c5d76a

Mesa3D build script: Convert gles variable value to lowercase just be safe. Regression from f86d93b8.

buildscript/modules/mesa3d.cmd
@@ -109,6 +109,7 @@ GOTO skipmesa
 
 @set /p gles=Do you want to build GLAPI shared library and GLES support (y/n):
 @echo.
+@if %pythonver%==2 if /I "%gles%"=="y" set gles=y
 @if %pythonver%==2 if /I "%gles%"=="y" set disableosmesa=1
 @if %pythonver%==2 if /I NOT "%gles%"=="y" set gles=0
 @if %pythonver% GEQ 3 if /I NOT "%gles%"=="y" set buildconf=%buildconf% -Dgles1=false -Dgles2=false