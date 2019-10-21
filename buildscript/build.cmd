pal1000/mesa-dist-win

commit 48dc6db9f1cee44dc93617f7cf2c2cb28861eee8

Release Meson build from manual startup mode. Disable LLVM support until is enabled upstream.

buildscript/modules/mesa3d.cmd
@@ -17,12 +17,9 @@
 @if NOT EXIST mesa if /i "%buildmesa%"=="y" echo.
 @if NOT EXIST mesa if /i NOT "%buildmesa%"=="y" GOTO skipmesa
 @if NOT EXIST mesa set branch=master
-@if NOT EXIST mesa IF %mesabldsys%==scons set /p branch=Enter Mesa source code branch name - defaults to master:
-@if NOT EXIST mesa IF %mesabldsys%==scons echo.
-@if NOT EXIST mesa IF %mesabldsys%==scons set mesarepo=https://gitlab.freedesktop.org/mesa/mesa.git
-@if NOT EXIST mesa IF %mesabldsys%==meson set mesarepo=https://gitlab.freedesktop.org/dbaker/mesa.git
-@if NOT EXIST mesa IF %mesabldsys%==meson set branch=meson-windows
-@if NOT EXIST mesa git clone --recurse-submodules --depth=1 --branch=%branch% %mesarepo% mesa
+@if NOT EXIST mesa set /p branch=Enter Mesa source code branch name - defaults to master:
+@if NOT EXIST mesa echo.
+@if NOT EXIST mesa git clone --recurse-submodules --depth=1 --branch=%branch% https://gitlab.freedesktop.org/mesa/mesa.git mesa
 @if NOT EXIST mesa echo.
 
 @if EXIST mesa if /i NOT "%buildmesa%"=="y" set /p buildmesa=Begin mesa build. Proceed (y/n):
@@ -89,8 +86,10 @@
 
 @set llvmless=n
 @if %havellvm%==0 set llvmless=y
-@if %havellvm%==1 set /p llvmless=Build Mesa without LLVM (y/n). llvmpipe and swr drivers and high performance JIT won't be available for other drivers and libraries:
-@if %havellvm%==1 echo.
+@rem Disable LLVM for Meson build
+@if %mesabldsys%==meson set llvmless=y
+@if %havellvm%==1 if %mesabldsys%==scons set /p llvmless=Build Mesa without LLVM (y/n). llvmpipe and swr drivers and high performance JIT won't be available for other drivers and libraries:
+@if %havellvm%==1 if %mesabldsys%==scons echo.
 @if %mesabldsys%==scons if /I "%llvmless%"=="y" set buildcmd=%buildcmd% llvm=no
 @if %mesabldsys%==meson if /I NOT "%llvmless%"=="y" call %mesa%\mesa-dist-win\buildscript\modules\llvmwrapgen.cmd
 @if %mesabldsys%==meson if /I NOT "%llvmless%"=="y" set buildconf=%buildconf% -Dllvm-wrap=llvm -Dllvm=true
@@ -164,14 +163,16 @@
 @IF %toolchain%==gcc set LDFLAGS=-static -s
 
 @rem Execute build.
-@if %mesabldsys%==scons echo Build command: %buildcmd%
-@if %mesabldsys%==scons echo.
-@if %mesabldsys%==scons %buildcmd%
-@if %mesabldsys%==scons IF %toolchain%==msvc echo.
-@if %mesabldsys%==meson echo Build configuration command is stored in buildconf variable.
-@if %mesabldsys%==meson echo Build execution command is stored in buildcmd variable.
+@if %mesabldsys%==meson echo Build configuration command: %buildconf%
 @if %mesabldsys%==meson echo.
-@if %mesabldsys%==meson cmd
+@if %mesabldsys%==meson %buildconf%
+@if %mesabldsys%==meson echo.
+@if %mesabldsys%==meson cd build\%abi%
+@echo Build command: %buildcmd%
+@echo.
+@%buildcmd%
+@echo.
+@if %mesabldsys%==meson cd ..\..\
 
 :skipmesa
 @rem Reset PATH after Mesa3D build to clean the environment again.

commit 83f1da9cb92fcb303dafab36d9622b954c984123

Fix a regression with Scons build introduced in 48dc6db9 buildconf variable is not initialized for Scons build. Continued work on environment vriables garbage collecting.

buildscript/build.cmd
@@ -51,6 +51,9 @@
 @rem If using MSYS2 Mingw-w64 GCC install necessary packages
 @IF %toolchain%==gcc call %mesa%\mesa-dist-win\buildscript\modules\msyspackages.cmd
 
+@rem Binary resource editor
+@call %mesa%\mesa-dist-win\buildscript\modules\resourcehacker.cmd
+
 @rem Dump build environment information
 @call %mesa%\mesa-dist-win\buildscript\modules\envdump.cmd
 

buildscript/modules/abi.cmd
@@ -1,3 +1,4 @@
+@setlocal
 @set abi=x86
 @set /p x64=Do you want to build for x64? (y/n) Otherwise build for x86:
 @echo.
@@ -6,3 +7,4 @@
 @TITLE %TITLE%
 @set longabi=%abi%
 @if %abi%==x64 set longabi=x86_64
+@endlocal&set abi=%abi%&set TITLE=%TITLE%&set longabi=%longabi%

buildscript/modules/addversioninfo.cmd
@@ -1,13 +1,10 @@
+@setlocal
 @rem Get Mesa3D version
 @set /p mesaver=<%mesa%\mesa\VERSION
 @if "%mesaver:~5,2%"=="0-" set mesaver=%mesaver:~0,6%
 
 @rem Check if resource hacker can be reached.
-@set rhstate=2
-@set ERRORLEVEL=0
-@where /q ResourceHacker.exe
-@IF ERRORLEVEL 1 set rhstate=1
-@IF %rhstate%==1 IF NOT EXIST %mesa%\resource-hacker\ResourceHacker.exe set rhstate=0&GOTO noresourcehacker
+@IF %rhstate%==0 GOTO noresourcehacker
 @IF %rhstate%==1 SET PATH=%mesa%\resource-hacker\;%PATH%
 
 @rem Create folder to store generated resource files
@@ -41,3 +38,4 @@
 @echo.
 
 :noresourcehacker
+@endlocal

buildscript/modules/applypatch.cmd
@@ -1,5 +1,7 @@
+@setlocal
 @echo Applying patch %*...
 @IF %toolchain%==msvc git apply --check --apply %mesa%\mesa-dist-win\patches\%*.patch
 @IF %toolchain%==msvc echo.
 @IF %toolchain%==gcc %msysloc%\usr\bin\bash --login -c "cd $mesa/mesa;patch -Np1 --no-backup-if-mismatch -r - -i $mesa/mesa-dist-win/patches/%*.patch"
 @IF %toolchain%==gcc echo.
+@endlocal

buildscript/modules/cmake.cmd
@@ -1,8 +1,8 @@
+@setlocal
 @rem CMake build generator. Can have all states.
-@SET ERRORLEVEL=0
-@SET cmakeloc=cmake.exe
 @set cmakestate=2
+@SET ERRORLEVEL=0
 @where /q cmake.exe
-@IF ERRORLEVEL 1 set cmakeloc=%mesa%\cmake\bin\cmake.exe
-@IF %cmakeloc%==%mesa%\cmake\bin\cmake.exe set cmakestate=1
-@IF %cmakestate%==1 IF NOT EXIST %cmakeloc% set cmakestate=0
+@IF ERRORLEVEL 1 set cmakestate=1
+@IF %cmakestate%==1 IF NOT EXIST %mesa%\cmake\bin\cmake.exe set cmakestate=0
+@endlocal&set cmakestate=%cmakestate%

buildscript/modules/dist.cmd
@@ -1,3 +1,4 @@
+@setlocal
 @rem Create distribution.
 @if NOT EXIST %mesa%\mesa\build\windows-%longabi% if NOT EXIST %mesa%\mesa\build\%abi% GOTO exit
 @set /p dist=Create or update Mesa3D distribution package (y/n):
@@ -51,5 +52,6 @@
 @call %mesa%\mesa-dist-win\buildscript\modules\addversioninfo.cmd
 
 :exit
+@endlocal
 @pause
 @exit

buildscript/modules/git.cmd
@@ -1,5 +1,7 @@
+@setlocal
 @rem Git version control. Can either be always present (2) or missing (0).
 @SET ERRORLEVEL=0
 @set gitstate=2
 @where /q git.exe
 @IF ERRORLEVEL 1 set gitstate=0
+@endlocal&set gitstate=%gitstate%

buildscript/modules/llvm.cmd
@@ -1,3 +1,4 @@
+@setlocal
 @rem Look for CMake build generator.
 @IF %cmakestate%==0 IF EXIST %mesa%\llvm\%abi%\bin IF EXIST %mesa%\llvm\%abi%\include IF EXIST %mesa%\llvm\%abi%\lib echo CMake not found but LLVM is already built. Skipping LLVM build.
 @IF %cmakestate%==0 IF EXIST %mesa%\llvm\%abi%\bin IF EXIST %mesa%\llvm\%abi%\include IF EXIST %mesa%\llvm\%abi%\lib GOTO skipllvm
@@ -57,6 +58,6 @@
 
 :skipllvm
 @echo.
-@rem Reset PATH and current folder after LLVM build.
-@set PATH=%oldpath%
+@rem Reset environment after LLVM build.
+@endlocal
 @cd %mesa%

buildscript/modules/mesa3d.cmd
@@ -73,6 +73,7 @@
 @if %mesabldsys%==scons if %toolchain%==gcc set buildcmd=%buildcmd% toolchain=mingw
 @if %mesabldsys%==scons if %toolchain%==msvc set buildcmd=%buildcmd% MSVC_USE_SCRIPT=%vsenv%
 
+@set buildconf=null
 @if %mesabldsys%==meson set buildconf=%mesonloc% build/%abi% --backend=
 @IF %mesabldsys%==meson set platformabi=Win32
 @IF %mesabldsys%==meson IF %abi%==x64 set platformabi=%abi%

buildscript/modules/msys.cmd
@@ -1,3 +1,4 @@
+@setlocal
 @rem ABI format conversions for Mingw
 @set mingwabi=%longabi%
 @set minabi=%abi:~1%
@@ -9,4 +10,4 @@
 @set msysloc=%mesa%\msys64
 @IF NOT EXIST %msysloc% set msysloc=%mesa%\msys32
 @IF NOT EXIST %msysloc% set msysstate=0
-@set MSYSTEM=MSYS
+@endlocal&set mingwabi=%mingwabi%&set minabi=%minabi%&set msysstate=%msysstate%&set msysloc=%msysloc%

buildscript/modules/msyspackages.cmd
@@ -1,3 +1,4 @@
+@setlocal
 @rem Select build system for Mesa3D build
 @set selectmesabld=1
 
@@ -18,10 +19,8 @@
 @IF %selectmesabld%==2 set mesabldsys=meson
 
 @rem Get dependencies
+@set MSYSTEM=MSYS
 @IF %mesabldsys%==scons %msysloc%\usr\bin\bash --login -c "pacman -S python2-mako mingw-w64-%mingwabi%-llvm mingw-w64-%mingwabi%-gcc mingw-w64-%mingwabi%-zlib flex bison patch scons --needed --noconfirm --disable-download-timeout"
 @IF %mesabldsys%==meson %msysloc%\usr\bin\bash --login -c "pacman -S mingw-w64-%mingwabi%-python3-mako mingw-w64-%mingwabi%-llvm mingw-w64-%mingwabi%-gcc mingw-w64-%mingwabi%-zlib mingw-w64-%mingwabi%-meson flex bison patch --needed --noconfirm --disable-download-timeout"
 @echo.
-@set flexstate=2
-@set ninjastate=2
-@set pkgconfigstate=1
-
+@endlocal&set mesabldsys=%mesabldsys%&set flexstate=2&set ninjastate=2&set pkgconfigstate=1

buildscript/modules/msysupdate.cmd
@@ -1,5 +1,7 @@
+@setlocal
 @rem Check for updates
 @rem Initial upgrade
+@set MSYSTEM=MSYS
 @IF NOT EXIST %msysloc%\usr\bin\bison.exe (
 @%msysloc%\usr\bin\bash --login -c "pacman -Syu --noconfirm --disable-download-timeout"
 @echo.
@@ -13,3 +15,4 @@
 @IF /I "%msysupdate%"=="y" echo.
 @IF /I "%msysupdate%"=="y" %msysloc%\usr\bin\bash --login -c "pacman -Syu --noconfirm --disable-download-timeout"
 @IF /I "%msysupdate%"=="y" echo.
+@endlocal

buildscript/modules/ninjallvmbuild.cmd
@@ -1,3 +1,4 @@
+@setlocal ENABLEDELAYEDEXPANSION
 @echo Building llvm-config tool...
 @ninja -j %throttle% install-llvm-config
 @echo.
@@ -5,9 +6,9 @@
 @rem Build LLVM libraries
 @set llvmlibstotal=0
 @FOR /F "skip=2 tokens=4 USEBACKQ" %%a IN (`%mesa%\llvm\%abi%\bin\llvm-config.exe --libnames engine coroutines 2^>^&1`) DO @set /a llvmlibstotal+=1
-@setlocal ENABLEDELAYEDEXPANSION
 @set llvmlibscount=0
 @FOR /F "skip=2 tokens=4 USEBACKQ" %%a IN (`%mesa%\llvm\%abi%\bin\llvm-config.exe --libnames engine coroutines 2^>^&1`) DO @set /a llvmlibscount+=1&echo Building library %%~na - !llvmlibscount! of %llvmlibstotal%...&ninja -j %throttle% install-%%~na&echo.
-@endlocal
+
 @echo Installing headers...
 @ninja -j %throttle% install-llvm-headers
+@endlocal

buildscript/modules/winflexbison.cmd
@@ -1,8 +1,8 @@
+@setlocal
 @rem winflexbison. Can have all states.
-@SET ERRORLEVEL=0
-@SET flexloc=win_flex.exe
 @set flexstate=2
+@SET ERRORLEVEL=0
 @where /q win_flex.exe
-@IF ERRORLEVEL 1 set flexloc=%mesa%\flexbison\win_flex.exe
-@IF %flexloc%==%mesa%\flexbison\win_flex.exe set flexstate=1
-@IF %flexstate%==1 IF NOT EXIST %flexloc% set flexstate=0
+@IF ERRORLEVEL 1 set flexstate=1
+@IF %flexstate%==1 IF NOT EXIST %mesa%\flexbison\win_flex.exe set flexstate=0
+@endlocal&set flexstate=%flexstate%