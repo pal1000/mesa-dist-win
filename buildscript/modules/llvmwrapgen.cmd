259246fdf8dcff16b5356e3d6bf15b124dda0241
pal1000 <liviuprodea@yahoo.com>
2019-11-20 18:58:27 +0200

    LLVM Meson wrap file generator: Detect RTTI support

--- buildscript/modules/llvmwrapgen.cmd
+++ buildscript/modules/llvmwrapgen.cmd
@@ -1,10 +1,13 @@
 @setlocal
+@set RTTI=false
 @IF %toolchain%==msvc FOR /F "tokens=* USEBACKQ" %%a IN (`%LLVM%\bin\llvm-config --libnames engine coroutines`) DO @SET llvmlibs=%%~a
 @IF %toolchain%==msvc FOR /F "tokens=* USEBACKQ" %%a IN (`%LLVM%\bin\llvm-config --version`) DO @SET llvmver=%%~a
+@IF %toolchain%==msvc FOR /F "tokens=* USEBACKQ" %%a IN (`%LLVM%\bin\llvm-config --has-rtti`) DO @IF /I "%%a"=="YES" SET RTTI=true
 @IF %toolchain%==gcc FOR /F "tokens=* USEBACKQ" %%a IN (`%msysloc%\usr\bin\bash --login -c "%LLVM%/bin/llvm-config --libnames engine coroutines"`) DO @SET llvmlibs=%%~a
 @IF %toolchain%==gcc FOR /F "tokens=* USEBACKQ" %%a IN (`%msysloc%\usr\bin\bash --login -c "%LLVM%/bin/llvm-config --version"`) DO @SET llvmver=%%~a
-@IF %toolchain%==gcc set llvmlibs=%llvmlibs:.a=%
+@IF %toolchain%==gcc FOR /F "tokens=* USEBACKQ" %%a IN (`%msysloc%\usr\bin\bash --login -c "%LLVM%/bin/llvm-config --has-rtti"`) DO @IF /I "%%a"=="YES" SET RTTI=true
 @IF %toolchain%==msvc set llvmlibs=%llvmlibs:.lib=%
+@IF %toolchain%==gcc set llvmlibs=%llvmlibs:.a=%
 @set llvmlibs='%llvmlibs: =', '%'
 @IF NOT EXIST %mesa%\mesa\subprojects\llvm md %mesa%\mesa\subprojects\llvm
 @(echo project^('llvm', ['cpp']^)
@@ -14,19 +17,18 @@ echo.
 echo _deps = []
 IF %toolchain%==msvc echo _search = '%LLVM:\=/%/lib'
 IF %toolchain%==gcc echo _search = '%msysloc:\=/%%LLVM:\=/%/lib'
-echo foreach d : [%llvmlibs%]
+echo foreach d ^: [%llvmlibs%]
 echo   _deps += cpp.find_library^(d, dirs ^: _search^)
 echo endforeach
 echo.
 echo dep_llvm = declare_dependency^(
 IF %toolchain%==msvc echo   include_directories ^: include_directories^('%LLVM:\=/%/include'^),
 IF %toolchain%==gcc echo   include_directories ^: include_directories^('%msysloc:\=/%%LLVM:\=/%/include'^),
-echo   dependencies : _deps,
-echo   version : '%llvmver%',
+echo   dependencies ^: _deps,
+echo   version ^: '%llvmver%',
 echo ^)
 echo.
-IF %toolchain%==msvc echo has_rtti = true
-IF %toolchain%==gcc echo has_rtti = false
+echo has_rtti = %RTTI%
 IF %toolchain%==msvc echo irbuilder_h = files^('%LLVM:\=/%/include/llvm/IR/IRBuilder.h'^)
 IF %toolchain%==gcc echo irbuilder_h = files^('%msysloc:\=/%%LLVM:\=/%/include/llvm/IR/IRBuilder.h'^)
 )>%mesa%\mesa\subprojects\llvm\meson.build
