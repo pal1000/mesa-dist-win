pal1000/mesa-dist-win

commit 527a5d5a1bd6781cec61b51c3ce6563e17dca664

Explicitly request static libraries from LLVM.

buildscript/modules/ninjallvmbuild.cmd
@@ -5,9 +5,9 @@
 
 @rem Build LLVM libraries
 @set llvmlibstotal=0
-@FOR /F "skip=2 tokens=4 USEBACKQ" %%a IN (`%mesa%\llvm\%abi%\bin\llvm-config.exe --libnames engine coroutines 2^>^&1`) DO @set /a llvmlibstotal+=1
+@FOR /F "skip=2 tokens=4 USEBACKQ" %%a IN (`%mesa%\llvm\%abi%\bin\llvm-config.exe --link-static --libnames engine coroutines 2^>^&1`) DO @set /a llvmlibstotal+=1
 @set llvmlibscount=0
-@FOR /F "skip=2 tokens=4 USEBACKQ" %%a IN (`%mesa%\llvm\%abi%\bin\llvm-config.exe --libnames engine coroutines 2^>^&1`) DO @set /a llvmlibscount+=1&echo Building library %%~na - !llvmlibscount! of %llvmlibstotal%...&ninja -j %throttle% install-%%~na&echo.
+@FOR /F "skip=2 tokens=4 USEBACKQ" %%a IN (`%mesa%\llvm\%abi%\bin\llvm-config.exe --link-static --libnames engine coroutines 2^>^&1`) DO @set /a llvmlibscount+=1&echo Building library %%~na - !llvmlibscount! of %llvmlibstotal%...&ninja -j %throttle% install-%%~na&echo.
 
 @echo Installing headers...
 @ninja -j %throttle% install-llvm-headers

buildscript/modules/llvmwrapgen.cmd
@@ -1,9 +1,9 @@
 @setlocal
 @set RTTI=false
-@IF %toolchain%==msvc FOR /F "tokens=* USEBACKQ" %%a IN (`%LLVM%\bin\llvm-config --libnames engine coroutines`) DO @SET llvmlibs=%%~a
+@IF %toolchain%==msvc FOR /F "tokens=* USEBACKQ" %%a IN (`%LLVM%\bin\llvm-config --link-static --libnames engine coroutines`) DO @SET llvmlibs=%%~a
 @IF %toolchain%==msvc FOR /F "tokens=* USEBACKQ" %%a IN (`%LLVM%\bin\llvm-config --version`) DO @SET llvmver=%%~a
 @IF %toolchain%==msvc FOR /F "tokens=* USEBACKQ" %%a IN (`%LLVM%\bin\llvm-config --has-rtti`) DO @IF /I "%%a"=="YES" SET RTTI=true
-@IF %toolchain%==gcc FOR /F "tokens=* USEBACKQ" %%a IN (`%msysloc%\usr\bin\bash --login -c "%LLVM%/bin/llvm-config --libnames engine coroutines"`) DO @SET llvmlibs=%%~a
+@IF %toolchain%==gcc FOR /F "tokens=* USEBACKQ" %%a IN (`%msysloc%\usr\bin\bash --login -c "%LLVM%/bin/llvm-config --link-static --libnames engine coroutines"`) DO @SET llvmlibs=%%~a
 @IF %toolchain%==gcc FOR /F "tokens=* USEBACKQ" %%a IN (`%msysloc%\usr\bin\bash --login -c "%LLVM%/bin/llvm-config --version"`) DO @SET llvmver=%%~a
 @IF %toolchain%==gcc FOR /F "tokens=* USEBACKQ" %%a IN (`%msysloc%\usr\bin\bash --login -c "%LLVM%/bin/llvm-config --has-rtti"`) DO @IF /I "%%a"=="YES" SET RTTI=true
 @IF %toolchain%==msvc set llvmlibs=%llvmlibs:.lib=%

debug/llvm-config.cmd
@@ -15,7 +15,7 @@
 @if EXIST %mesa%\mesa-dist-win\debug\llvm-config.txt REN %mesa%\mesa-dist-win\debug\llvm-config.txt llvm-config-old.txt
 @set llvmlibs=
 @setlocal ENABLEDELAYEDEXPANSION
-@FOR /F "tokens=* USEBACKQ" %%a IN (`llvm-config --libnames %llvmmodules% 2^>^&1`) DO @SET libname=%%~na&IF NOT "!libname:~0,5!"=="llvm-" SET llvmlibs=!llvmlibs! !libname!
+@FOR /F "tokens=* USEBACKQ" %%a IN (`llvm-config --link-static --libnames %llvmmodules% 2^>^&1`) DO @SET libname=%%~na&IF NOT "!libname:~0,5!"=="llvm-" SET llvmlibs=!llvmlibs! !libname!
 @endlocal&set llvmlibs=%llvmlibs:~1%
 @set llvmlibs=%llvmlibs:.lib=%
 @IF %format%==pythonlist set llvmlibs='%llvmlibs: =', '%'

commit 1883f07f7024d64d33f1a1b92de6c72664629933

Fix LLVM build truncation by 2 libraries. Regression from 527a5d5

buildscript/modules/ninjallvmbuild.cmd
@@ -5,9 +5,9 @@
 
 @rem Build LLVM libraries
 @set llvmlibstotal=0
-@FOR /F "skip=2 tokens=4 USEBACKQ" %%a IN (`%devroot%\llvm\%abi%\bin\llvm-config.exe --link-static --libnames engine coroutines 2^>^&1`) DO @set /a llvmlibstotal+=1
+@FOR /F "tokens=4 USEBACKQ" %%a IN (`%devroot%\llvm\%abi%\bin\llvm-config.exe --link-static --libnames engine coroutines 2^>^&1`) DO @set /a llvmlibstotal+=1
 @set llvmlibscount=0
-@FOR /F "skip=2 tokens=4 USEBACKQ" %%a IN (`%devroot%\llvm\%abi%\bin\llvm-config.exe --link-static --libnames engine coroutines 2^>^&1`) DO @set /a llvmlibscount+=1&echo Building library %%~na - !llvmlibscount! of %llvmlibstotal%...&ninja -j %throttle% install-%%~na&echo.
+@FOR /F "tokens=4 USEBACKQ" %%a IN (`%devroot%\llvm\%abi%\bin\llvm-config.exe --link-static --libnames engine coroutines 2^>^&1`) DO @set /a llvmlibscount+=1&echo Building library %%~na - !llvmlibscount! of %llvmlibstotal%...&ninja -j %throttle% install-%%~na&echo.
 
 @echo Installing headers...
 @ninja -j %throttle% install-llvm-headers