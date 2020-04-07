431372394100b9bf741e5615f4ee3f70d4c0ba7a
pal1000 <liviuprodea@yahoo.com>
2020-04-07 13:05:32 +0300

    Remove Scons build support final remnants

--- buildscript/build.cmd
+++ buildscript/build.cmd
@@ -23,9 +23,6 @@
 @rem If using MSVC search for Python. State tracking is pointless as it is loaded once and we are done. Hard fail if missing.
 @IF %toolchain%==msvc call %devroot%\%projectname%\buildscript\modules\discoverpython.cmd
 
-@rem Select build system to use with Mesa3D.
-@call %devroot%\%projectname%\buildscript\modules\selectmesabldsys.cmd
-
 @rem If using MSVC search for Python packages. Install missing packages automatically. Ask to do an update to all packages.
 @IF %toolchain%==msvc call %devroot%\%projectname%\buildscript\modules\pythonpackages.cmd
 
