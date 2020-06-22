7629c02e56f1ec0d79e268bf57af81aeaf993e3d
pal1000 <liviuprodea@yahoo.com>
2020-06-22 19:09:41 +0300

    LLVM monorepo support part 1: Consider monorepo source during validation

--- buildscript/modules/llvm.cmd
+++ buildscript/modules/llvm.cmd
@@ -1,23 +1,36 @@
 @setlocal
 @set llvmloc=%devroot%\llvm
 
-@rem Look for CMake build generator.
-@IF %cmakestate%==0 IF EXIST %devroot%\llvm\%abi%\bin IF EXIST %devroot%\llvm\%abi%\include IF EXIST %devroot%\llvm\%abi%\lib echo CMake not found but LLVM is already built. Skipping LLVM build.
-@IF %cmakestate%==0 IF EXIST %devroot%\llvm\%abi%\bin IF EXIST %devroot%\llvm\%abi%\include IF EXIST %devroot%\llvm\%abi%\lib GOTO skipllvm
+@rem Check lf LLVM binaries are available
+@set llvmbinaries=0
+@IF EXIST %devroot%\llvm\%abi%\bin IF EXIST %devroot%\llvm\%abi%\include IF EXIST %devroot%\llvm\%abi%\lib set llvmbinaries=1
+
+@rem Check lf LLVM sources are available
+@set llvmsources=1
+@if NOT EXIST %devroot%\llvm\cmake if NOT EXIST %devroot%\llvm-project IF %gitstate EQU 0 set llvmsources=0
+
+@rem Verify if LLVM can be used.
+@IF %llvmbinaries% EQU 1 IF %llvmsources% EQU 0 echo LLVM source code not found but LLVM is already built. Skipping LLVM build.
+@IF %llvmbinaries% EQU 1 IF %llvmsources% EQU 0 GOTO skipllvm
+@IF %cmakestate%==0 IF %llvmbinaries% EQU 1 echo CMake not found but LLVM is already built. Skipping LLVM build.
+@IF %cmakestate%==0 IF %llvmbinaries% EQU 1 GOTO skipllvm
 @IF %cmakestate%==0 echo CMake is required for LLVM build. If you want to build Mesa3D anyway it will be without llvmpipe and swr drivers and high performance JIT won't be available for other drivers and libraries.
 @IF %cmakestate%==0 GOTO skipllvm
-
-@rem Look for LLVM source code and binaries.
-@if NOT EXIST %devroot%\llvm echo WARNING: Both LLVM source code and binaries not found. If you want to build Mesa3D anyway it will be without llvmpipe and swr drivers and high performance JIT won't be available for other drivers and libraries.
-@if NOT EXIST %devroot%\llvm GOTO skipllvm
+@IF %llvmbinaries% EQU 0 IF %llvmsources% EQU 0 echo WARNING: Both LLVM source code and binaries not found. If you want to build Mesa3D anyway it will be without llvmpipe and swr drivers and high performance JIT won't be available for other drivers and libraries.
+@IF %llvmbinaries% EQU 0 IF %llvmsources% EQU 0 GOTO skipllvm
 
 @rem Ask to do LLVM build
-@if EXIST %devroot%\llvm\cmake set /p buildllvm=Begin LLVM build. Only needs to run once for each ABI and version. Proceed (y/n):
-@if EXIST %devroot%\llvm\cmake echo.
+@set /p buildllvm=Begin LLVM build. Only needs to run once for each ABI and version. Proceed (y/n):
+@echo.
 
 @rem LLVM source is found, binaries not found and LLVM build is refused.
-@if /I NOT "%buildllvm%"=="y" if EXIST %devroot%\llvm\cmake IF NOT EXIST %devroot%\llvm\%abi%\include echo WARNING: Not building LLVM. If you want to build Mesa3D anyway it will be without swr and llvmpipe drivers and high performance JIT won't be available for other drivers and libraries.
+@IF %llvmsources% EQU 1 IF %llvmbinaries% EQU 0 if /I NOT "%buildllvm%"=="y" echo WARNING: Not building LLVM. If you want to build Mesa3D anyway it will be without swr and llvmpipe drivers and high performance JIT won't be available for other drivers and libraries.
 @if /I NOT "%buildllvm%"=="y" GOTO skipllvm
+
+@rem Handle unimplemented LLVM build from monorepo gracefully
+@IF %llvmsources% EQU 1 if NOT EXIST %devroot%\llvm\cmake echo WARNING: Build from LLVM monorepo is not yet implemented.
+@IF %llvmsources% EQU 1 if NOT EXIST %devroot%\llvm\cmake IF %llvmbinaries% EQU 0 echo WARNING: Both LLVM source code and binaries not found. If you want to build Mesa3D anyway it will be without llvmpipe and swr drivers and high performance JIT won't be available for other drivers and libraries.
+@IF %llvmsources% EQU 1 if NOT EXIST %devroot%\llvm\cmake GOTO skipllvm
 
 @rem Always clean build
 @cd %devroot%\llvm
