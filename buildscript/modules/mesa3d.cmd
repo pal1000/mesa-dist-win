pal1000/mesa-dist-win

commit 36c12dbb54c0ce946153146151bed56a8f342624

MSYS2: Use a very brutal but simpler approach to static linking zlib Reuse it for zstd

buildscript/modules/mesa3d.cmd
@@ -81,9 +81,10 @@
 
 :configmesabuild
 @rem Configure Mesa build.
-@set buildconf=%mesonloc% build/%abi% --buildtype=release --prefix=%devroot:\=/%/%projectname%/dist/%abi%
-@IF %toolchain%==msvc set buildconf=%buildconf% -Db_vscrt=mt -Dzlib:default_library=static
-@IF NOT %toolchain%==msvc set buildconf=%buildconf% -Dc_args='-march=core2 -pipe' -Dcpp_args='-march=core2 -pipe' -Dc_link_args='-static -s' -Dcpp_link_args='-static -s' --force-fallback-for=zlib
+@set buildconf=%mesonloc% build/%abi% -Dzlib:default_library=static --buildtype=release --prefix=%devroot:\=/%/%projectname%/dist/%abi%
+@IF %toolchain%==msvc set buildconf=%buildconf% -Db_vscrt=mt
+@IF NOT %toolchain%==msvc set buildconf=%buildconf% -Dc_args='-march=core2 -pipe' -Dcpp_args='-march=core2 -pipe' -Dc_link_args='-static -s' -Dcpp_link_args='-static -s'
+@IF NOT %toolchain%==msvc IF %intmesaver% GTR 20000 set buildconf=%buildconf% -Dzstd=%mesonbooltrue% -Dlibzstd:default_library=static
 @IF %toolchain%==clang set CC=clang
 @IF %toolchain%==clang set CXX=clang++
 @set buildcmd=msbuild /p^:Configuration=release,Platform=Win32 mesa.sln /m^:%throttle%
@@ -97,7 +98,7 @@
 @if %havellvm%==1 set /p llvmless=Build Mesa without LLVM (y/n). llvmpipe and swr drivers and high performance JIT won't be available for other drivers and libraries:
 @if %havellvm%==1 echo.
 @call %devroot%\%projectname%\buildscript\modules\mesonsubprojects.cmd
-@if /I NOT "%llvmless%"=="y" IF %llvmconfigbusted% EQU 1 set buildconf=%buildconf%,llvm
+@if /I NOT "%llvmless%"=="y" IF %llvmconfigbusted% EQU 1 set buildconf=%buildconf% --force-fallback-for=llvm
 @if /I NOT "%llvmless%"=="y" set buildconf=%buildconf% -Dllvm=%mesonbooltrue%
 @if /I NOT "%llvmless%"=="y" IF %llvmconfigbusted% EQU 0 set buildconf=%buildconf% -Dshared-llvm=%mesonboolfalse%
 @if /I NOT "%llvmless%"=="y" IF %toolchain%==msvc SET PATH=%devroot%\llvm\%abi%\bin\;%PATH%

buildscript/modules/mesonsubprojects.cmd
@@ -48,7 +48,7 @@ echo irbuilder_h ^= files^(llvmloc + '/include/llvm/IR/IRBuilder.h'^)
 )
 
 :zlibwrap
-@IF %toolchain%==msvc IF EXIST "%devroot%\mesa\subprojects\zlib\" RD /S /Q %devroot%\mesa\subprojects\zlib
+@IF EXIST "%devroot%\mesa\subprojects\zlib\" RD /S /Q %devroot%\mesa\subprojects\zlib
 @CMD /C EXIT 0
 @IF %toolchain%==msvc FC /B %devroot%\%projectname%\buildscript\mesonsubprojects\zlib.wrap %devroot%\mesa\subprojects\zlib.wrap>NUL 2>&1
 @if NOT "%ERRORLEVEL%"=="0" (
@@ -59,33 +59,8 @@ echo irbuilder_h ^= files^(llvmloc + '/include/llvm/IR/IRBuilder.h'^)
 @IF %toolchain%==msvc GOTO donewrap
 @for /d %%a in ("%devroot%\mesa\subprojects\zlib-*") do @RD /S /Q "%%~a"
 @IF EXIST %devroot%\mesa\subprojects\zlib.wrap del %devroot%\mesa\subprojects\zlib.wrap
-@FOR /F USEBACKQ^ tokens^=5^ delims^=-^  %%a IN (`%msysloc%\usr\bin\bash --login -c "/usr/bin/pacman -Q ${MINGW_PACKAGE_PREFIX}-zlib"`) DO @SET zlibver=%%~a
-@IF NOT EXIST "%devroot%\mesa\subprojects\zlib\" md %devroot%\mesa\subprojects\zlib
-@(
-echo project^('zlib', ['cpp']^)
-echo.
-echo cpp ^= meson.get_compiler^('cpp'^)
-echo.
-echo _deps ^= []
-echo zlibloc ^= run_command^('%devroot:\=/%/%projectname%/buildscript/modules/msysmingwruntimeloc.cmd'^).stdout^(^).strip^(^)
-echo _search ^= zlibloc + '/lib'
-echo foreach d ^: ['libz']
-echo   _deps ^+^= cpp.find_library^(d, dirs ^: _search, static ^: true^)
-echo endforeach
-echo.
-echo zlib_dep ^= declare_dependency^(
-echo   include_directories ^: include_directories^(zlibloc + '/include'^),
-echo   dependencies ^: _deps,
-echo   version ^: '%zlibver%',
-echo ^)
-)>%devroot%\%projectname%\buildscript\mesonsubprojects\zlib_meson.build
-@CMD /C EXIT 0
-@FC /B %devroot%\%projectname%\buildscript\mesonsubprojects\zlib_meson.build %devroot%\mesa\subprojects\zlib\meson.build>NUL 2>&1
-@if NOT "%ERRORLEVEL%"=="0" (
-@echo Using binary wrap to find zlib...
-@copy /Y %devroot%\%projectname%\buildscript\mesonsubprojects\zlib_meson.build %devroot%\mesa\subprojects\zlib\meson.build
-@echo.
-)
+@IF EXIST %msysloc%\%MSYSTEM%\lib\libz.dll.a del %msysloc%\%MSYSTEM%\lib\libz.dll.a
+@IF EXIST %msysloc%\%MSYSTEM%\lib\libzstd.dll.a del %msysloc%\%MSYSTEM%\lib\libzstd.dll.a
 
 :donewrap
 @endlocal&set RTTI=%RTTI%&set llvmconfigbusted=%llvmconfigbusted%

commit c66c29942b6902d8f9782db3d60199fe13f23157

Mesa3D: Avoid using Meson options that don't work with toolchain in use Regression from 36c12dbb

buildscript/modules/mesa3d.cmd
@@ -81,10 +81,10 @@
 
 :configmesabuild
 @rem Configure Mesa build.
-@set buildconf=%mesonloc% build/%abi% -Dzlib:default_library=static --buildtype=release --prefix=%devroot:\=/%/%projectname%/dist/%abi%
-@IF %toolchain%==msvc set buildconf=%buildconf% -Db_vscrt=mt
+@set buildconf=%mesonloc% build/%abi% --buildtype=release --prefix=%devroot:\=/%/%projectname%/dist/%abi%
+@IF %toolchain%==msvc set buildconf=%buildconf% -Db_vscrt=mt -Dzlib:default_library=static
 @IF NOT %toolchain%==msvc set buildconf=%buildconf% -Dc_args='-march=core2 -pipe' -Dcpp_args='-march=core2 -pipe' -Dc_link_args='-static -s' -Dcpp_link_args='-static -s'
-@IF NOT %toolchain%==msvc IF %intmesaver% GTR 20000 set buildconf=%buildconf% -Dzstd=%mesonbooltrue% -Dlibzstd:default_library=static
+@IF NOT %toolchain%==msvc IF %intmesaver% GTR 20000 set buildconf=%buildconf% -Dzstd=%mesonbooltrue%
 @IF %toolchain%==clang set CC=clang
 @IF %toolchain%==clang set CXX=clang++
 @set buildcmd=msbuild /p^:Configuration=release,Platform=Win32 mesa.sln /m^:%throttle%