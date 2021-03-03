bcbf8056f42195d87fba6fa8d225f7c9406e2d4a
pal1000 <liviuprodea@yahoo.com>
2021-03-03 11:26:46 +0200

    Fix lavapipe JSON patcher. Fixes 0a775146

--- buildscript/modules/lavapipejson.cmd
+++ buildscript/modules/lavapipejson.cmd
@@ -3,7 +3,7 @@
 @for /R %devroot%\%projectname%\bin\%abi%\ %%a IN (lvp_icd.*.json) do @for /f tokens^=^*^ delims^= %%b IN (%%a) do @(
 @set /a jsonlines+=1
 @IF !jsonlines! NEQ 4 set jsonline[!jsonlines!]=%%b
-@IF !jsonlines! EQU 4 set jsonline[!jsonlines!]=        "library_path": "vulkan_lvp.dll"
+@IF !jsonlines! EQU 4 set jsonline[!jsonlines!]=        "library_path": ".^\^\vulkan_lvp.dll"
 )
 @for /R %devroot%\%projectname%\bin\%abi%\ %%a IN (lvp_icd.*.json) do @(
 @del %%a
