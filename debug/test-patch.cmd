1a437e69ddc4f908f71a2ee5865aa20f3c0e4ecd
pal1000 <liviuprodea@yahoo.com>
2021-04-18 12:27:32 +0300

    Implement list command in patch tester

--- debug/test-patch.cmd
+++ debug/test-patch.cmd
@@ -28,6 +28,8 @@
 @IF %gitstate% GTR 0 IF EXIST "%CD%\.git\" echo.
 @exit
 )
+@IF /I "%ptstcmd%"=="list" for /f %%i in ('FORFILES /P %devroot%\%projectname%\patches /M *.patch /C "cmd /c echo @fname"') do @echo %%~i
+@IF /I "%ptstcmd%"=="list" echo.
 @IF /I "%ptstcmd:~0,3%"=="cd " %ptstcmd%
-@IF /I NOT "%ptstcmd:~0,3%"=="cd " call %devroot%\%projectname%\buildscript\modules\applypatch.cmd %ptstcmd%
+@IF /I NOT "%ptstcmd:~0,3%"=="cd " IF /I NOT "%ptstcmd%"=="list" call %devroot%\%projectname%\buildscript\modules\applypatch.cmd %ptstcmd%
 @GOTO command
