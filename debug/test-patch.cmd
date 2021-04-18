bb5855d7fc07469256e2a8f9ee200bb7fbed2558
pal1000 <liviuprodea@yahoo.com>
2021-04-18 13:51:31 +0300

    Create revert command in patch tester

--- debug/test-patch.cmd
+++ debug/test-patch.cmd
@@ -21,15 +21,17 @@
 @set /p ptstcmd=Enter patch testing command:
 @echo.
 @set msyspatchdir=%CD%
-@IF /I "%ptstcmd%"=="exit" (
+@IF /I "%ptstcmd%"=="revert" (
 @IF %gitstate% GTR 0 IF EXIST "%CD%\.git\" echo Reverting test patches...
 @IF %gitstate% GTR 0 IF EXIST "%CD%\.git\" git checkout .
-@IF %gitstate% GTR 0 IF EXIST "%CD%\.git\" pause
 @IF %gitstate% GTR 0 IF EXIST "%CD%\.git\" echo.
+)
+@IF /I "%ptstcmd%"=="exit" (
+@IF %gitstate% GTR 0 IF EXIST "%CD%\.git\" pause
 @exit
 )
 @IF /I "%ptstcmd%"=="list" for /f %%i in ('FORFILES /P %devroot%\%projectname%\patches /M *.patch /C "cmd /c echo @fname"') do @echo %%~i
 @IF /I "%ptstcmd%"=="list" echo.
 @IF /I "%ptstcmd:~0,3%"=="cd " %ptstcmd%
-@IF /I NOT "%ptstcmd:~0,3%"=="cd " IF /I NOT "%ptstcmd%"=="list" call %devroot%\%projectname%\buildscript\modules\applypatch.cmd %ptstcmd%
+@IF /I NOT "%ptstcmd:~0,3%"=="cd " IF /I NOT "%ptstcmd%"=="list" IF /I NOT "%ptstcmd%"=="revert" call %devroot%\%projectname%\buildscript\modules\applypatch.cmd %ptstcmd%
 @GOTO command
