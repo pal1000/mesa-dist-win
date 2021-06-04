5252f3bb08edabc2bca212eb2e25e52e76e5a644
pal1000 <liviuprodea@yahoo.com>
2021-06-04 12:20:08 +0300

    Improve libclc build cleaning

--- buildscript/modules/libclc.cmd
+++ buildscript/modules/libclc.cmd
@@ -20,6 +20,9 @@
 @echo Cleanning libclc build...
 @echo.
 @if EXIST clc-%abi% RD /S /Q clc-%abi%
+@if EXIST %devroot%\llvm\%abi%\share\clc RD /S /Q %devroot%\llvm\%abi%\share\clc
+@if EXIST %devroot%\llvm\%abi%\share\pkgconfig RD /S /Q %devroot%\llvm\%abi%\share\pkgconfig
+@if EXIST %devroot%\llvm\%abi%\include\clc RD /S /Q %devroot%\llvm\%abi%\include\clc
 @md clc-%abi%
 @cd clc-%abi%
 @cmake ../libclc -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-m%MSYSTEM:~-2%" -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -DCMAKE_INSTALL_PREFIX="../../llvm/%abi%" -DLIBCLC_TARGETS_TO_BUILD="spirv-mesa3d-;spirv64-mesa3d-"
@@ -30,3 +33,6 @@
 @echo.
 
 :finishclc
+@rem Reset environment after libclc build.
+@endlocal
+@cd %devroot%
--- releasenotes.md
+++ releasenotes.md
@@ -1,3 +1,6 @@
+# Next release
+### Build script
+- Improve libclc build cleaning.
 # 21.1.2
 - Updated Mesa3D to [21.1.2](https://docs.mesa3d.org/relnotes/21.1.2.html).
 ### Binary version info
