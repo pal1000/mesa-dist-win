pal1000/mesa-dist-win

commit 5eae0f423cf2419817391db1fe6a0bd6edc83085

More improvement to libelf update

buildscript/modules/mesonsubprojects.cmd
@@ -116,7 +116,14 @@ echo.
 cd /D %devroot%\mesa
 )
 
-@for /d %%a in ("%devroot%\mesa\subprojects\libelf-lfg-win32-*") do @IF EXIST "%%~a" IF %gitstate% GTR 0 (
+@CMD /C EXIT 0
+@FC /B %devroot%\%projectname%\buildscript\mesonsubprojects\libelf.wrap %devroot%\mesa\subprojects\libelf.wrap>NUL 2>&1
+@if NOT "%ERRORLEVEL%"=="0" (
+@echo Switching libelf to full clone with master branch pre-fetched...
+@copy /Y %devroot%\%projectname%\buildscript\mesonsubprojects\libelf.wrap %devroot%\mesa\subprojects\libelf.wrap
+@echo.
+)
+@for /d %%a in ("%devroot%\mesa\subprojects\libelf-lfg-win32") do @IF EXIST "%%~a" IF %gitstate% GTR 0 (
 cd /D "%%~a"
 echo Refreshing libelf for Windows...
 git pull -v --progress --recurse-submodules origin

buildscript/mesonsubprojects/libelf.wrap
@@ -1,0 +1,5 @@
+[wrap-git]
+directory = libelf-lfg-win32
+
+url = https://github.com/LagFreeGames/libelf-lfg-win32.git
+revision = head

commit 64160037d55c12f13756e88dd44e68f255a696a1

Fix libelf dependency validation. Regression from 5eae0f42

buildscript/modules/mesa3d.cmd
@@ -198,7 +198,7 @@
 @IF %intmesaver% LSS 21200 set canradv=0
 @IF %toolchain%==msvc IF NOT EXIST %devroot%\llvm\%abi%\lib\LLVMAMDGPU*.lib set canradv=0
 @IF %abi%==x86 set canradv=0
-@IF NOT EXIST %devroot%\mesa\subprojects\libelf-lfg-win32-* IF %gitstate% EQU 0 set canradv=0
+@IF NOT EXIST %devroot%\mesa\subprojects\libelf-lfg-win32 IF %gitstate% EQU 0 set canradv=0
 @IF NOT %toolchain%==msvc IF %disableootpatch%==1 set canradv=0
 @IF %canradv% EQU 1 set /p radv=Build AMD Vulkan driver - radv (y/n):
 @IF %canradv% EQU 1 echo.

buildscript/modules/mesonsubprojects.cmd
@@ -123,8 +123,8 @@ cd /D %devroot%\mesa
 @copy /Y %devroot%\%projectname%\buildscript\mesonsubprojects\libelf.wrap %devroot%\mesa\subprojects\libelf.wrap
 @echo.
 )
-@for /d %%a in ("%devroot%\mesa\subprojects\libelf-lfg-win32") do @IF EXIST "%%~a" IF %gitstate% GTR 0 (
-cd /D "%%~a"
+@IF EXIST %devroot%\mesa\subprojects\libelf-lfg-win32 IF %gitstate% GTR 0 (
+@cd /D %devroot%\mesa\subprojects\libelf-lfg-win32
 echo Refreshing libelf for Windows...
 git pull -v --progress --recurse-submodules origin
 echo.