pal1000/mesa-dist-win

commit 7b1e045e7e292da47beb474a48c71f385a180413

Change LLVM build and install folders so Git ignores them Get 7-zip version from command line tool Update SPIRV translator if possible even when not building LLVM Some more 32-bit build host improvements, 435beaca follow up

buildscript/modules/envdump.cmd
@@ -27,9 +27,14 @@
 @IF %rhstate% GTR 0 IF %toolchain%==msvc echo Ressource Hacker %rhver:,=.%>>%devroot%\%projectname%\buildinfo\msvc.txt
 
 @rem Dump 7-Zip version and compression level
-@set sevenzipver=21.04
-@IF NOT %toolchain%==msvc echo 7-Zip %sevenzipver% ultra compression>>%devroot%\%projectname%\buildinfo\mingw.txt
-@IF %toolchain%==msvc echo 7-Zip %sevenzipver% ultra compression>>%devroot%\%projectname%\buildinfo\msvc.txt
+@set sevenzipver=null
+@set exitloop=1
+@if EXIST "%ProgramFiles%\7-Zip\7z.exe" for /f "tokens=1-2 delims= " %%a IN ('"%ProgramFiles%\7-Zip\7z.exe"') DO @IF defined exitloop IF /I "%%"=="7-Zip" (
+@set "exitloop="
+@set sevenzipver=%%b
+)
+@IF NOT %toolchain%==msvc IF NOT %sevenzipver%==null echo 7-Zip %sevenzipver% ultra compression>>%devroot%\%projectname%\buildinfo\mingw.txt
+@IF %toolchain%==msvc IF NOT %sevenzipver%==null echo 7-Zip %sevenzipver% ultra compression>>%devroot%\%projectname%\buildinfo\msvc.txt
 
 @rem Get Git version
 @IF NOT %gitstate%==0 FOR /F "USEBACKQ tokens=3" %%a IN (`git --version`) do @set gitver=%%a
@@ -75,7 +80,7 @@ echo CMake %%a>>%devroot%\%projectname%\buildinfo\msvc.txt
 @IF %toolchain%==msvc IF NOT "%ninjastate%"=="0" IF NOT "%ninjastate%"=="" for /f "USEBACKQ" %%a IN (`ninja --version`) do @echo Ninja %%a>>%devroot%\%projectname%\buildinfo\msvc.txt
 
 @rem Get LLVM version
-@IF %toolchain%==msvc IF EXIST %devroot%\llvm\%hostabi%\bin\llvm-config.exe FOR /F "USEBACKQ" %%a IN (`%devroot%\llvm\%hostabi%\bin\llvm-config.exe --version`) do @echo LLVM %%a>>%devroot%\%projectname%\buildinfo\msvc.txt
+@IF %toolchain%==msvc IF EXIST %devroot%\llvm\build\%hostabi%\bin\llvm-config.exe FOR /F "USEBACKQ" %%a IN (`%devroot%\llvm\build\%hostabi%\bin\llvm-config.exe --version`) do @echo LLVM %%a>>%devroot%\%projectname%\buildinfo\msvc.txt
 
 @rem Get SPIRV Tools version
 @IF %toolchain%==msvc IF EXIST %devroot%\spirv-tools\build\%abi%\bin for /f tokens^=1-2^ delims^=^  %%a IN ('type %devroot%\spirv-tools\build\%abi%\lib\pkgconfig\SPIRV-Tools.pc') do @IF /I "%%a"=="Version:" echo SPIRV Tools %%b>>%devroot%\%projectname%\buildinfo\msvc.txt

buildscript/modules/libclc.cmd
@@ -4,9 +4,9 @@
 @IF %cmakestate% EQU 1 set PATH=%devroot%\cmake\bin\;%PATH%
 @if %ninjastate% EQU 0 set canclc=0
 @if %ninjastate% EQU 1 set PATH=%devroot%\ninja\;%PATH%
-@if NOT EXIST %devroot%\llvm\%hostabi%\bin\llvm-spirv.exe set canclc=0
-@if NOT EXIST %devroot%\llvm\%hostabi%\bin\clang.exe set canclc=0
-@if NOT EXIST %devroot%\llvm\%hostabi%\bin\lld.exe set canclc=0
+@if NOT EXIST %devroot%\llvm\build\%hostabi%\bin\llvm-spirv.exe set canclc=0
+@if NOT EXIST %devroot%\llvm\build\%hostabi%\bin\clang.exe set canclc=0
+@if NOT EXIST %devroot%\llvm\build\%hostabi%\bin\lld.exe set canclc=0
 @if NOT EXIST %devroot%\llvm-project set canclc=0
 @if %canclc% EQU 0 echo libclc requires cmake, ninja and %hostabi% LLVM build with clang, LLD and SPIRV translator.
 @if %canclc% EQU 0 echo.
@@ -16,14 +16,14 @@
 @if /I NOT "%buildclc%"=="y" GOTO finishclc
 @call %vsenv% %hostabi%
 @echo.
-@cd %devroot%\llvm-project
+@cd %devroot%\llvm-project\build
 @echo Cleanning libclc build...
 @echo.
-@if EXIST buildsys-clc RD /S /Q buildsys-clc
-@if EXIST %devroot%\llvm\clc RD /S /Q %devroot%\llvm\clc
+@if EXIST "buildsys-clc\" RD /S /Q buildsys-clc
+@if EXIST "clc\" RD /S /Q clc
 @md buildsys-clc
 @cd buildsys-clc
-@set buildconf=cmake ../libclc -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-m64" -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -DCMAKE_INSTALL_PREFIX="../../llvm/clc" -DLIBCLC_TARGETS_TO_BUILD="spirv-mesa3d-;spirv64-mesa3d-" -DCMAKE_PREFIX_PATH="../../llvm/%hostabi%"
+@set buildconf=cmake ../../libclc -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-m64" -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -DCMAKE_INSTALL_PREFIX="../../../llvm/build/clc" -DLIBCLC_TARGETS_TO_BUILD="spirv-mesa3d-;spirv64-mesa3d-" -DCMAKE_PREFIX_PATH="../../../llvm/build/%hostabi%"
 @echo Build configuration command: %buildconf%
 @echo.
 @%buildconf%

buildscript/modules/llvm.cmd
@@ -1,8 +1,23 @@
 @setlocal
 
+@rem Updating LLVM SPIRV translator
+@IF EXIST %devroot%\llvm-project\llvm\projects\SPIRV-LLVM-Translator IF %gitstate% GTR 0 (
+@cd %devroot%\llvm-project\llvm\projects\SPIRV-LLVM-Translator
+@git pull -v --progress --recurse-submodules origin
+@echo.
+)
+@IF EXIST %devroot%\llvm-project\llvm\projects\SPIRV-LLVM-Translator\spirv-headers-tag.conf IF EXIST %devroot%\llvm-project\llvm\projects\SPIRV-Headers IF %gitstate% GTR 0 (
+@cd %devroot%\llvm-project\llvm\projects\SPIRV-Headers
+@git checkout master
+@git pull -v --progress --recurse-submodules origin
+@for /f tokens^=* %%a IN ('type %devroot%\llvm-project\llvm\projects\SPIRV-LLVM-Translator\spirv-headers-tag.conf') do @git checkout %%a
+@echo.
+)
+@cd %devroot%
+
 @rem Check lf LLVM binaries are available
 @set llvmbinaries=0
-@IF EXIST %devroot%\llvm\%abi%\include IF EXIST %devroot%\llvm\%abi%\lib set llvmbinaries=1
+@IF EXIST %devroot%\llvm\build\%abi%\include IF EXIST %devroot%\llvm\build\%abi%\lib set llvmbinaries=1
 
 @rem Check lf LLVM sources are available or obtainable
 @set llvmsources=1
@@ -15,19 +30,19 @@
 @IF %cmakestate%==0 IF %llvmbinaries% EQU 1 echo CMake not found but LLVM is already built. Skipping LLVM build.
 @IF %cmakestate%==0 IF %llvmbinaries% EQU 1 echo.
 @IF %cmakestate%==0 IF %llvmbinaries% EQU 1 GOTO skipllvm
-@IF %cmakestate%==0 echo CMake is required for LLVM build. If you want to build Mesa3D anyway it will be without llvmpipe and swr drivers and high performance JIT won't be available for other drivers and libraries.
+@IF %cmakestate%==0 echo CMake is required for LLVM build. If you want to build Mesa3D anyway it will be without llvmpipe, swr, RADV and all OpenCL drivers and high performance JIT won't be available for softpipe, osmesa and graw.
 @IF %cmakestate%==0 echo.
 @IF %cmakestate%==0 GOTO skipllvm
-@IF %llvmbinaries% EQU 0 IF %llvmsources% EQU 0 echo WARNING: Both LLVM source code and binaries not found. If you want to build Mesa3D anyway it will be without llvmpipe and swr drivers and high performance JIT won't be available for other drivers and libraries.
+@IF %llvmbinaries% EQU 0 IF %llvmsources% EQU 0 echo WARNING: Both LLVM source code and binaries not found. If you want to build Mesa3D anyway it will be without llvmpipe, swr, RADV and all OpenCL drivers and high performance JIT won't be available for softpipe, osmesa and graw.
 @IF %llvmbinaries% EQU 0 IF %llvmsources% EQU 0 echo.
 @IF %llvmbinaries% EQU 0 IF %llvmsources% EQU 0 GOTO skipllvm
 
 @rem Ask to do LLVM build
-@set /p buildllvm=Begin LLVM build. Only needs to run once for each ABI and version. Proceed (y/n):
+@set /p buildllvm=Begin LLVM build (y/n):
 @echo.
 
 @rem LLVM source is found or is obtainable, binaries not found and LLVM build is refused.
-@IF %llvmsources% EQU 1 IF %llvmbinaries% EQU 0 if /I NOT "%buildllvm%"=="y" echo WARNING: Not building LLVM. If you want to build Mesa3D anyway it will be without swr and llvmpipe drivers and high performance JIT won't be available for other drivers and libraries.
+@IF %llvmsources% EQU 1 IF %llvmbinaries% EQU 0 if /I NOT "%buildllvm%"=="y" echo WARNING: Not building LLVM. If you want to build Mesa3D anyway it will be without llvmpipe, swr, RADV and all OpenCL drivers and high performance JIT won't be available for softpipe, osmesa and graw.
 @IF %llvmsources% EQU 1 IF %llvmbinaries% EQU 0 if /I NOT "%buildllvm%"=="y" echo.
 @if /I NOT "%buildllvm%"=="y" GOTO skipllvm
 
@@ -71,18 +86,6 @@
 @IF EXIST %devroot%\llvm-project\llvm\projects\SPIRV-LLVM-Translator\spirv-headers-tag.conf IF NOT EXIST %devroot%\llvm-project\llvm\projects\SPIRV-Headers IF %gitstate% EQU 0 set canllvmspirv=0
 @IF %canllvmspirv% EQU 1 set /p buildllvmspirv=Build SPIRV LLVM Translator - required for OpenCL (y/n):
 @IF %canllvmspirv% EQU 1 echo.
-@if /I "%buildllvmspirv%"=="y" IF EXIST %devroot%\llvm-project\llvm\projects\SPIRV-LLVM-Translator IF %gitstate% GTR 0 (
-@cd %devroot%\llvm-project\llvm\projects\SPIRV-LLVM-Translator
-@git pull -v --progress --recurse-submodules origin
-@echo.
-)
-@if /I "%buildllvmspirv%"=="y" IF EXIST %devroot%\llvm-project\llvm\projects\SPIRV-LLVM-Translator\spirv-headers-tag.conf IF EXIST %devroot%\llvm-project\llvm\projects\SPIRV-Headers IF %gitstate% GTR 0 (
-@cd %devroot%\llvm-project\llvm\projects\SPIRV-Headers
-@git checkout master
-@git pull -v --progress --recurse-submodules origin
-@for /f tokens^=* %%a IN ('type %devroot%\llvm-project\llvm\projects\SPIRV-LLVM-Translator\spirv-headers-tag.conf') do @git checkout %%a
-@echo.
-)
 @if /I "%buildllvmspirv%"=="y" IF NOT EXIST %devroot%\llvm-project\llvm\projects\SPIRV-LLVM-Translator (
 @git clone -b llvm_release_130 https://github.com/KhronosGroup/SPIRV-LLVM-Translator %devroot%\llvm-project\llvm\projects\SPIRV-LLVM-Translator
 @echo.
@@ -101,7 +104,7 @@
 @if %cmakestate%==1 set PATH=%devroot%\cmake\bin\;%PATH%
 
 @rem Construct build configuration command.
-@set buildconf=cmake ..
+@set buildconf=cmake ../..
 @if EXIST %devroot%\llvm-project set buildconf=%buildconf%/llvm
 @set buildconf=%buildconf% -G
 @if /I NOT "%ninja%"=="y" set buildconf=%buildconf% "Visual Studio %toolset%"
@@ -109,14 +112,14 @@
 @if %abi%==x64 if /I NOT "%ninja%"=="y" set buildconf=%buildconf% -A x64
 @if /I NOT "%ninja%"=="y" IF /I %PROCESSOR_ARCHITECTURE%==AMD64 set buildconf=%buildconf% -Thost=x64
 @if /I "%ninja%"=="y" set buildconf=%buildconf%Ninja
-@set buildconf=%buildconf% -DCMAKE_BUILD_TYPE=Release -DLLVM_USE_CRT_RELEASE=MT -DCMAKE_INSTALL_PREFIX="../../llvm/%abi%"
+@set buildconf=%buildconf% -DCMAKE_BUILD_TYPE=Release -DLLVM_USE_CRT_RELEASE=MT -DCMAKE_INSTALL_PREFIX="../../../llvm/build/%abi%"
 @if EXIST %devroot%\llvm-project IF /I NOT "%buildclang%"=="y" set buildconf=%buildconf% -DLLVM_ENABLE_PROJECTS=""
 @IF /I "%buildclang%"=="y" set buildconf=%buildconf% -DLLVM_ENABLE_PROJECTS="clang;lld"
 @set buildconf=%buildconf% -DLLVM_TARGETS_TO_BUILD=
 @IF /I "%amdgpu%"=="y" set buildconf=%buildconf%AMDGPU;
 @set buildconf=%buildconf%X86 -DLLVM_OPTIMIZED_TABLEGEN=TRUE -DLLVM_INCLUDE_UTILS=OFF -DLLVM_INCLUDE_RUNTIMES=OFF -DLLVM_INCLUDE_TESTS=OFF -DLLVM_INCLUDE_EXAMPLES=OFF -DLLVM_INCLUDE_GO_TESTS=OFF -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_BUILD_LLVM_C_DYLIB=OFF -DLLVM_ENABLE_DIA_SDK=OFF
 @if EXIST %devroot%\llvm-project IF /I NOT "%buildclang%"=="y" set buildconf=%buildconf% -DLLVM_BUILD_TOOLS=OFF
-@if EXIST %devroot%\llvm-project IF /I NOT "%buildclang%"=="y" IF NOT %abi%==x64 set buildconf=%buildconf% -DLLVM_INCLUDE_TOOLS=OFF
+@if EXIST %devroot%\llvm-project IF /I NOT "%buildclang%"=="y" IF NOT %abi%==%hostabi% set buildconf=%buildconf% -DLLVM_INCLUDE_TOOLS=OFF
 @IF /I "%buildclang%"=="y" set buildconf=%buildconf% -DCLANG_BUILD_TOOLS=ON
 @if /I "%buildllvmspirv%"=="y" set buildconf=%buildconf% -DLLVM_SPIRV_INCLUDE_TESTS=OFF
 @set buildconf=%buildconf% -DLLVM_ENABLE_RTTI=ON -DLLVM_ENABLE_TERMINFO=OFF
@@ -131,8 +134,10 @@
 @echo.
 @echo Cleanning LLVM build. Please wait...
 @echo.
-@if EXIST %devroot%\llvm\%abi% RD /S /Q %devroot%\llvm\%abi%
-@if EXIST buildsys-%abi% RD /S /Q buildsys-%abi%
+@if EXIST %devroot%\llvm\build\%abi% RD /S /Q %devroot%\llvm\build\%abi%
+@if EXIST build\buildsys-%abi% RD /S /Q build\buildsys-%abi%
+@if NOT EXIST "build\" MD build
+@cd build
 @md buildsys-%abi%
 @cd buildsys-%abi%
 @pause
@@ -140,8 +145,8 @@
 
 @rem Load Visual Studio environment. Can only be loaded in the background when using MsBuild.
 @if /I "%ninja%"=="y" call %vsenv% %vsabi%
-@if /I "%ninja%"=="y" if NOT EXIST %devroot%\llvm-project cd %devroot%\llvm\buildsys-%abi%
-@if /I "%ninja%"=="y" if EXIST %devroot%\llvm-project cd %devroot%\llvm-project\buildsys-%abi%
+@if /I "%ninja%"=="y" if NOT EXIST %devroot%\llvm-project cd %devroot%\llvm\build\buildsys-%abi%
+@if /I "%ninja%"=="y" if EXIST %devroot%\llvm-project cd %devroot%\llvm-project\build\buildsys-%abi%
 @if /I "%ninja%"=="y" echo.
 
 @rem Configure and execute the build with the configuration made above.
@@ -150,11 +155,11 @@
 @pause
 @echo.
 @if /I NOT "%ninja%"=="y" cmake --build . -j %throttle% --config Release --target install
-@if /I NOT "%ninja%"=="y" IF /I NOT "%buildclang%"=="y" IF %abi%==x64 cmake --build . -j %throttle% --config Release --target llvm-config
-@if /I NOT "%ninja%"=="y" IF /I NOT "%buildclang%"=="y" IF %abi%==x64 copy .\Release\bin\llvm-config.exe ..\..\llvm\%abi%\bin\
+@if /I NOT "%ninja%"=="y" IF /I NOT "%buildclang%"=="y" IF %abi%==%hostabi% cmake --build . -j %throttle% --config Release --target llvm-config
+@if /I NOT "%ninja%"=="y" IF /I NOT "%buildclang%"=="y" IF %abi%==%hostabi% copy .\Release\bin\llvm-config.exe ..\..\..\llvm\build\%abi%\bin\
 @if /I "%ninja%"=="y" ninja -j %throttle% install
-@if /I "%ninja%"=="y" IF /I NOT "%buildclang%"=="y" IF %abi%==x64 ninja -j %throttle% llvm-config
-@if /I "%ninja%"=="y" IF /I NOT "%buildclang%"=="y" IF %abi%==x64 copy .\bin\llvm-config.exe ..\..\llvm\%abi%\bin\
+@if /I "%ninja%"=="y" IF /I NOT "%buildclang%"=="y" IF %abi%==%hostabi% ninja -j %throttle% llvm-config
+@if /I "%ninja%"=="y" IF /I NOT "%buildclang%"=="y" IF %abi%==%hostabi% copy .\bin\llvm-config.exe ..\..\..\llvm\build\%abi%\bin\
 @echo.
 
 :skipllvm

buildscript/modules/mesa3d.cmd
@@ -130,17 +130,17 @@
 @IF %abi%==x64 set buildcmd=msbuild /p^:Configuration=release,Platform=x64 mesa.sln /m^:%throttle%
 
 @set havellvm=0
-@IF %toolchain%==msvc IF EXIST %devroot%\llvm\%abi% IF %cmakestate% GTR 0 set havellvm=1
+@IF %toolchain%==msvc IF EXIST %devroot%\llvm\build\%abi% IF %cmakestate% GTR 0 set havellvm=1
 @IF NOT %toolchain%==msvc set havellvm=1
 @set llvmless=n
 @if %havellvm%==0 set llvmless=y
-@if %havellvm%==1 set /p llvmless=Build Mesa without LLVM (y/n). llvmpipe and swr drivers and high performance JIT won't be available for other drivers and libraries:
+@if %havellvm%==1 set /p llvmless=Build Mesa without LLVM (y/n). llvmpipe, swr, RADV and all OpenCL drivers won't be available and high performance JIT won't be available for softpipe, osmesa and graw:
 @if %havellvm%==1 echo.
 @call %devroot%\%projectname%\buildscript\modules\mesonsubprojects.cmd
 @IF NOT %toolchain%==msvc set buildconf=%buildconf% --force-fallback-for=zlib,libzstd
 @if /I NOT "%llvmless%"=="y" IF %llvmconfigbusted% EQU 1 set buildconf=%buildconf%,llvm
 @if /I NOT "%llvmless%"=="y" set buildconf=%buildconf% -Dllvm=%mesonbooltrue% -Dshared-llvm=%mesonboolfalse%
-@if /I NOT "%llvmless%"=="y" IF %toolchain%==msvc set buildconf=%buildconf% --cmake-prefix-path=%devroot:\=/%/llvm/%abi%
+@if /I NOT "%llvmless%"=="y" IF %toolchain%==msvc set buildconf=%buildconf% --cmake-prefix-path=%devroot:\=/%/llvm/build/%abi%
 @if /I NOT "%llvmless%"=="y" IF %toolchain%==msvc IF %cmakestate% EQU 1 SET PATH=%devroot%\cmake\bin\;%PATH%
 @if /I "%llvmless%"=="y" set buildconf=%buildconf% -Dllvm=%mesonboolfalse%
 
@@ -216,7 +216,7 @@
 @set canradv=1
 @if /I "%llvmless%"=="y" set canradv=0
 @IF %intmesaver% LSS 21200 set canradv=0
-@IF %toolchain%==msvc IF NOT EXIST %devroot%\llvm\%abi%\lib\LLVMAMDGPU*.lib set canradv=0
+@IF %toolchain%==msvc IF NOT EXIST %devroot%\llvm\build\%abi%\lib\LLVMAMDGPU*.lib set canradv=0
 @IF %abi%==x86 IF %intmesaver% LSS 22000 set canradv=0
 @IF %toolchain%==msvc IF NOT EXIST %devroot%\mesa\subprojects\libelf-lfg-win32 IF %gitstate% EQU 0 set canradv=0
 @IF NOT %toolchain%==msvc IF %disableootpatch%==1 IF %intmesaver% LSS 21251 set canradv=0
@@ -286,14 +286,14 @@
 @IF %intmesaver% LSS 21000 set canopencl=0
 @IF NOT %toolchain%==msvc set canopencl=0
 @if /I "%llvmless%"=="y" set canopencl=0
-@IF NOT EXIST %devroot%\llvm\clc\share\pkgconfig set canopencl=0
+@IF NOT EXIST %devroot%\llvm\build\clc\share\pkgconfig set canopencl=0
 
 @rem OpenCL SPIR-V requirements: basic support + Clang, LLD, LLVM SPIRV translator and SPIRV tools
 @set canclspv=1
 @IF %canopencl% EQU 0 set canclspv=0
-@IF NOT EXIST %devroot%\llvm\%abi%\lib\clang*.lib set canclspv=0
-@IF NOT EXIST %devroot%\llvm\%abi%\lib\lld*.lib set canclspv=0
-@IF NOT EXIST %devroot%\llvm\%abi%\lib\pkgconfig set canclspv=0
+@IF NOT EXIST %devroot%\llvm\build\%abi%\lib\clang*.lib set canclspv=0
+@IF NOT EXIST %devroot%\llvm\build\%abi%\lib\lld*.lib set canclspv=0
+@IF NOT EXIST %devroot%\llvm\build\%abi%\lib\pkgconfig set canclspv=0
 @IF NOT EXIST %devroot%\spirv-tools\build\%abi%\lib\pkgconfig set canclspv=0
 
 @rem Clover requirements: basic support + Mesa 21.3, LLVM build with RTTI, gallium swrast and out of tree patches
@@ -332,8 +332,8 @@
 @IF /I "%buildclover%"=="y" set buildconf=%buildconf% -Dopencl-native=true
 
 @rem Apply PKG_CONFIG search PATH adjustments
-@IF %PKG_CONFIG_LIBCLC% EQU 1 set buildconf=%buildconf% -Dstatic-libclc=all --pkg-config-path=%devroot:\=/%/llvm/clc/share/pkgconfig
-@IF %PKG_CONFIG_SPV% EQU 1 set buildconf=%buildconf%;%devroot:\=/%/llvm/%abi%/lib/pkgconfig;%devroot:\=/%/spirv-tools/build/%abi%/lib/pkgconfig
+@IF %PKG_CONFIG_LIBCLC% EQU 1 set buildconf=%buildconf% -Dstatic-libclc=all --pkg-config-path=%devroot:\=/%/llvm/build/clc/share/pkgconfig
+@IF %PKG_CONFIG_SPV% EQU 1 set buildconf=%buildconf%;%devroot:\=/%/llvm/build/%abi%/lib/pkgconfig;%devroot:\=/%/spirv-tools/build/%abi%/lib/pkgconfig
 @set "PKG_CONFIG_LIBCLC="
 @set "PKG_CONFIG_SPV="
 

buildscript/modules/mesonsubprojects.cmd
@@ -12,7 +12,7 @@ cd /D %devroot%\mesa
 @rem Find LLVM dependency
 @set RTTI=false
 @set llvmconfigbusted=0
-@IF %toolchain%==msvc IF EXIST %devroot%\llvm\%abi%\lib\cmake\llvm\LLVMConfig.cmake FOR /F "tokens=* delims=" %%a IN ('type %devroot%\llvm\%abi%\lib\cmake\llvm\LLVMConfig.cmake') DO @IF "%%a"=="set(LLVM_ENABLE_RTTI ON)" SET RTTI=true
+@IF %toolchain%==msvc IF EXIST %devroot%\llvm\build\%abi%\lib\cmake\llvm\LLVMConfig.cmake FOR /F "tokens=* delims=" %%a IN ('type %devroot%\llvm\build\%abi%\lib\cmake\llvm\LLVMConfig.cmake') DO @IF "%%a"=="set(LLVM_ENABLE_RTTI ON)" SET RTTI=true
 @IF NOT %toolchain%==msvc IF EXIST %msysloc%\%LMSYSTEM%\lib\cmake\llvm\LLVMConfig.cmake FOR /F "tokens=* delims=" %%a IN ('type %msysloc%\%LMSYSTEM%\lib\cmake\llvm\LLVMConfig.cmake') DO @IF "%%a"=="set(LLVM_ENABLE_RTTI ON)" SET RTTI=true
 @IF NOT %toolchain%==msvc if /I NOT "%llvmless%"=="y" FOR /F "tokens=* USEBACKQ" %%a IN (`%msysloc%\usr\bin\bash --login -c "${MINGW_PREFIX}/bin/llvm-config --has-rtti" 2^>^&1`) DO @IF /I NOT "%%a"=="YES" IF /I NOT "%%a"=="NO" set llvmconfigbusted=1
 @IF %llvmconfigbusted% EQU 0 IF EXIST "%devroot%\mesa\subprojects\llvm\" RD /S /Q %devroot%\mesa\subprojects\llvm

buildscript/modules/ninjallvmbuild.cmd
@@ -5,9 +5,9 @@
 
 @rem Build LLVM libraries
 @set llvmlibstotal=0
-@FOR /F "tokens=4 USEBACKQ" %%a IN (`%devroot%\llvm\%abi%\bin\llvm-config.exe --link-static --libnames 2^>^&1`) DO @set /a llvmlibstotal+=1
+@FOR /F "tokens=4 USEBACKQ" %%a IN (`%devroot%\llvm\build\%abi%\bin\llvm-config.exe --link-static --libnames 2^>^&1`) DO @set /a llvmlibstotal+=1
 @set llvmlibscount=0
-@FOR /F "tokens=4 USEBACKQ" %%a IN (`%devroot%\llvm\%abi%\bin\llvm-config.exe --link-static --libnames 2^>^&1`) DO @(
+@FOR /F "tokens=4 USEBACKQ" %%a IN (`%devroot%\llvm\build\%abi%\bin\llvm-config.exe --link-static --libnames 2^>^&1`) DO @(
 set /a llvmlibscount+=1
 echo Building library %%~na - !llvmlibscount! of %llvmlibstotal%...
 ninja -j %throttle% install-%%~na

buildscript/modules/pkgconf.cmd
@@ -21,8 +21,7 @@
 @IF NOT EXIST pkgconf\pkg-config.exe set buildpkgconf=y
 @IF NOT EXIST pkgconf\pkg-config.exe echo Begin pkgconf build...
 @echo.
-@IF /I "%buildpkgconf%"=="y" IF /I %PROCESSOR_ARCHITECTURE%==AMD64 call %vsenv% x64
-@IF /I "%buildpkgconf%"=="y" IF /I %PROCESSOR_ARCHITECTURE%==x86 call %vsenv% x86
+@IF /I "%buildpkgconf%"=="y" call %vsenv% %hostabi%
 @IF /I "%buildpkgconf%"=="y" echo.
 @IF /I "%buildpkgconf%"=="y" IF EXIST pkgconf RD /S /Q pkgconf
 @set useninja=n

debug/llvm-config.cmd
@@ -2,8 +2,8 @@
 @set format=pythonlist
 @set llvmmodules=engine coroutines
 @for %%a in ("%cd%") do @set devroot=%%~sa
-@IF NOT EXIST llvm GOTO error
-@cd llvm
+@IF NOT EXIST llvm\build GOTO error
+@cd llvm\build
 @if NOT EXIST x64 if NOT EXIST x86 GOTO error
 @if EXIST x64 cd x64
 @if EXIST x86 cd x86

commit 8375f51ff8c780e9fea8dac64e19e21b5dc6ebde

Fix 7-zip version detection. Regression from 7b1e045e

buildscript/modules/envdump.cmd
@@ -29,7 +29,7 @@
 @rem Dump 7-Zip version and compression level
 @set sevenzipver=null
 @set exitloop=1
-@if EXIST "%ProgramFiles%\7-Zip\7z.exe" for /f "tokens=1-2 delims= " %%a IN ('"%ProgramFiles%\7-Zip\7z.exe"') DO @IF defined exitloop IF /I "%%"=="7-Zip" (
+@if EXIST "%ProgramFiles%\7-Zip\7z.exe" for /f "tokens=1-2 delims= " %%a IN ('"%ProgramFiles%\7-Zip\7z.exe"') DO @IF defined exitloop IF /I "%%a"=="7-Zip" (
 @set "exitloop="
 @set sevenzipver=%%b
 )