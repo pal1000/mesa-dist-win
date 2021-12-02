pal1000/mesa-dist-win

commit 5518bacf502b483d010c9f5f983e71f8dfeb6c5f

MSYS2: Use binary wrap to link LunarG Vulkan SDK

buildscript/modules/mesonsubprojects.cmd
@@ -58,6 +58,7 @@ echo irbuilder_h ^= files^(llvmloc + '/include/llvm/IR/IRBuilder.h'^)
 @IF NOT %toolchain%==msvc GOTO mingwwraps
 @IF EXIST "%devroot%\mesa\subprojects\zlib\" RD /S /Q %devroot%\mesa\subprojects\zlib
 @IF EXIST "%devroot%\mesa\subprojects\libzstd\" RD /S /Q %devroot%\mesa\subprojects\libzstd
+@IF EXIST "%devroot%\mesa\subprojects\vulkan\" RD /S /Q %devroot%\mesa\subprojects\vulkan
 
 @rem Use updated zlib wrap
 @CMD /C EXIT 0
@@ -133,6 +134,47 @@ echo meson.override_dependency^('libzstd', zstd_override^)
 @if NOT "%ERRORLEVEL%"=="0" (
 @echo Overriding ZSTD dependency...
 @copy /Y %devroot%\%projectname%\buildscript\mesonsubprojects\zstd-meson.build %devroot%\mesa\subprojects\libzstd\meson.build
+@echo.
+)
+
+@rem Vulkan dependency
+@IF "%vksdkselect%"=="2" (
+@set "VULKAN_SDK="
+@set "VK_SDK_PATH="
+@%msysloc%\usr\bin\bash --login -c "/usr/bin/pacman -S ${MINGW_PACKAGE_PREFIX}-vulkan-loader --noconfirm"
+@echo.
+@IF EXIST "%devroot%\mesa\subprojects\vulkan\" RD /S /Q %devroot%\mesa\subprojects\vulkan
+@GOTO donewrap
+)
+@IF NOT EXIST "%devroot%\mesa\subprojects\vulkan\" md %devroot%\mesa\subprojects\vulkan
+@(
+echo project^('vulkan', ['cpp']^)
+echo.
+echo cpp ^= meson.get_compiler^('cpp'^)
+echo.
+echo _deps ^= []
+echo _search ^= '%windir:\=/%/'
+if /I %PROCESSOR_ARCHITECTURE%==AMD64 IF %abi%==x86 echo _search ^+^= 'SysWOW64'
+if /I %PROCESSOR_ARCHITECTURE%==AMD64 IF %abi%==x64 echo _search ^+^= 'system32'
+if /I %PROCESSOR_ARCHITECTURE%==%abi% echo _search ^+^= 'system32'
+echo foreach d ^: ['vulkan-1']
+echo   _deps ^+^= cpp.find_library^(d, dirs ^: _search^)
+echo endforeach
+echo.
+echo dep_vk_override ^= declare_dependency^(
+IF EXIST "%VULKAN_SDK%" IF "%VULKAN_SDK%"=="%VK_SDK_PATH%" echo   include_directories ^: include_directories^('%VULKAN_SDK:\=/%/Include/vulkan'^),
+IF EXIST "%VULKAN_SDK%" IF NOT EXIST "%VK_SDK_PATH%" echo   include_directories ^: include_directories^('%VULKAN_SDK:\=/%/Include/vulkan'^),
+IF NOT EXIST "%VULKAN_SDK%" IF EXIST "%VK_SDK_PATH%" echo   include_directories ^: include_directories^('%VK_SDK_PATH:\=/%/Include/vulkan'^),
+echo   dependencies ^: _deps
+echo ^)
+echo.
+echo meson.override_dependency^('vulkan', dep_vk_override^)
+)>%devroot%\%projectname%\buildscript\mesonsubprojects\vulkan-meson.build
+@CMD /C EXIT 0
+@FC /B %devroot%\%projectname%\buildscript\mesonsubprojects\vulkan-meson.build %devroot%\mesa\subprojects\vulkan\meson.build>NUL 2>&1
+@if NOT "%ERRORLEVEL%"=="0" (
+@echo Using binary wrap to find Vulkan...
+@copy /Y %devroot%\%projectname%\buildscript\mesonsubprojects\vulkan-meson.build %devroot%\mesa\subprojects\vulkan\meson.build
 @echo.
 )
 

buildscript/modules/mesa3d.cmd
@@ -150,6 +150,7 @@
 @if %havellvm%==1 set /p llvmless=Build Mesa without LLVM (y/n). llvmpipe, swr, RADV, lavapipe and all OpenCL drivers won't be available and high performance JIT won't be available for softpipe, osmesa and graw:
 @if %havellvm%==1 echo.
 @call %devroot%\%projectname%\buildscript\modules\mesonsubprojects.cmd
+@IF "%vksdkselect%"=="1" IF NOT %toolchain%==msvc set buildconf=%buildconf%,vulkan
 @if /I NOT "%llvmless%"=="y" IF %llvmconfigbusted% EQU 1 set buildconf=%buildconf%,llvm
 @IF %intmesaver% GEQ 22000 set buildconf=%buildconf% -Dcpp_rtti=%RTTI%
 @if /I NOT "%llvmless%"=="y" set buildconf=%buildconf% -Dllvm=%mesonbooltrue% -Dshared-llvm=%mesonboolfalse%
@@ -182,7 +183,6 @@
 @set zink=n
 @set canzink=0
 @IF NOT %toolchain%==msvc IF %intmesaver% GEQ 21000 set canzink=1
-@IF %toolchain%==clang IF NOT EXIST %msysloc%\%LMSYSTEM%\bin\libvulkan-1.dll set canzink=0
 @IF %toolchain%==msvc IF %intmesaver% GEQ 21200 set canzink=1
 @IF %toolchain%==msvc IF %intmesaver% GEQ 21301 IF %intmesaver% LSS 21303 IF %abi%==x86 IF %disableootpatch%==1 set canzink=0
 @IF %toolchain%==msvc IF NOT EXIST "%VK_SDK_PATH%" IF NOT EXIST "%VULKAN_SDK%" set canzink=0

buildscript/modules/vulkan.cmd
@@ -15,15 +15,4 @@
 @GOTO selectvksdk
 )
 @IF %vksdkstd% EQU 0 set vksdkselect=2
-@IF "%vksdkselect%"=="1" (
-@IF EXIST %msysloc%\%LMSYSTEM%\lib\libvulkan.dll.a del %msysloc%\%LMSYSTEM%\lib\libvulkan.dll.a
-@IF EXIST %msysloc%\%LMSYSTEM%\lib\pkgconfig\vulkan.pc del %msysloc%\%LMSYSTEM%\lib\pkgconfig\vulkan.pc
-@IF EXIST %msysloc%\%LMSYSTEM%\bin\libvulkan-1.dll del %msysloc%\%LMSYSTEM%\bin\libvulkan-1.dll
-)
-@IF "%vksdkselect%"=="2" (
-@set "VULKAN_SDK="
-@set "VK_SDK_PATH="
-@%msysloc%\usr\bin\bash --login -c "/usr/bin/pacman -S ${MINGW_PACKAGE_PREFIX}-vulkan-loader --noconfirm"
-@echo.
-)
-@endlocal&set "VK_SDK_PATH=%VK_SDK_PATH%"&set "VULKAN_SDK=%VULKAN_SDK%"
+@endlocal&set vksdkselect=%vksdkselect%

commit 222ec156773089f4dca624a794ab41553d0c5b06

Fix another regression from 5518bacf

buildscript/modules/mesonsubprojects.cmd
@@ -178,4 +178,4 @@ echo meson.override_dependency^('vulkan', dep_vk_override^)
 @IF %toolchain%==gcc IF EXIST %msysloc%\%LMSYSTEM%\bin\libvulkan-1.dll del %msysloc%\%LMSYSTEM%\bin\libvulkan-1.dll
 
 :donewrap
-@endlocal&set RTTI=%RTTI%&set llvmconfigbusted=%llvmconfigbusted%
+@endlocal&set RTTI=%RTTI%&set llvmconfigbusted=%llvmconfigbusted%&set "VK_SDK_PATH=%VK_SDK_PATH%"&set "VULKAN_SDK=%VULKAN_SDK%"