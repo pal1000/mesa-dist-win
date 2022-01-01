e63f156c5aeb067bda03dfed7841350f1f109bb8
pal1000 <liviuprodea@yahoo.com>
2022-01-01 13:24:35 +0200

    Revert 5aac0ca4 as this doesn't actually work as expected

--- buildscript/modules/msysupdate.cmd
+++ buildscript/modules/msysupdate.cmd
@@ -2,7 +2,7 @@
 @rem Check for updates
 @rem Initial upgrade
 @IF %msysstate% GTR 0 IF NOT EXIST %msysloc%\usr\bin\bison.exe (
-@%msysloc%\usr\bin\bash --login -c "/usr/bin/pacman -Syuu --disable-download-timeout"
+@%msysloc%\usr\bin\bash --login -c "/usr/bin/pacman -Syu --noconfirm --disable-download-timeout"
 @echo.
 @%msysloc%\usr\bin\bash --login -c "/usr/bin/pacman -Syu --noconfirm --disable-download-timeout"
 @echo.
@@ -10,7 +10,7 @@
 
 @IF EXIST %msysloc%\usr\bin\bison.exe set /p msysupdate=Update MSYS2 packages (y/n):
 @IF EXIST %msysloc%\usr\bin\bison.exe echo.
-@IF /I "%msysupdate%"=="y" %msysloc%\usr\bin\bash --login -c "/usr/bin/pacman -Syuu --disable-download-timeout"
+@IF /I "%msysupdate%"=="y" %msysloc%\usr\bin\bash --login -c "/usr/bin/pacman -Syu --noconfirm --disable-download-timeout"
 @IF /I "%msysupdate%"=="y" echo.
 @IF /I "%msysupdate%"=="y" %msysloc%\usr\bin\bash --login -c "/usr/bin/pacman -Syu --noconfirm --disable-download-timeout"
 @IF /I "%msysupdate%"=="y" echo.
