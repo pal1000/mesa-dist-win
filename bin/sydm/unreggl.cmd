48f1170df60a66f170b0577bae898b53b9a26343
pal1000 <liviuprodea@yahoo.com>
2022-03-10 12:31:01 +0200

    System wide deployment: Supress REG QUERY output

--- bin/sydm/unreggl.cmd
+++ bin/sydm/unreggl.cmd
@@ -1,8 +1,8 @@
 @set "ERRORLEVEL="
 @CMD /C EXIT 0
-@REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\OpenGLDrivers\MSOGL" /d /f %1.dll
+@REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\OpenGLDrivers\MSOGL" /d /f %1.dll >nul 2>&1
 @if "%ERRORLEVEL%"=="0" REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\OpenGLDrivers\MSOGL" /f
 @CMD /C EXIT 0
-@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows NT\CurrentVersion\OpenGLDrivers\MSOGL" /d /f %1.dll
+@IF /I %PROCESSOR_ARCHITECTURE%==AMD64 REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows NT\CurrentVersion\OpenGLDrivers\MSOGL" /d /f %1.dll >nul 2>&1
 @if "%ERRORLEVEL%"=="0" IF /I %PROCESSOR_ARCHITECTURE%==AMD64 REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows NT\CurrentVersion\OpenGLDrivers\MSOGL" /f
 @CMD /C EXIT 0
