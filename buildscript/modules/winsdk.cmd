3f0f1312ecfe190eea9e8742f40ac78abdb8a86e
pal1000 <liviuprodea@yahoo.com>
2022-03-17 12:59:11 +0200

    Misc: Always use for /R when possible

--- buildscript/modules/winsdk.cmd
+++ buildscript/modules/winsdk.cmd
@@ -2,7 +2,7 @@
 @set winsdk=null
 @IF /I NOT %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%ProgramFiles%\Windows Kits\10\Redist" set winsdk=%ProgramFiles%\Windows Kits\10
 @IF /I %PROCESSOR_ARCHITECTURE%==AMD64 IF EXIST "%ProgramFiles% (x86)\Windows Kits\10\Redist" set winsdk=%ProgramFiles% (x86)\Windows Kits\10
-@IF NOT "%winsdk%"=="null" IF "%1"=="dxil" for /f "tokens=* delims=" %%a in ('dir /b /s "%winsdk%\bin"') do @if "%%~nxa"=="dxil.dll" (
+@IF NOT "%winsdk%"=="null" IF "%1"=="dxil" for /R "%winsdk%\bin" %%a in (dxil.dl?) do @if "%%~nxa"=="dxil.dll" (
 @set dxilloc=%%~dpa
 @IF "!dxilloc:~-1!"=="\" set dxilloc=!dxilloc:~0,-1!
 @IF !dxilloc:~-3!==%abi% echo "!dxilloc!\dxil.dll"
