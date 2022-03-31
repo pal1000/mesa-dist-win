pal1000/mesa-dist-win

commit e0c14cffd7bec7c79d12e8220558fafd2b1000c8

Fix regression from last commit If a MSYS2 command ends with a digit and only a digit CMD parser confuses it with 0>, 1> or 2>... instead of outputing to shell script

buildscript/modules/mesa3d.cmd
@@ -178,9 +178,9 @@
 @if /I "%useninja%"=="y" if %ninjastate%==1 IF %toolchain%==msvc set PATH=%devroot%\ninja\;%PATH%
 @if /I "%useninja%"=="y" set buildconf=%buildconf% --backend=ninja
 @if /I "%useninja%"=="y" IF %toolchain%==msvc set buildcmd=ninja -C %devroot:\=/%/mesa/build/%toolchain%-%abi% -j %throttle% -k 0
-@IF NOT %toolchain%==msvc set buildcmd=%runmsys% 
+@IF NOT %toolchain%==msvc set buildcmd=%runmsys% cd $(/usr/bin/cygpath -m ${devroot})/mesa/build/${toolchain}-${abi};
 @IF NOT %toolchain%==msvc IF %gitstate% GTR 0 set buildcmd=%buildcmd%PATH=${PATH}:${gitloc};
-@IF NOT %toolchain%==msvc set buildcmd=%buildcmd%${MINGW_PREFIX}/bin/ninja -C $(/usr/bin/cygpath -m ${devroot})/mesa/build/%toolchain%-${abi} -j ${throttle} -k 0
+@IF NOT %toolchain%==msvc set buildcmd=%buildcmd%${MINGW_PREFIX}/bin/ninja -k 0
 @if /I NOT "%useninja%"=="y" set buildconf=%buildconf% --backend=vs
 
 @set galliumcount=0

buildscript/modules/runmsys.cmd
@@ -1,2 +1,2 @@
-@echo %*>%devroot%\%projectname%\buildscript\assets\temp.sh
+@echo %*  >%devroot%\%projectname%\buildscript\assets\temp.sh
 @%msysloc%\usr\bin\bash -l "%devroot%\%projectname%\buildscript\assets\temp.sh"

commit 88cfe15dfd8beeb72a5f44d77b0f37de1a3bd758

MSYS2: Restore build throttling support. Regression from e0c14cffd7bec7c79d12e8220558fafd2b1000c8

buildscript/modules/mesa3d.cmd
@@ -180,7 +180,7 @@
 @if /I "%useninja%"=="y" IF %toolchain%==msvc set buildcmd=ninja -C "%devroot%\mesa\build\%toolchain%-%abi%" -j %throttle% -k 0
 @IF NOT %toolchain%==msvc set buildcmd=%runmsys% cd "%devroot%\mesa\build\%toolchain%-%abi%";
 @IF NOT %toolchain%==msvc IF %gitstate% GTR 0 set buildcmd=%buildcmd%PATH=${PATH}:${gitloc};
-@IF NOT %toolchain%==msvc set buildcmd=%buildcmd%/%LMSYSTEM%/bin/ninja -k 0
+@IF NOT %toolchain%==msvc set buildcmd=%buildcmd%/%LMSYSTEM%/bin/ninja -j %throttle% -k 0
 @if /I NOT "%useninja%"=="y" set buildconf=%buildconf% --backend=vs
 
 @set galliumcount=0