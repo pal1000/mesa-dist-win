916f5b340e4caf88d73d0bada87b058d8b8637e0
pal1000 <liviuprodea@yahoo.com>
2022-07-17 19:42:47 +0300

    Mesa3D incremental build: Support LLVM discovery method change

--- buildscript/modules/mesa3d.cmd
+++ buildscript/modules/mesa3d.cmd
@@ -175,7 +175,8 @@
 @if /I NOT "%llvmless%"=="y" set buildconf=%buildconf% -Dllvm=%mesonbooltrue% -Dshared-llvm=%mesonboolfalse%
 @if /I NOT "%llvmless%"=="y" IF %llvmmethod%==cmake set buildconf=%buildconf% --cmake-prefix-path="%devroot:\=/%/llvm/build/%abi%"
 @if /I NOT "%llvmless%"=="y" IF %llvmmethod%==cmake IF %cmakestate% EQU 1 SET PATH=%devroot%\cmake\bin\;%PATH%
-@if /I NOT "%llvmless%"=="y" IF %toolchain%==msvc IF NOT %llvmmethod%==cmake SET PATH=%devroot%\llvm\build\%abi%\llvmconfig\;%PATH%
+@if /I NOT "%llvmless%"=="y" IF NOT %llvmmethod%==cmake set buildconf=%buildconf% --cmake-prefix-path=
+@if /I NOT "%llvmless%"=="y" IF NOT %llvmmethod%==cmake IF %toolchain%==msvc SET PATH=%devroot%\llvm\build\%abi%\llvmconfig\;%PATH%
 @if /I "%llvmless%"=="y" set buildconf=%buildconf% -Dllvm=%mesonboolfalse%
 
 @set useninja=n
--- releasenotes.md
+++ releasenotes.md
@@ -1,3 +1,5 @@
+### Build script
+- Mesa3D incremental build: Support LLVM discovery method change.
 # 22.1.4
 - Updated Mesa3D to [22.1.4](https://docs.mesa3d.org/relnotes/22.1.4.html).
 ### Build script
