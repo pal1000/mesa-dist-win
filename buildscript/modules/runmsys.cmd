4a91924059a3217d0f29ed77d6c5a1196ada7000
pal1000 <liviuprodea@yahoo.com>
2022-08-07 13:51:30 +0300

    MSYS2 git config import: Use include directive

--- buildscript/modules/runmsys.cmd
+++ buildscript/modules/runmsys.cmd
@@ -1,4 +1,8 @@
+@IF %gitstate% GTR 0 for /f tokens^=2^ delims^=^\^ eol^= %%a IN ('whoami') DO @IF NOT EXIST "%msysloc%\home\%%a\.gitconfig" (
+@echo [include]
+@echo path ^= %USERPROFILE:\=\\%\\.gitconfig
+)>"%msysloc%\home\%%a\.gitconfig"
 @set CD=
-@IF %gitstate% GTR 0 echo PATH^=${PATH}^:${gitloc};cp -f ${USERPROFILE}/.gitconfig ~;cd "%CD%";%*  >"%devroot%\%projectname%\buildscript\assets\temp.sh"
+@IF %gitstate% GTR 0 echo PATH^=${PATH}^:${gitloc};cd "%CD%";%*  >"%devroot%\%projectname%\buildscript\assets\temp.sh"
 @IF %gitstate% EQU 0 echo cd "%CD%";%*  >"%devroot%\%projectname%\buildscript\assets\temp.sh"
 @"%msysloc%\usr\bin\bash.exe" -l "%devroot%\%projectname%\buildscript\assets\temp.sh"
