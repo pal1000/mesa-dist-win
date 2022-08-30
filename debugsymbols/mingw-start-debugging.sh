0f43147485610b7b4f1780d3ad8f50875a923a19
pal1000 <liviuprodea@yahoo.com>
2022-08-31 01:20:02 +0300

    MinGW: Minor fix to start debugging script

--- debugsymbols/mingw-start-debugging.sh
+++ debugsymbols/mingw-start-debugging.sh
@@ -13,8 +13,9 @@ pacman -S ${MINGW_PACKAGE_PREFIX}-gdb --needed --noconfirm
 # - Run this shell script in MSYS2 by copy-pasting full path to it there
 # - Once the faulting module is known add back the symbol file to GDB command bellow then re-run this shell script
 # The symbol file name has to match faulting module name with extra .sym extension
-cd "c:\Software\systools\GPU/GPU_Caps_Viewer";gdb -ex 'symbol-file c:/Software/Development/projects/mesa-dist-win/debugsymbols/x86/clon12compiler.dll.sym' -ex 'target exec GPU_Caps_Viewer.exe' -ex 'run' -ex 'q'
+cd "c:\Software\systools\GPU/GPU_Caps_Viewer";gdb -ex 'symbol-file c:/Software/Development/projects/mesa-dist-win/debugsymbols/x86/clon12compiler.dll.sym' -ex 'target exec GPU_Caps_Viewer.exe' -ex 'run'
 # - Once program crashes collect backtrace by entering `bt` at GDB shell then type `continue`
 # - Repeat last step until the crashing program is closed
 # You can do more complex debugging if you are an advanced GDB user
 # Copy-paste all backtraces in an issue report at Mesa3D: https://gitlab.freedesktop.org/mesa/mesa/-/issues
+# To quit GDB enter `q`
