9f0a5e822870f3664f46b6e95d78994a75fe2b03
pal1000 <liviuprodea@yahoo.com>
2022-08-30 17:23:30 +0300

    MinGW: Don't try getting debug symbols from Microsoft binary blobs and MSVC only binaries

--- buildscript/modules/collectdebugsymbols.cmd
+++ buildscript/modules/collectdebugsymbols.cmd
@@ -1,11 +1,11 @@
 @echo Copying debug information...
 @IF %toolchain%==msvc ROBOCOPY "%devroot%\mesa\build\%toolchain%-%abi%" "%devroot%\%projectname%\debugsymbols\%abi%" *.pdb /E
-@IF NOT %toolchain%==msvc IF EXIST "%msysloc%\%LMSYSTEM%\bin\strip.exe" for /f delims^=^ eol^= %%a in ('dir /b "%devroot%\%projectname%\bin\%abi%\*.dll" 2^>^&1') do @IF EXIST "%devroot%\%projectname%\bin\%abi%\%%~nxa"(
+@IF NOT %toolchain%==msvc IF EXIST "%msysloc%\%LMSYSTEM%\bin\strip.exe" for /f delims^=^ eol^= %%a in ('dir /b "%devroot%\%projectname%\bin\%abi%\*.dll" 2^>^&1') do @IF EXIST "%devroot%\%projectname%\bin\%abi%\%%~nxa" IF /I NOT "%%~na"=="dxil" IF /I NOT "%%~na"=="openclon12" IF /I NOT "%%~na"=="WinPixEventRuntime" (
 @copy "%devroot%\%projectname%\bin\%abi%\%%~nxa" "%devroot%\%projectname%\debugsymbols\%abi%\%%~nxa.sym"
 @"%msysloc%\%LMSYSTEM%\bin\strip.exe" --only-keep-debug "%devroot%\%projectname%\debugsymbols\%abi%\%%~nxa.sym"
 @"%msysloc%\%LMSYSTEM%\bin\strip.exe" --strip-debug --strip-unneeded "%devroot%\%projectname%\bin\%abi%\%%~nxa"
 )
-@IF NOT %toolchain%==msvc IF EXIST "%msysloc%\%LMSYSTEM%\bin\strip.exe" for /f delims^=^ eol^= %%a in ('dir /b "%devroot%\%projectname%\bin\%abi%\*.exe" 2^>^&1') do @IF EXIST "%devroot%\%projectname%\bin\%abi%\%%~nxa"(
+@IF NOT %toolchain%==msvc IF EXIST "%msysloc%\%LMSYSTEM%\bin\strip.exe" for /f delims^=^ eol^= %%a in ('dir /b "%devroot%\%projectname%\bin\%abi%\*.exe" 2^>^&1') do @IF EXIST "%devroot%\%projectname%\bin\%abi%\%%~nxa" (
 @copy "%devroot%\%projectname%\bin\%abi%\%%~nxa" "%devroot%\%projectname%\debugsymbols\%abi%\%%~nxa.sym"
 @"%msysloc%\%LMSYSTEM%\bin\strip.exe" --only-keep-debug "%devroot%\%projectname%\debugsymbols\%abi%\%%~nxa.sym"
 @"%msysloc%\%LMSYSTEM%\bin\strip.exe" --strip-debug --strip-unneeded "%devroot%\%projectname%\bin\%abi%\%%~nxa"
