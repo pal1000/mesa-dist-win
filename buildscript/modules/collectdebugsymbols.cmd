a12e10f3d339434bb52a2f573004b06ab1ef14ca
pal1000 <liviuprodea@yahoo.com>
2022-08-30 19:12:15 +0300

    MinGW: Speed up debug symbols extraction

--- buildscript/modules/collectdebugsymbols.cmd
+++ buildscript/modules/collectdebugsymbols.cmd
@@ -1,13 +1,7 @@
 @echo Copying debug information...
 @IF %toolchain%==msvc ROBOCOPY "%devroot%\mesa\build\%toolchain%-%abi%" "%devroot%\%projectname%\debugsymbols\%abi%" *.pdb /E
-@IF NOT %toolchain%==msvc IF EXIST "%msysloc%\%LMSYSTEM%\bin\strip.exe" for /f delims^=^ eol^= %%a in ('dir /b "%devroot%\%projectname%\bin\%abi%\*.dll" 2^>^&1') do @IF EXIST "%devroot%\%projectname%\bin\%abi%\%%~nxa" IF /I NOT "%%~na"=="dxil" IF /I NOT "%%~na"=="openclon12" IF /I NOT "%%~na"=="WinPixEventRuntime" (
-@copy "%devroot%\%projectname%\bin\%abi%\%%~nxa" "%devroot%\%projectname%\debugsymbols\%abi%\%%~nxa.sym"
-@"%msysloc%\%LMSYSTEM%\bin\strip.exe" --only-keep-debug "%devroot%\%projectname%\debugsymbols\%abi%\%%~nxa.sym"
-@"%msysloc%\%LMSYSTEM%\bin\strip.exe" --strip-debug --strip-unneeded "%devroot%\%projectname%\bin\%abi%\%%~nxa"
-)
-@IF NOT %toolchain%==msvc IF EXIST "%msysloc%\%LMSYSTEM%\bin\strip.exe" for /f delims^=^ eol^= %%a in ('dir /b "%devroot%\%projectname%\bin\%abi%\*.exe" 2^>^&1') do @IF EXIST "%devroot%\%projectname%\bin\%abi%\%%~nxa" (
-@copy "%devroot%\%projectname%\bin\%abi%\%%~nxa" "%devroot%\%projectname%\debugsymbols\%abi%\%%~nxa.sym"
-@"%msysloc%\%LMSYSTEM%\bin\strip.exe" --only-keep-debug "%devroot%\%projectname%\debugsymbols\%abi%\%%~nxa.sym"
+@IF NOT %toolchain%==msvc IF EXIST "%msysloc%\%LMSYSTEM%\bin\strip.exe" IF EXIST "%msysloc%\%LMSYSTEM%\bin\objcopy.exe" for /f delims^=^ eol^= %%a in ('dir /b "%devroot%\%projectname%\bin\%abi%\*.*" 2^>^&1') do @IF EXIST "%devroot%\%projectname%\bin\%abi%\%%~nxa" IF /I NOT "%%~na"=="dxil" IF /I NOT "%%~na"=="openclon12" IF /I NOT "%%~na"=="WinPixEventRuntime" IF /I NOT "%%~xa"=="json" (
+@"%msysloc%\%LMSYSTEM%\bin\objcopy.exe" --only-keep-debug "%devroot%\%projectname%\bin\%abi%\%%~nxa" "%devroot%\%projectname%\debugsymbols\%abi%\%%~nxa.sym"
 @"%msysloc%\%LMSYSTEM%\bin\strip.exe" --strip-debug --strip-unneeded "%devroot%\%projectname%\bin\%abi%\%%~nxa"
 )
 @echo.
