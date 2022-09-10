pal1000/mesa-dist-win

commit 548a04f4a5856cc8f550355de6d25d0790aa2366

LLVM SPIRV translator: Build out of tree and allow build without clang

buildscript/modules/llvm.cmd
@@ -51,9 +51,7 @@
 @if %cmakestate%==1 set PATH=%devroot%\cmake\bin\;%PATH%
 
 @rem Construct build configuration command.
-@set buildconf=cmake "%devroot%\llvm
-@if EXIST "%devroot%\llvm-project\" set buildconf=%buildconf%-project\llvm
-@set buildconf=%buildconf%" -G
+@set buildconf=cmake -G
 @if /I NOT "%useninja%"=="y" set buildconf=%buildconf% "Visual Studio %toolset%"
 @if %abi%==x86 if /I NOT "%useninja%"=="y" set buildconf=%buildconf% -A Win32
 @if %abi%==x64 if /I NOT "%useninja%"=="y" set buildconf=%buildconf% -A x64
@@ -69,7 +67,8 @@
 @IF /I "%buildclang%"=="y" set buildconf=%buildconf% -DCLANG_BUILD_TOOLS=ON
 @set buildconf=%buildconf% -DLLVM_ENABLE_RTTI=ON -DLLVM_ENABLE_TERMINFO=OFF
 
-@echo LLVM build configuration command^: %buildconf% -DCMAKE_INSTALL_PREFIX="%devroot%\llvm\build\%abi%"
+@if NOT EXIST "%devroot%\llvm-project\" echo LLVM build configuration command^: %buildconf% -DCMAKE_INSTALL_PREFIX="%devroot%\llvm\build\%abi%" "%devroot%\llvm"
+@if EXIST "%devroot%\llvm-project\" echo LLVM build configuration command^: %buildconf% -DCMAKE_INSTALL_PREFIX="%devroot%\llvm\build\%abi%" "%devroot%\llvm-project\llvm"
 @echo.
 
 @rem Ask to do LLVM build
@@ -77,11 +76,9 @@
 @echo.
 @IF /I NOT "%buildllvm%"=="y" GOTO skipllvm
 
-@rem Always clean build and remove LLVM SPIRV translator
+@rem Always clean build
 @echo Cleanning LLVM build. Please wait...
 @echo.
-@if EXIST "%devroot%\llvm-project\llvm\projects\SPIRV-LLVM-Translator\" RD /S /Q "%devroot%\llvm-project\llvm\projects\SPIRV-LLVM-Translator"
-@if EXIST "%devroot%\llvm-project\llvm\projects\SPIRV-Headers\" RD /S /Q "%devroot%\llvm-project\llvm\projects\SPIRV-Headers"
 @if EXIST "%devroot%\llvm\build\%abi%\" RD /S /Q "%devroot%\llvm\build\%abi%"
 @if NOT EXIST "%devroot%\llvm-project\" cd "%devroot%\llvm"
 @if EXIST "%devroot%\llvm-project\" cd "%devroot%\llvm-project"
@@ -100,7 +97,8 @@
 @if /I "%useninja%"=="y" echo.
 
 @rem Configure and execute the build with the configuration made above.
-@%buildconf% -DCMAKE_INSTALL_PREFIX="%devroot%\llvm\build\%abi%"
+@if NOT EXIST "%devroot%\llvm-project\" %buildconf% -DCMAKE_INSTALL_PREFIX="%devroot%\llvm\build\%abi%" "%devroot%\llvm"
+@if EXIST "%devroot%\llvm-project\" %buildconf% -DCMAKE_INSTALL_PREFIX="%devroot%\llvm\build\%abi%" "%devroot%\llvm-project\llvm"
 @echo.
 @pause
 @echo.
@@ -123,7 +121,7 @@
 @IF %llvmsources% EQU 0 echo.
 @IF %llvmsources% EQU 1 IF %cmakestate% EQU 0 echo.
 @IF %llvmsources% EQU 1 IF %cmakestate% GTR 0 IF NOT EXIST "%devroot%\llvm\build\%abi%\lib\" echo.
-@IF /I "%buildclang%"=="y" call "%devroot%\%projectname%\buildscript\modules\llvmspv.cmd"
+@IF EXIST "%devroot%\llvm\build\%abi%\lib\" call "%devroot%\%projectname%\buildscript\modules\llvmspv.cmd"
 
 @rem Reset environment after LLVM build.
 @endlocal

buildscript/modules/llvmspv.cmd
@@ -1,15 +1,15 @@
 @setlocal
 
 @rem Updating LLVM SPIRV translator source code
-@IF EXIST "%devroot%\llvm-project\llvm\projects\SPIRV-LLVM-Translator\" IF %gitstate% GTR 0 (
-@cd "%devroot%\llvm-project\llvm\projects\SPIRV-LLVM-Translator"
+@IF EXIST "%devroot%\SPIRV-LLVM-Translator\" IF %gitstate% GTR 0 (
+@cd "%devroot%\SPIRV-LLVM-Translator"
 @echo Updating LLVM SPIRV translator...
 @git pull --progress --tags --recurse-submodules origin
 @echo.
 )
-@IF EXIST "%devroot%\llvm-project\llvm\projects\SPIRV-LLVM-Translator\spirv-headers-tag.conf" IF EXIST "%devroot%\llvm-project\llvm\projects\SPIRV-Headers\" IF %gitstate% GTR 0 for /f delims^=^ eol^= %%a IN ('type "%devroot%\llvm-project\llvm\projects\SPIRV-LLVM-Translator\spirv-headers-tag.conf"') do @for /f delims^=^ eol^= %%b IN ('type "%devroot%\llvm-project\llvm\projects\SPIRV-Headers\.git\HEAD"') do @IF NOT %%a==%%b (
+@IF EXIST "%devroot%\SPIRV-LLVM-Translator\spirv-headers-tag.conf" IF EXIST "%devroot%\SPIRV-Headers\" IF %gitstate% GTR 0 for /f delims^=^ eol^= %%a IN ('type "%devroot%\SPIRV-LLVM-Translator\spirv-headers-tag.conf"') do @for /f delims^=^ eol^= %%b IN ('type "%devroot%\SPIRV-Headers\.git\HEAD"') do @IF NOT %%a==%%b (
 @echo Updating SPIRV headers used by LLVM SPIRV translator...
-@cd "%devroot%\llvm-project\llvm\projects\SPIRV-Headers"
+@cd "%devroot%\SPIRV-Headers"
 @for /f tokens^=2^ delims^=/^ eol^= %%c in ('git symbolic-ref --short refs/remotes/origin/HEAD 2^>^&^1') do @git checkout %%c
 @git pull --progress --tags --recurse-submodules origin
 @git checkout %%a
@@ -17,28 +17,28 @@
 )
 
 @set canllvmspirv=1
-@if NOT EXIST "%devroot%\llvm-project\llvm\projects\" set canllvmspirv=0
+@if NOT EXIST "%devroot%\llvm-project\" set canllvmspirv=0
 @IF %cmakestate%==0 set canllvmspirv=0
-@IF NOT EXIST "%devroot%\llvm-project\llvm\projects\SPIRV-LLVM-Translator\" IF %gitstate% EQU 0 set canllvmspirv=0
-@IF EXIST "%devroot%\llvm-project\llvm\projects\SPIRV-LLVM-Translator\spirv-headers-tag.conf" IF NOT EXIST "%devroot%\llvm-project\llvm\projects\SPIRV-Headers\" IF %gitstate% EQU 0 set canllvmspirv=0
+@IF NOT EXIST "%devroot%\SPIRV-LLVM-Translator\" IF %gitstate% EQU 0 set canllvmspirv=0
+@IF EXIST "%devroot%\SPIRV-LLVM-Translator\spirv-headers-tag.conf" IF NOT EXIST "%devroot%\SPIRV-Headers\" IF %gitstate% EQU 0 set canllvmspirv=0
 @IF %canllvmspirv% EQU 1 set /p buildllvmspirv=Build SPIRV LLVM Translator - required for OpenCL (y/n):
 @IF %canllvmspirv% EQU 1 echo.
 @if /I NOT "%buildllvmspirv%"=="y" GOTO skipspvllvm
 
-@IF NOT EXIST "%devroot%\llvm-project\llvm\projects\SPIRV-LLVM-Translator\" (
+@IF NOT EXIST "%devroot%\SPIRV-LLVM-Translator\" (
 @echo Getting LLVM SPIRV translator source code...
-@git clone -b llvm_release_140 https://github.com/KhronosGroup/SPIRV-LLVM-Translator "%devroot%\llvm-project\llvm\projects\SPIRV-LLVM-Translator"
+@git clone -b llvm_release_140 https://github.com/KhronosGroup/SPIRV-LLVM-Translator "%devroot%\SPIRV-LLVM-Translator"
 @echo.
 )
-@IF EXIST "%devroot%\llvm-project\llvm\projects\SPIRV-LLVM-Translator\spirv-headers-tag.conf" IF NOT EXIST "%devroot%\llvm-project\llvm\projects\SPIRV-Headers\" (
+@IF EXIST "%devroot%\SPIRV-LLVM-Translator\spirv-headers-tag.conf" IF NOT EXIST "%devroot%\SPIRV-Headers\" (
 @echo Getting source code of SPIRV headers used by LLVM SPIRV translator...
-@git clone https://github.com/KhronosGroup/SPIRV-Headers "%devroot%\llvm-project\llvm\projects\SPIRV-Headers"
-@cd "%devroot%\llvm-project\llvm\projects\SPIRV-Headers"
-@for /f delims^=^ eol^= %%a IN ('type "%devroot%\llvm-project\llvm\projects\SPIRV-LLVM-Translator\spirv-headers-tag.conf"') do @git checkout %%a
+@git clone https://github.com/KhronosGroup/SPIRV-Headers "%devroot%\SPIRV-Headers"
+@cd "%devroot%\SPIRV-Headers"
+@for /f delims^=^ eol^= %%a IN ('type "%devroot%\SPIRV-LLVM-Translator\spirv-headers-tag.conf"') do @git checkout %%a
 @echo.
 )
 
-@set buildconf=%buildconf% -DCMAKE_INSTALL_PREFIX="%devroot%\llvm\build\spv-%abi%" -DLLVM_SPIRV_INCLUDE_TESTS=OFF
+@set buildconf=%buildconf% -DLLVM_EXTERNAL_SPIRV_HEADERS_SOURCE_DIR="%devroot%\SPIRV-Headers" -DCMAKE_INSTALL_PREFIX="%devroot%\llvm\build\spv-%abi%" -DCMAKE_PREFIX_PATH="%devroot%\llvm\build\%abi%" -DLLVM_SPIRV_INCLUDE_TESTS=OFF "%devroot%\SPIRV-LLVM-Translator"
 
 @rem SPIRV Tools integration for LLVM SPIRV translator. This is a feature introduced in LLVM SPIRV translator 14.x.
 @rem IF %canllvmspirv% EQU 1 IF EXIST "%devroot%\spirv-tools\build\%abi%\" IF %pkgconfigstate% GTR 0 set /p integratespvtools=Build with SPIRV Tools integration (y/n):
@@ -46,11 +46,6 @@
 @IF /I "%integratespvtools%"=="y" set PATH=%pkgconfigloc%\;%PATH%
 @IF /I "%integratespvtools%"=="y" set PKG_CONFIG_PATH=%devroot:\=/%/spirv-tools/build/%abi%/lib/pkgconfig
 @IF /I NOT "%integratespvtools%"=="y" set PKG_CONFIG_PATH=
-
-@rem Speedup build configuration
-@if EXIST "%devroot%\llvm\build\%abi%\" set /p boostllvmspvbld=Speedup build configuration (y/n):
-@if EXIST "%devroot%\llvm\build\%abi%\" echo.
-@IF /I "%boostllvmspvbld%"=="y" set buildconf=%buildconf% -DCMAKE_PREFIX_PATH="%devroot%\llvm\build\%abi%"
 
 @echo SPIRV LLVM translator build configuration command^: %buildconf%
 @echo.
@@ -78,7 +73,7 @@
 @pause
 @echo.
 @if /I NOT "%useninja%"=="y" cmake --build . -j %throttle% --config Release --target install
-@if /I "%useninja%"=="y" ninja -j %throttle% projects/SPIRV-LLVM-Translator/install
+@if /I "%useninja%"=="y" ninja -j %throttle% install
 @echo.
 
 :skipspvllvm

commit 34cda6d5c353db7dc4ecf202b70b9cfa88dbc894

LLVM SPIRV translator: Don't try building without LLVM build configuration command, regression from 548a04f4

buildscript/modules/llvm.cmd
@@ -74,8 +74,10 @@
 @echo.
 
 @rem Ask to do LLVM build
-@set /p buildllvm=Begin LLVM build (y/n):
-@echo.
+@if EXIST "%devroot%\llvm-project\" set /p buildllvm=Begin LLVM build (y/n):
+@if EXIST "%devroot%\llvm-project\" echo.
+@if NOT EXIST "%devroot%\llvm-project\" if EXIST "%devroot%\llvm\cmake\" set /p buildllvm=Begin LLVM build (y/n):
+@if NOT EXIST "%devroot%\llvm-project\" if EXIST "%devroot%\llvm\cmake\" echo.
 @IF /I NOT "%buildllvm%"=="y" GOTO skipllvm
 
 @rem Always clean build
@@ -123,7 +125,7 @@
 @IF %llvmsources% EQU 0 echo.
 @IF %llvmsources% EQU 1 IF %cmakestate% EQU 0 echo.
 @IF %llvmsources% EQU 1 IF %cmakestate% GTR 0 IF NOT EXIST "%devroot%\llvm\build\%abi%\lib\" echo.
-@IF EXIST "%devroot%\llvm\build\%abi%\lib\" call "%devroot%\%projectname%\buildscript\modules\llvmspv.cmd"
+@IF EXIST "%devroot%\llvm\build\%abi%\lib\" if /I "%cfgllvmbuild%"=="y" call "%devroot%\%projectname%\buildscript\modules\llvmspv.cmd"
 
 @rem Reset environment after LLVM build.
 @endlocal