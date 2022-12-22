f67072462d7ff2c1a4d6366d6dbe7f5369694f9c
pal1000 <liviuprodea@yahoo.com>
2022-12-22 17:55:06 +0200

    Fix DXIL location detection. for /R doesn't support loop vatiable expansion from parent loop

--- buildscript/modules/dxil.cmd
+++ buildscript/modules/dxil.cmd
@@ -1,7 +1,8 @@
 @setlocal ENABLEDELAYEDEXPANSION
 @set dxilloc=null
-@IF %dxilloc%==null for /f delims^=^ eol^= %%a in ('@call "%devroot%\%projectname%\buildscript\modules\winsdkloc.cmd"') do @IF EXIST "%%a" for /R "%%a\bin" %%b in (dxil.dl?) do @if "%%~nxb"=="dxil.dll" (
-@set dxilloc="%%~fb"
-@IF NOT "!dxilloc:~-13,3!"=="%abi%" set dxilloc=null
+@IF %dxilloc%==null for /f delims^=^ eol^= %%a in ('@call "%devroot%\%projectname%\buildscript\modules\winsdkloc.cmd"') do @IF EXIST "%%a" for /f delims^=^ eol^= %%b in ('dir /b /s "%%a\bin\dxil.dll" 2^>^&1') do @if EXIST "%%b" (
+@set dxilwinsdk="%%~b"
+@IF "!dxilwinsdk:~-13,3!"=="%abi%" set dxilloc="%%~b"
 )
+@set "dxilwinsdk="
 @echo %dxilloc%
