b371c0269621d4ac713a49726823f4b2b1154567
pal1000 <liviuprodea@yahoo.com>
2022-12-27 16:26:26 +0200

    Support downloading and extracting Resource Hacker automatically

--- buildscript/modules/nuget.cmd
+++ buildscript/modules/nuget.cmd
@@ -6,10 +6,10 @@
 @if NOT "%ERRORLEVEL%"=="0" set nugetstate=1
 @set updnuget=n
 @IF %nugetstate%==1 IF EXIST "%devroot%\nuget\nuget.exe" set /p updnuget=Update Nuget CLI tool (y/n):
+@IF %nugetstate%==1 IF NOT EXIST "%devroot%\nuget\" MD "%devroot%\nuget"
 @IF %nugetstate%==1 IF NOT EXIST "%devroot%\nuget\nuget.exe" echo Getting Nuget CLI tool...
+@IF %nugetstate%==1 IF NOT EXIST "%devroot%\nuget\nuget.exe" set updnuget=y
 @IF %nugetstate%==1 echo.
 @if /I "%updnuget%"=="y" powershell -NoLogo "Invoke-WebRequest -Uri 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe' -OutFile '%devroot%\nuget\nuget.exe'" 2>nul
-@IF %nugetstate%==1 IF NOT EXIST "%devroot%\nuget\" MD "%devroot%\nuget"
-@IF %nugetstate%==1 IF NOT EXIST "%devroot%\nuget\nuget.exe" powershell -NoLogo "Invoke-WebRequest -Uri 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe' -OutFile '%devroot%\nuget\nuget.exe'" 2>nul
 @IF %nugetstate%==1 IF NOT EXIST "%devroot%\nuget\nuget.exe" set nugetstate=0
 @endlocal&set nugetstate=%nugetstate%
--- buildscript/modules/resourcehacker.cmd
+++ buildscript/modules/resourcehacker.cmd
@@ -3,5 +3,15 @@
 @CMD /C EXIT 0
 @where /q ResourceHacker.exe
 @if NOT "%ERRORLEVEL%"=="0" set rhstate=1
+@set updrh=n
+@IF %rhstate%==1 IF EXIST "%devroot%\resource-hacker\ResourceHacker.exe" set /p updrh=Update Resource Hacker (y/n):
+@IF %rhstate%==1 IF NOT EXIST "%devroot%\resource-hacker\" MD "%devroot%\resource-hacker"
+@IF %rhstate%==1 IF NOT EXIST "%devroot%\resource-hacker\ResourceHacker.exe" echo Getting Resource Hacker...
+@IF %rhstate%==1 IF NOT EXIST "%devroot%\resource-hacker\ResourceHacker.exe" set updrh=y
+@IF %rhstate%==1 echo.
+@if /I "%updrh%"=="y" IF NOT EXIST "%USERPROFILE%\Downloads\%projectname%\" md "%USERPROFILE%\Downloads\%projectname%"
+@if /I "%updrh%"=="y" powershell -NoLogo "Invoke-WebRequest -Uri 'http://www.angusj.com/resourcehacker/resource_hacker.zip' -OutFile '%USERPROFILE%\Downloads\%projectname%\resource_hacker.zip'" 2>nul
+@if /I "%updrh%"=="y" IF EXIST "%USERPROFILE%\Downloads\%projectname%\resource_hacker.zip" powershell -NoLogo "Expand-Archive -Path '%USERPROFILE%\Downloads\%projectname%\resource_hacker.zip' -Destination '%devroot%\resource-hacker'" 2>nul
+@if /I "%updrh%"=="y" IF EXIST "%USERPROFILE%\Downloads\%projectname%\" RD /S /Q "%USERPROFILE%\Downloads\%projectname%"
 @IF %rhstate%==1 IF NOT EXIST "%devroot%\resource-hacker\ResourceHacker.exe" set rhstate=0
 @endlocal&set rhstate=%rhstate%
