e6daa713bbf6c72bb0c4465bf632afae2430effb
pal1000 <liviuprodea@yahoo.com>
2022-12-27 16:51:29 +0200

    Make sure Resource hacker extraction folder is properly overwrritten

--- buildscript/modules/resourcehacker.cmd
+++ buildscript/modules/resourcehacker.cmd
@@ -11,6 +11,7 @@
 @IF %rhstate%==1 echo.
 @if /I "%updrh%"=="y" IF NOT EXIST "%USERPROFILE%\Downloads\%projectname%\" md "%USERPROFILE%\Downloads\%projectname%"
 @if /I "%updrh%"=="y" powershell -NoLogo "Invoke-WebRequest -Uri 'http://www.angusj.com/resourcehacker/resource_hacker.zip' -OutFile '%USERPROFILE%\Downloads\%projectname%\resource_hacker.zip'" 2>nul
+@if /I "%updrh%"=="y" IF EXIST "%devroot%\resource-hacker\ResourceHacker.exe" RD /S /Q "%devroot%\resource-hacker"
 @if /I "%updrh%"=="y" IF EXIST "%USERPROFILE%\Downloads\%projectname%\resource_hacker.zip" powershell -NoLogo "Expand-Archive -Path '%USERPROFILE%\Downloads\%projectname%\resource_hacker.zip' -Destination '%devroot%\resource-hacker'" 2>nul
 @if /I "%updrh%"=="y" IF EXIST "%USERPROFILE%\Downloads\%projectname%\" RD /S /Q "%USERPROFILE%\Downloads\%projectname%"
 @IF %rhstate%==1 IF NOT EXIST "%devroot%\resource-hacker\ResourceHacker.exe" set rhstate=0
