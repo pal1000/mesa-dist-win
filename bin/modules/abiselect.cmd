867ee7a04c5a53473c3082e82b4da502c9fc3a9f
pal1000 <liviuprodea@yahoo.com>
2023-01-05 15:35:12 +0200

    Hide ABI selection menu on x86 32-bit

--- bin/modules/abiselect.cmd
+++ bin/modules/abiselect.cmd
@@ -1,11 +1,11 @@
 :cpuchoice
-@echo 1. x86 32-bit (Default)
+@if /I NOT %PROCESSOR_ARCHITECTURE%==X86 echo 1. x86 32-bit (Default)
 @if /I NOT %PROCESSOR_ARCHITECTURE%==X86 echo 2. x64 (x86 64-bit)
 @if /I %PROCESSOR_ARCHITECTURE%==ARM64 echo 3. ARM64
-@set /p cpuchoice=Enter choice:
-@echo.
+@if /I NOT %PROCESSOR_ARCHITECTURE%==X86 set /p cpuchoice=Enter choice:
+@if /I NOT %PROCESSOR_ARCHITECTURE%==X86 echo.
 @set invalidcpuchoice=0
-@if /I %PROCESSOR_ARCHITECTURE%==X86 IF NOT "%cpuchoice%"=="1" set invalidcpuchoice=1
+@if /I %PROCESSOR_ARCHITECTURE%==X86 set cpuchoice=1
 @if /I %PROCESSOR_ARCHITECTURE%==AMD64 IF NOT "%cpuchoice%"=="1" IF NOT "%cpuchoice%"=="2" set invalidcpuchoice=1
 @if /I %PROCESSOR_ARCHITECTURE%==ARM64 IF NOT "%cpuchoice%"=="1" IF NOT "%cpuchoice%"=="2" IF NOT "%cpuchoice%"=="3" set invalidcpuchoice=1
 @if %invalidcpuchoice% EQU 1 echo Invalid choice.
