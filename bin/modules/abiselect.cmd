1c2b2a1493ab88c0f31878b421d9f210276dd59b
pal1000 <liviuprodea@yahoo.com>
2023-01-05 13:58:41 +0200

    Make ABI selection menu header customizable

--- bin/modules/abiselect.cmd
+++ bin/modules/abiselect.cmd
@@ -1,5 +1,4 @@
 :cpuchoice
-@echo Select processor architecture
 @echo 1. x86 32-bit (Default)
 @if /I NOT %PROCESSOR_ARCHITECTURE%==X86 echo 2. x64 (x86 64-bit)
 @if /I %PROCESSOR_ARCHITECTURE%==ARM64 echo 3. ARM64
