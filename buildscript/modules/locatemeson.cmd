pal1000/mesa-dist-win

commit e3c32000da2d77840c0f02cefb609f00881b3183

Implemented aggressive build environment cleanup. Stubbed MSYS2 Mingw-w64 build with Meson.

buildscript/modules/locatemeson.cmd
@@ -1,0 +1,14 @@
+@setlocal
+@IF %mesabldsys%==scons set mesonloc=null
+@IF %mesabldsys%==scons GOTO foundmeson
+@IF %mesabldsys%==meson IF %toolchain%==gcc IF %abi%==x86 set mesonloc=%msysloc%\usr\bin\bash --login -c "cd $mesa/mesa;/mingw32/bin/meson
+@IF %mesabldsys%==meson IF %toolchain%==gcc IF %abi%==x64 set mesonloc=%msysloc%\usr\bin\bash --login -c "cd $mesa/mesa;/mingw64/bin/meson
+@IF %mesabldsys%==meson IF %toolchain%==gcc GOTO foundmeson
+@IF %mesonstate%==2 set mesonloc=meson.exe
+@IF %mesonstate%==2 GOTO foundmeson
+
+@set mesonloc=%pythonloc:~0,-10%Scripts\meson.exe
+@IF NOT EXIST %mesonloc% set mesonloc=%pythonloc% %mesonloc:~0,-3%py
+
+:foundmeson
+@endlocal&set mesonloc=%mesonloc%

buildscript/modules/pythonpackages.cmd
@@ -1,3 +1,4 @@
+@setlocal
 @rem Install missing python packages.
 @rem State tracking is irrelevant for Python packages as they can easily be added via Pypi if missing.
 @rem Meson is the exception because its a Python 3 only package and as a result it can be missing.
@@ -20,6 +21,7 @@
 @set pypack=pywin32
 @GOTO pypackinstall
 )
+@set sconspypi=0
 @IF %mesabldsys%==scons for /F "skip=2 delims= " %%a in ('%pythonloc% -W ignore -m pip list --disable-pip-version-check') do @IF /I "%%a"=="scons" set sconspypi=1
 @IF %mesabldsys%==scons IF NOT "%sconspypi%"=="1" IF NOT EXIST %mesa%\scons (
 @set pypack=scons
@@ -28,12 +30,11 @@
 @if %mesabldsys%==scons set mesonstate=0
 @if %mesabldsys%==scons GOTO pyupdate
 
-@SET mesonloc=meson.exe
 @set mesonstate=2
 @SET ERRORLEVEL=0
 @where /q meson.exe
-@IF ERRORLEVEL 1 set mesonloc=%pythonloc:~0,-10%Scripts\meson.py
-@IF %mesonloc%==%pythonloc:~0,-10%Scripts\meson.py IF NOT EXIST %mesonloc% IF NOT EXIST %mesonloc:~0,-2%exe (
+@IF ERRORLEVEL 1 set mesonstate=1
+@IF NOT EXIST %pythonloc:~0,-10%Scripts\meson.py IF NOT EXIST %pythonloc:~0,-10%Scripts\meson.exe (
 @set pypack=meson
 @GOTO pypackinstall
 )
@@ -67,10 +68,6 @@
 @if /I "%pyupd%"=="y" IF %pythonver:~0,1%==2 IF NOT EXIST "%windir%\system32\pythoncom27.dll" IF NOT EXIST "%windir%\syswow64\pythoncom27.dll" GOTO locatemeson
 @if /I "%pyupd%"=="y" IF %pythonver:~0,1%==2 powershell -Command Start-Process "%mesa%\mesa-dist-win\buildscript\modules\pywin32.cmd" -Args "%pythonloc%" -Verb runAs 2>nul
 
-:locatemeson
-@IF %mesabldsys%==scons GOTO endpython
-@IF %mesonloc%==%pythonloc:~0,-10%Scripts\meson.py IF NOT EXIST %mesonloc% set mesonloc=%mesonloc:~0,-2%exe
-@IF %mesonloc%==%pythonloc:~0,-10%Scripts\meson.py IF EXIST %mesonloc% set mesonloc=%pythonloc% %mesonloc%
-
 :endpython
 @echo.
+@endlocal&set sconspypi=%sconspypi%&set mesonstate=%mesonstate%

buildscript/build.cmd
@@ -21,8 +21,8 @@
 @rem If using MSVC search for Python. State tracking is pointless as it is loaded once and we are done. Hard fail if missing.
 @IF %toolchain%==msvc call %mesa%\mesa-dist-win\buildscript\modules\discoverpython.cmd
 
-@rem If using MSVC select build system to use with Mesa3D.
-@IF %toolchain%==msvc call %mesa%\mesa-dist-win\buildscript\modules\selectmesabldsys.cmd
+@rem Select build system to use with Mesa3D.
+@call %mesa%\mesa-dist-win\buildscript\modules\selectmesabldsys.cmd
 
 @rem If using MSVC search for Python packages. Install missing packages automatically. Ask to do an update to all packages.
 @IF %toolchain%==msvc call %mesa%\mesa-dist-win\buildscript\modules\pythonpackages.cmd
@@ -33,14 +33,14 @@
 @rem Version control
 @call %mesa%\mesa-dist-win\buildscript\modules\git.cmd
 
+@rem Get Meson build location
+@call %mesa%\mesa-dist-win\buildscript\modules\locatemeson.cmd
+
 @rem If using MSVC check for remaining dependencies: cmake, ninja, winflexbison and pkg-config if applies.
 @IF %toolchain%==msvc call %mesa%\mesa-dist-win\buildscript\modules\cmake.cmd
 @IF %toolchain%==msvc call %mesa%\mesa-dist-win\buildscript\modules\ninja.cmd
 @IF %toolchain%==msvc call %mesa%\mesa-dist-win\buildscript\modules\winflexbison.cmd
 @IF %toolchain%==msvc call %mesa%\mesa-dist-win\buildscript\modules\pkg-config.cmd
-
-@rem Backup PATH before building anything to easily keep environment clean.
-@set oldpath=%PATH%
 
 @rem If using MSVC do LLVM build.
 @IF %toolchain%==msvc call %mesa%\mesa-dist-win\buildscript\modules\llvm.cmd

buildscript/modules/mesa3d.cmd
@@ -6,7 +6,7 @@
 @if NOT EXIST mesa if %gitstate%==0 echo Fatal: Both Mesa3D code and Git are missing. At least one is required. Execution halted.
 @if NOT EXIST mesa if %gitstate%==0 echo.
 @if NOT EXIST mesa if %gitstate%==0 GOTO skipmesa
-@IF %mesabldsys%==meson IF %pkgconfigstate%==0 echo No suitable pkg-config implementtion found. pkgconf or pkg-config-lite is required to build Mesa3D with Meson and MSVC.
+@IF %mesabldsys%==meson IF %pkgconfigstate%==0 echo No suitable pkg-config implementation found. pkgconf or pkg-config-lite is required to build Mesa3D with Meson and MSVC.
 @IF %mesabldsys%==meson IF %pkgconfigstate%==0 echo.
 @IF %mesabldsys%==meson IF %pkgconfigstate%==0 GOTO skipmesa
 
@@ -33,18 +33,14 @@
 @if /i NOT "%buildmesa%"=="y" GOTO skipmesa
 @cd mesa
 
-@rem Locate LLVM
-@IF %toolchain%==msvc set LLVM=%mesa%\llvm\%abi%
-@IF %toolchain%==gcc set LLVM=/mingw%minabi%
-@set havellvm=0
-@IF EXIST %LLVM% set havellvm=1
-@IF %toolchain%==gcc set havellvm=1
-
 @rem Get Mesa3D version as an integer
 @set /p mesaver=<VERSION
 @if "%mesaver:~-7%"=="0-devel" set /a intmesaver=%mesaver:~0,2%%mesaver:~3,1%00
 @if "%mesaver:~5,4%"=="0-rc" set /a intmesaver=%mesaver:~0,2%%mesaver:~3,1%00+%mesaver:~9%
 @if NOT "%mesaver:~5,2%"=="0-" set /a intmesaver=%mesaver:~0,2%%mesaver:~3,1%50+%mesaver:~5%
+@IF NOT EXIST %mesa%\mesa\subprojects if %mesabldsys%==meson echo Meson build support is only available in Mesa 19.3 and newer.
+@IF NOT EXIST %mesa%\mesa\subprojects if %mesabldsys%==meson echo.
+@IF NOT EXIST %mesa%\mesa\subprojects if %mesabldsys%==meson GOTO skipmesa
 
 @REM Collect information about Mesa3D code. Apply patches
 @if %gitstate%==0 IF %toolchain%==msvc GOTO configmesabuild
@@ -62,26 +58,22 @@
 @if %mesabldsys%==scons if %toolchain%==msvc IF "%sconspypi%"=="1" set buildcmd=%pythonloc:~0,-10%Scripts\scons.py
 @if %mesabldsys%==scons if %toolchain%==msvc IF "%sconspypi%"=="1" IF NOT EXIST "%buildcmd%" set buildcmd=%pythonloc:~0,-10%Scripts\scons
 @if %mesabldsys%==scons if %toolchain%==msvc set buildcmd=%pythonloc% %buildcmd%
-@if %mesabldsys%==scons if %toolchain%==gcc set buildcmd=%msysloc%\usr\bin\bash --login -c "cd $mesa/mesa;scons
+@if %mesabldsys%==scons if %toolchain%==gcc set buildcmd=%msysloc%\usr\bin\bash --login -c "cd $mesa/mesa;/usr/bin/scons
 @if %mesabldsys%==scons set buildcmd=%buildcmd% -j%throttle% build=release platform=windows machine=%longabi%
 @if %mesabldsys%==scons if %toolchain%==gcc set buildcmd=%buildcmd% toolchain=mingw
 @if %mesabldsys%==scons if %toolchain%==msvc set buildcmd=%buildcmd% MSVC_USE_SCRIPT=%vsenv%
 
 @set buildconf=null
-@if %mesabldsys%==meson set buildconf=%mesonloc% build/%abi% --backend=
-@IF %mesabldsys%==meson set platformabi=Win32
-@IF %mesabldsys%==meson IF %abi%==x64 set platformabi=%abi%
-@if %mesabldsys%==meson set buildcmd=msbuild /p^:Configuration=release,Platform=%platformabi% mesa.sln /m^:%throttle%
+@if %mesabldsys%==meson set buildconf=%mesonloc% build/%abi% --default-library=static --buildtype=release -Db_vscrt=mt
+@if %mesabldsys%==meson set buildcmd=msbuild /p^:Configuration=release,Platform=Win32 mesa.sln /m^:%throttle%
+@if %mesabldsys%==meson IF %abi%==x64 set buildcmd=msbuild /p^:Configuration=release,Platform=x64 mesa.sln /m^:%throttle%
 
-@set useninja=n
-@if %mesabldsys%==meson if NOT %ninjastate%==0 set /p useninja=Use Ninja build system instead of MsBuild (y/n); less storage device strain and maybe faster build:
-@if %mesabldsys%==meson if NOT %ninjastate%==0 echo.
-@if /I "%useninja%"=="y" if %ninjastate%==1 set PATH=%mesa%\ninja\;%PATH%
-@if /I "%useninja%"=="y" set buildconf=%buildconf%ninja
-@if /I "%useninja%"=="y" set buildcmd=ninja -j %throttle%
-@if %mesabldsys%==meson if /I NOT "%useninja%"=="y" set buildconf=%buildconf%vs
-@if %mesabldsys%==meson set buildconf=%buildconf% --default-library=static --buildtype=release -Db_vscrt=mt
-
+@IF %toolchain%==msvc set LLVM=%mesa%\llvm\%abi%
+@IF %toolchain%==gcc set LLVM=/mingw32
+@IF %toolchain%==gcc IF %abi%==x64 set LLVM=/mingw64
+@set havellvm=0
+@IF EXIST %LLVM% set havellvm=1
+@IF %toolchain%==gcc set havellvm=1
 @set llvmless=n
 @if %havellvm%==0 set llvmless=y
 @if %havellvm%==1 set /p llvmless=Build Mesa without LLVM (y/n). llvmpipe and swr drivers and high performance JIT won't be available for other drivers and libraries:
@@ -89,6 +81,16 @@
 @if %mesabldsys%==scons if /I "%llvmless%"=="y" set buildcmd=%buildcmd% llvm=no
 @if %mesabldsys%==meson if /I NOT "%llvmless%"=="y" call %mesa%\mesa-dist-win\buildscript\modules\llvmwrapgen.cmd
 @if %mesabldsys%==meson if /I NOT "%llvmless%"=="y" set buildconf=%buildconf% -Dllvm=true
+
+@set useninja=n
+@if %mesabldsys%==meson IF %toolchain%==gcc set useninja=y
+@if %mesabldsys%==meson if NOT %ninjastate%==0 IF %toolchain%==msvc set /p useninja=Use Ninja build system instead of MsBuild (y/n); less storage device strain and maybe faster build:
+@if %mesabldsys%==meson if NOT %ninjastate%==0 IF %toolchain%==msvc echo.
+@if /I "%useninja%"=="y" if %ninjastate%==1 IF %toolchain%==msvc set PATH=%mesa%\ninja\;%PATH%
+@if /I "%useninja%"=="y" set buildconf=%buildconf% --backend=ninja
+@if /I "%useninja%"=="y" IF %toolchain%==msvc set buildcmd=ninja -j %throttle%
+@if /I "%useninja%"=="y" IF %toolchain%==gcc set buildcmd=%msysloc%\usr\bin\bash --login -c "cd $mesa/mesa/build/%abi%;%LLVM%/bin/ninja -j %throttle%"
+@if %mesabldsys%==meson if /I NOT "%useninja%"=="y" set buildconf=%buildconf% --backend=vs
 
 @if %mesabldsys%==scons IF %toolchain%==msvc set /p openmp=Build Mesa3D with OpenMP. Faster build and smaller binaries (y/n):
 @if %mesabldsys%==scons IF %toolchain%==msvc echo.
@@ -123,23 +125,24 @@
 @IF /I "%expressmesabuild%"=="y" set graw=y
 @if %mesabldsys%==meson if /I "%graw%"=="y" set buildconf=%buildconf% -Dbuild-tests=true
 
-@if %mesabldsys%==scons if %toolchain%==gcc set buildcmd=%buildcmd%"
+@if %toolchain%==gcc IF %mesabldsys%==scons set buildcmd=%buildcmd%"
+@if %toolchain%==gcc IF %mesabldsys%==meson set buildconf=%buildconf%"
 
 @set cleanbuild=n
+@IF %mesabldsys%==meson for /d %%a in ("%mesa%\mesa\subprojects\zlib-*") do @RD /S /Q "%%~a"
+@IF %mesabldsys%==meson for /d %%a in ("%mesa%\mesa\subprojects\expat-*") do @RD /S /Q "%%~a"
 @IF %mesabldsys%==scons if EXIST build\windows-%longabi% set /p cleanbuild=Do you want to clean build (y/n):
 @IF %mesabldsys%==scons if EXIST build\windows-%longabi% echo.
 @IF %mesabldsys%==meson if EXIST build\%abi% set /p cleanbuild=Do you want to clean build (y/n):
 @IF %mesabldsys%==meson if EXIST build\%abi% echo.
 @IF %mesabldsys%==scons if /I "%cleanbuild%"=="y" RD /S /Q build\windows-%longabi%
 @IF %mesabldsys%==meson if /I "%cleanbuild%"=="y" RD /S /Q build\%abi%
-@IF %mesabldsys%==meson if /I "%cleanbuild%"=="y" for /d %%a in ("%mesa%\mesa\subprojects\zlib-*") do @RD /S /Q "%%~a"
-@IF %mesabldsys%==meson if /I "%cleanbuild%"=="y" for /d %%a in ("%mesa%\mesa\subprojects\expat-*") do @RD /S /Q "%%~a"
 
 @IF %toolchain%==msvc IF %flexstate%==1 set PATH=%mesa%\flexbison\;%PATH%
 @if %mesabldsys%==meson IF %toolchain%==msvc set PATH=%pkgconfigloc%\;%PATH%
 
 :build_mesa
-@rem Generate dummy heaader for MSVC build when git is missing.
+@rem Generate dummy header for MSVC build when git is missing.
 @IF %toolchain%==msvc if NOT EXIST build md build
 @IF %toolchain%==msvc IF %mesabldsys%==scons if NOT EXIST build\windows-%longabi% md build\windows-%longabi%
 @IF %toolchain%==msvc IF %mesabldsys%==scons if NOT EXIST build\windows-%longabi%\git_sha1.h echo 0 > build\windows-%longabi%\git_sha1.h
@@ -152,7 +155,8 @@
 @IF %toolchain%==msvc IF /I "%useninja%"=="y" call %vsenv% %abi%
 @IF %toolchain%==msvc IF /I NOT "%useninja%"=="y" call %vsenv% %vsabi%
 @IF %toolchain%==msvc echo.
-@IF %toolchain%==gcc set MSYSTEM=MINGW%minabi%
+@IF %toolchain%==gcc set MSYSTEM=MINGW32
+@IF %toolchain%==gcc IF %abi%==x64 set MSYSTEM=MINGW64
 @IF %toolchain%==gcc set CFLAGS=-march=core2 -pipe
 @IF %toolchain%==gcc set CXXFLAGS=-march=core2 -pipe
 @IF %toolchain%==gcc set LDFLAGS=-static -s
@@ -162,14 +166,14 @@
 @if %mesabldsys%==meson echo.
 @if %mesabldsys%==meson %buildconf%
 @if %mesabldsys%==meson echo.
-@if %mesabldsys%==meson cd build\%abi%
+@if %mesabldsys%==meson IF %toolchain%==msvc cd build\%abi%
 @echo Build command: %buildcmd%
 @echo.
 @if %mesabldsys%==meson pause
 @if %mesabldsys%==meson echo.
 @%buildcmd%
 @echo.
-@if %mesabldsys%==meson cd ..\..\
+@if %mesabldsys%==meson IF %toolchain%==msvc cd ..\..\
 
 :skipmesa
 @rem Reset environment.

buildscript/modules/msys.cmd
@@ -1,8 +1,6 @@
 @setlocal
 @rem ABI format conversions for Mingw
 @set mingwabi=%longabi%
-@set minabi=%abi:~1%
-@IF %abi%==x86 set minabi=32
 @IF %abi%==x86 set mingwabi=i686
 
 @rem Load MSYS2 environment
@@ -10,4 +8,4 @@
 @set msysloc=%mesa%\msys64
 @IF NOT EXIST %msysloc% set msysloc=%mesa%\msys32
 @IF NOT EXIST %msysloc% set msysstate=0
-@endlocal&set mingwabi=%mingwabi%&set minabi=%minabi%&set msysstate=%msysstate%&set msysloc=%msysloc%
+@endlocal&set mingwabi=%mingwabi%&set msysstate=%msysstate%&set msysloc=%msysloc%

buildscript/modules/pkg-config.cmd
@@ -17,7 +17,8 @@
 @call %mesa%\mesa-dist-win\buildscript\modules\msysupdate.cmd
 @%msysloc%\usr\bin\bash --login -c "pacman -S mingw-w64-%mingwabi%-pkg-config --needed --noconfirm --disable-download-timeout"
 @echo.
-@set pkgconfigloc=%msysloc%\mingw%minabi%\bin
+@set pkgconfigloc=%msysloc%\mingw32\bin
+@IF %abi%==x64 set pkgconfigloc=%msysloc%\mingw64\bin
 @GOTO doneenvcheck
 
 :nonmingwpkgconfig

buildscript/modules/selectmesabldsys.cmd
@@ -1,15 +1,29 @@
 @setlocal
 
-:mesabldsys
+:msmesabldsys
 @set selectmesabld=1
+@IF %toolchain%==gcc GOTO gccmesabldsys
 @IF %pythonver:~0,1% GEQ 3 set selectmesabld=2
-@rem IF %pythonver:~0,1% GEQ 3 echo Select Mesa3D build system
-@rem IF %pythonver:~0,1% GEQ 3 echo --------------------------
-@rem IF %pythonver:~0,1% GEQ 3 echo 1. Scons (recommended)
-@rem IF %pythonver:~0,1% GEQ 3 echo 2. Meson (experimental, incomplete, work in progress)
-@rem IF %pythonver:~0,1% GEQ 3 echo.
-@rem IF %pythonver:~0,1% GEQ 3 set /p selectmesabld=Select Mesa3D build system:
-@rem IF %pythonver:~0,1% GEQ 3 echo.
+@GOTO donemesabldsys
+
+@IF %pythonver:~0,1% GEQ 3 echo Select Mesa3D build system
+@IF %pythonver:~0,1% GEQ 3 echo --------------------------
+@IF %pythonver:~0,1% GEQ 3 echo 1. Scons (recommended)
+@IF %pythonver:~0,1% GEQ 3 echo 2. Meson (experimental, incomplete, work in progress)
+@IF %pythonver:~0,1% GEQ 3 echo.
+@IF %pythonver:~0,1% GEQ 3 set /p selectmesabld=Select Mesa3D build system:
+@IF %pythonver:~0,1% GEQ 3 echo.
+
+:gccmesabldsys
+@echo Select Mesa3D build system
+@echo --------------------------
+@echo 1. Scons (recommended)
+@echo 2. Meson (experimental, incomplete, work in progress)
+@echo.
+@set /p selectmesabld=Select Mesa3D build system:
+@echo.
+
+:donemesabldsys
 @IF "%selectmesabld%"=="1" set mesabldsys=scons
 @IF "%selectmesabld%"=="2" set mesabldsys=meson
 @IF NOT "%selectmesabld%"=="1" IF NOT "%selectmesabld%"=="2" (
@@ -17,6 +31,6 @@
 @echo.
 @puse
 @cls
-@GOTO mesabldsys
+@GOTO msmesabldsys
 )
 @endlocal&set mesabldsys=%mesabldsys%

commit 4dc721667c4820c1db793512b48fc44c6a79f2e6

MSVC build: Drop attempt at supporting Meson Windows installer that was broken anyway since e3c32000da2d77840c0f02cefb609f00881b3183

buildscript/modules/locatemeson.cmd
@@ -1,9 +1,6 @@
 @setlocal
 @IF NOT %toolchain%==msvc set mesonloc=%runmsys% /%LMSYSTEM%/bin/meson
 @IF NOT %toolchain%==msvc GOTO foundmeson
-@IF %mesonstate%==2 set mesonloc=meson.exe
-@IF %mesonstate%==2 GOTO foundmeson
-
 @set mesonloc="%devroot%\%projectname%\buildscript\assets\venv\Scripts\meson.exe"
 @IF NOT EXIST %mesonloc% set mesonloc="%devroot%\%projectname%\buildscript\assets\venv\Scripts\python.exe" %mesonloc:~0,-4%py"
 

buildscript/modules/pythonpackages.cmd
@@ -20,7 +20,7 @@
 @setlocal
 @rem Install missing python packages.
 @rem State tracking is irrelevant for Python packages as they can easily be added via Pypi if missing.
-@rem Meson is the exception because it can be provided as an embedded Python distribution.
+@rem Meson also provides an alternative Windows installer that adds it to PATH but supporting it is totally not worth it.
 
 @set pypack=0
 @set firstpyinstall=1
@@ -35,11 +35,6 @@
 @set pypack=Mako
 @GOTO pypackinstall
 )
-
-@set mesonstate=2
-@CMD /C EXIT 0
-@where /q meson.exe
-@if NOT "%ERRORLEVEL%"=="0" set mesonstate=1
 @IF NOT EXIST "%devroot%\%projectname%\buildscript\assets\venv\Scripts\meson.py" IF NOT EXIST "%devroot%\%projectname%\buildscript\assets\venv\Scripts\meson.exe" (
 @set pypack=meson
 @GOTO pypackinstall
@@ -74,4 +69,4 @@ echo.
 )
 
 :endpython
-@endlocal&set mesonstate=%mesonstate%
+@endlocal