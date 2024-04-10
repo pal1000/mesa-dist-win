e247836c49b83edd3c543b2b4b342f5b6d651d51
pal1000 <liviuprodea@yahoo.com>
2024-04-10 18:38:35 +0300

    MSVC build: Avoid using zstd MSYS program

--- buildscript/modules/winflexbison.cmd
+++ buildscript/modules/winflexbison.cmd
@@ -8,3 +8,7 @@
 @IF %flexstate%==1 IF NOT EXIST "%devroot%\flexbison\win_flex.exe" set flexloc=%msysloc%\usr\bin
 @IF %flexstate%==1 IF NOT EXIST "%devroot%\flexbison\win_flex.exe" IF NOT EXIST "%msysloc%\usr\bin\flex.exe" set flexstate=0
 @endlocal&set flexstate=%flexstate%&set flexloc=%flexloc%
+@IF %flexstate%==1 IF "%flexloc%"=="%msysloc%\usr\bin" IF EXIST "%msysloc%\flexbison\" RD /S /Q "%msysloc%\flexbison"
+@IF %flexstate%==1 IF "%flexloc%"=="%msysloc%\usr\bin" ROBOCOPY "%msysloc%\usr" "%msysloc%\flexbison" /E
+@IF %flexstate%==1 IF "%flexloc%"=="%msysloc%\usr\bin" IF EXIST "%msysloc%\flexbison\bin\zstd.exe" del "%msysloc%\flexbison\bin\zstd.exe"
+@IF %flexstate%==1 IF "%flexloc%"=="%msysloc%\usr\bin" set flexloc=%msysloc%\flexbison\bin
