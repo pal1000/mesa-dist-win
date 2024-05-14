209c9648b0064af586bc9ce80f59362a8bc154ab
pal1000 <liviuprodea@yahoo.com>
2024-05-14 18:32:30 +0300

    MSYS2 debugger/setup command: Install only prefix specific packages.

--- buildscript/debug/msysconsole.cmd
+++ buildscript/debug/msysconsole.cmd
@@ -46,8 +46,8 @@
 @IF /I "%msyscmd%"=="shell" GOTO selectshell
 @IF /I "%msyscmd%"=="clearcache" call "%devroot%\%projectname%\buildscript\modules\msyspkgclean.cmd"
 @IF /I "%msyscmd%"=="cleancache" call "%devroot%\%projectname%\buildscript\modules\msyspkgclean.cmd"
-@IF /I "%msyscmd%"=="setup" IF %shell% EQU 1 %runmsys% pacman -S flex bison patch tar mingw-w64-i686-%mingwpkglst% mingw-w64-ucrt-x86_64-%mingwpkglst% --needed
-@IF /I "%msyscmd%"=="setup" IF %shell% GTR 1 %runmsys% pacman -S flex bison patch tar ${MINGW_PACKAGE_PREFIX}-%mingwpkglst% --needed
+@IF /I "%msyscmd%"=="setup" IF %shell% EQU 1 %runmsys% pacman -S flex bison patch tar --needed
+@IF /I "%msyscmd%"=="setup" IF %shell% GTR 1 %runmsys% pacman -S ${MINGW_PACKAGE_PREFIX}-%mingwpkglst% --needed
 @IF /I NOT "%msyscmd%"=="clearcache" IF /I NOT "%msyscmd%"=="cleancache" IF /I NOT "%msyscmd%"=="setup" %runmsys% %msyscmd%
 @echo.
 @GOTO command
