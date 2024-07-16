88ff7ca0af5b8d6307599a6ff7b248891fdfd475
pal1000 <liviuprodea@yahoo.com>
2024-07-16 01:25:58 -0700

    distribution maker: binary toolchain detection
    Todo list:
        - [x] Get mesa version;
        - [x] Check 7-zip version and hard fail if missing;
        - [x] Detect binary toolchain;
        - [ ] Detect MinGW buildtype;
        - [ ] Create release/debug and unit test packages;
        - [ ] Cleanup obsolete distribution cruft.

--- dist/create-distribution.cmd
+++ dist/create-distribution.cmd
@@ -14,10 +14,25 @@
 @if NOT "%ERRORLEVEL%"=="0" set sevenzip="%ProgramFiles%\7-Zip\7z.exe"
 @if NOT %sevenzip%==7z.exe if NOT EXIST %sevenzip% set sevenzip="%ProgramW6432%\7-Zip\7z.exe"
 @if NOT %sevenzip%==7z.exe if NOT EXIST %sevenzip% set sevenzip="%ProgramFiles(x86)%\7-Zip\7z.exe"
+@if NOT %sevenzip%==7z.exe if NOT EXIST %sevenzip% echo FATAL ERROR^: 7-Zip is not installed.
 @if %sevenzip%==7z.exe echo OK.
 @if NOT %sevenzip%==7z.exe if EXIST %sevenzip% echo OK.
-@if NOT %sevenzip%==7z.exe if NOT EXIST %sevenzip% echo FATAL ERROR^: 7-Zip is not installed.
 @echo.
 @if NOT %sevenzip%==7z.exe if NOT EXIST %sevenzip% pause
-@if NOT %sevenzip%==7z.exe if NOT EXIST %sevenzip% exit
+@if NOT %sevenzip%==7z.exe if NOT EXIST %sevenzip% exit /B
+
+@echo Detecting binary toolchain...
+@set msvclibs=0
+@for /f %%a IN ('dir /b /s ..\lib\*.lib 2^>nul') DO @set /a msvclibs+=1
+@set mingwlibs=0
+@for /f %%a IN ('dir /b /s ..\lib\*.dll.a 2^>nul') DO @set /a mingwlibs+=1
+@if %msvclibs% EQU 0 if %mingwlibs% EQU 0 echo FATAL ERROR^: Missing binaries!
+@if %msvclibs% GTR 0 if %mingwlibs% GTR 0 echo FATAL ERROR^: Binaries are corrupt!
+@if %msvclibs% GTR 0 if %mingwlibs% EQU 0 echo MSVC
+@if %msvclibs% EQU 0 if %mingwlibs% GTR 0 echo MINGW
+@echo.
+@if %msvclibs% EQU 0 if %mingwlibs% EQU 0 pause
+@if %msvclibs% EQU 0 if %mingwlibs% EQU 0 exit /B
+@if %msvclibs% GTR 0 if %mingwlibs% GTR 0 pause
+@if %msvclibs% GTR 0 if %mingwlibs% GTR 0 exit /B
 @pause
