7e1365fd7adf22a4e8f24ed1e6523568248aae88
pal1000 <liviuprodea@yahoo.com>
2024-11-01 08:17:31 -0700

    prompt module: Warn about variables not defined in CI build configuration script

--- bin/modules/prompt.cmd
+++ bin/modules/prompt.cmd
@@ -1,6 +1,6 @@
 @IF NOT defined cimode set cimode=0
 @if %cimode% EQU 0 set /p %1=%~2
-@if %cimode% EQU 0 echo.
 @setlocal ENABLEDELAYEDEXPANSION
 @if %cimode% EQU 1 IF NOT "!%1!"=="" echo %~2!%1!
-@if %cimode% EQU 1 IF NOT "!%1!"=="" echo.
+@if %cimode% EQU 1 IF "!%1!"=="" echo WARNING: Variable '%1' is undefined in CI build configuration script.
+@echo.
