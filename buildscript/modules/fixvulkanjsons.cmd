26ce85dd8dcd824206862265d5c99bae00f4e2e0
pal1000 <liviuprodea@yahoo.com>
2025-06-23 05:33:24 -0700

    Simpllify Vulkan JSON patch tool

--- buildscript/modules/fixvulkanjsons.cmd
+++ buildscript/modules/fixvulkanjsons.cmd
@@ -1,12 +1,10 @@
-@setlocal ENABLEDELAYEDEXPANSION
-@set jsonlines=0
-@for /R "%devroot%\%projectname%\bin\%abi%" %%a IN (%1_icd.*.json, %1_vk_icd.*.json) do @for /f delims^=^ eol^= %%b IN ('type "%%~a"') do @for /f tokens^=1-2^ delims^=^:^  %%c IN ("%%b") do (
-@set /a jsonlines+=1
-@if /I NOT "%%~c"=="library_path" set jsonline[!jsonlines!]=%%b
-@if /I "%%~c"=="library_path" IF EXIST "%devroot%\%projectname%\bin\%abi%\libvulkan_%1.dll" set jsonline[!jsonlines!]=        "library_path"^: ".^\^\libvulkan_%1.dll"
-@if /I "%%~c"=="library_path" IF NOT EXIST "%devroot%\%projectname%\bin\%abi%\libvulkan_%1.dll" set jsonline[!jsonlines!]=        "library_path"^: ".^\^\vulkan_%1.dll"
-)
+@setlocal
+@if EXIST "%devroot%\%projectname%\buildscript\assets\vulkan-patch-%toolchain%-%abi%.json" del "%devroot%\%projectname%\buildscript\assets\vulkan-patch-%toolchain%-%abi%.json"
 @for /R "%devroot%\%projectname%\bin\%abi%" %%a IN (%1_icd.*.json, %1_vk_icd.*.json) do @(
-@del "%%~a"
-@for /L %%b IN (1,1,%jsonlines%) do @echo !jsonline[%%b]!>>"%%~a"
+@for /f delims^=^ eol^= %%b IN ('type "%%~a"') do @for /f tokens^=1-2^ delims^=^:^  %%c IN ("%%b") do @(
+@if /I NOT "%%~c"=="library_path" echo %%b
+@if /I "%%~c"=="library_path" IF EXIST "%devroot%\%projectname%\bin\%abi%\libvulkan_%1.dll" echo         "library_path"^: ".^\^\libvulkan_%1.dll"
+@if /I "%%~c"=="library_path" IF NOT EXIST "%devroot%\%projectname%\bin\%abi%\libvulkan_%1.dll" echo         "library_path"^: ".^\^\vulkan_%1.dll"
+)>>"%devroot%\%projectname%\buildscript\assets\vulkan-patch-%toolchain%-%abi%.json"
+@copy "%devroot%\%projectname%\buildscript\assets\vulkan-patch-%toolchain%-%abi%.json" "%%~a"
 )
