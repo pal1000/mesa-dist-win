pal1000/mesa-dist-win

commit 5b3ca1ffc46e5c38ccd87ef63595c0745f8b0024

MSVC: Support looking for Ninja build in mesa-dist-win parent folder

buildscript/modules/ninja.cmd
@@ -4,5 +4,5 @@
 @CMD /C EXIT 0
 @where /q ninja.exe
 @if NOT "%ERRORLEVEL%"=="0" set ninjastate=1
-@IF %ninjastate%==1 IF NOT EXIST "%devroot%\ninja\ninja.exe" set ninjastate=0
+@IF %ninjastate%==1 IF NOT EXIST "%devroot%\ninja.exe" IF NOT EXIST "%devroot%\ninja\ninja.exe" set ninjastate=0
 @endlocal&set ninjastate=%ninjastate%

buildscript/debug/modify-llvm-install.cmd
@@ -13,7 +13,7 @@
 @call "%devroot%\%projectname%\buildscript\modules\throttle.cmd"
 @call "%devroot%\%projectname%\buildscript\modules\cmake.cmd"
 @call "%devroot%\%projectname%\buildscript\modules\ninja.cmd"
-@IF %ninjastate% EQU 1 SET PATH=%devroot%\ninja\;%PATH%
+@IF %ninjastate% EQU 1 call "%devroot%\%projectname%\buildscript\modules\useninja.cmd" noprompt
 @IF %cmakestate% EQU 1 SET PATH=%devroot%\cmake\bin\;%PATH%
 @call %vsenv% %vsabi%
 @set llvmloc=%devroot%\llvm-project

buildscript/modules/envdump.cmd
@@ -92,7 +92,7 @@ echo CMake %%a>>"%devroot%\%projectname%\buildinfo\msvc.txt"
 )
 
 @rem Get Ninja version
-@IF %toolchain%==msvc IF "%ninjastate%"=="1" set PATH=%devroot%\ninja\;%PATH%
+@IF %toolchain%==msvc IF "%ninjastate%"=="1" call "%devroot%\%projectname%\buildscript\modules\useninja.cmd" noprompt
 @IF %toolchain%==msvc IF NOT "%ninjastate%"=="0" IF NOT "%ninjastate%"=="" for /f eol^= %%a IN ('ninja --version') do @echo Ninja %%a>>"%devroot%\%projectname%\buildinfo\msvc.txt"
 
 @rem Get LLVM version

buildscript/modules/libclc.cmd
@@ -3,7 +3,7 @@
 @IF %cmakestate% EQU 0 set canclc=0
 @IF %cmakestate% EQU 1 set PATH=%devroot%\cmake\bin\;%PATH%
 @if %ninjastate% EQU 0 set canclc=0
-@if %ninjastate% EQU 1 set PATH=%devroot%\ninja\;%PATH%
+@if %ninjastate% EQU 1 call "%devroot%\%projectname%\buildscript\modules\useninja.cmd" noprompt
 @if NOT EXIST "%llvminstloc%\spv-%hostabi%\bin\llvm-spirv.exe" set canclc=0
 @if NOT EXIST "%llvminstloc%\%hostabi%\bin\clang.exe" set canclc=0
 @if NOT EXIST "%devroot%\llvm-project\" set canclc=0

buildscript/modules/useninja.cmd
@@ -1,4 +1,6 @@
 @if %cimode% EQU 0 set useninja=n
 @IF NOT %toolchain%==msvc set useninja=y
+@IF %toolchain%==msvc if %ninjastate% GTR 0 IF NOT "%*"=="" set useninja=y
 @IF %toolchain%==msvc if %ninjastate% GTR 0 IF "%*"=="" call "%devroot%\%projectname%\bin\modules\prompt.cmd" useninja "Use Ninja build system for less storage device strain and faster build than MsBuild (y/n):"
-@IF %toolchain%==msvc if /I "%useninja%"=="y" if %ninjastate%==1 set PATH=%devroot%\ninja\;%PATH%
+@IF %toolchain%==msvc if /I "%useninja%"=="y" if %ninjastate%==1 IF EXIST "%devroot%\ninja.exe" set PATH=%devroot%\;%PATH%
+@IF %toolchain%==msvc if /I "%useninja%"=="y" if %ninjastate%==1 IF NOT EXIST "%devroot%\ninja.exe" set PATH=%devroot%\ninja\;%PATH%

commit bf9c57070d29705a9c84af370c319c46f0c89eac

Avoid finding Ninja in current folder to make sure it's added to PATH Regression from 5b3ca1ffc46e5c38ccd87ef63595c0745f8b0024

buildscript/modules/ninja.cmd
@@ -1,8 +1,9 @@
-@setlocal
+@setlocal EnableDelayedExpansion
 @rem Ninja build system. Can have all states.
-@set ninjastate=2
-@CMD /C EXIT 0
-@where /q ninja.exe
-@if NOT "%ERRORLEVEL%"=="0" set ninjastate=1
+@set ninjastate=1
+@set mlpath=!PATH:;=^
+
+!
+@for /f tokens^=*^ eol^= %%a IN ("!mlpath!") DO @IF EXIST "%%~a\ninja.exe" set ninjastate=2
 @IF %ninjastate%==1 IF NOT EXIST "%devroot%\ninja.exe" IF NOT EXIST "%devroot%\ninja\ninja.exe" set ninjastate=0
 @endlocal&set ninjastate=%ninjastate%