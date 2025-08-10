pal1000/mesa-dist-win

commit 13e8cc928c2f5f3f1e82521ff3c0b71418304d63

MSVC: Build LLVM early to mmake clang-cl available on first build

buildscript/modules/llvmspv.cmd
@@ -39,6 +39,9 @@
 @echo.
 )
 
+@rem Build with Ninja if LLVM was also built with Ninja
+@for /f tokens^=*^ delims^= %%a IN ('echo "%llvmbuildconf%" ^| find "-GNinja"') DO @call "%devroot%\%projectname%\buildscript\modules\useninja.cmd" noprompt
+
 @rem SPIRV Tools integration for SPIRV LLVM translator. This is a feature introduced in SPIRV LLVM translator 14.x.
 @rem However it fails to link SPIRV Tools for some reason.
 @rem IF EXIST "%devroot%\spirv-tools\build\%abi%\" IF %pkgconfigstate% GTR 0 call "%devroot%\%projectname%\bin\modules\prompt.cmd" integratespvtools "Build with SPIRV Tools integration (y/n):"
@@ -46,7 +49,7 @@
 @IF /I "%integratespvtools%"=="y" set PKG_CONFIG_PATH=%devroot:\=/%/spirv-tools/build/%abi%/lib/pkgconfig
 @IF /I NOT "%integratespvtools%"=="y" set PKG_CONFIG_PATH=
 
-@set buildconf=%buildconf% -DLLVM_EXTERNAL_SPIRV_HEADERS_SOURCE_DIR="%devroot%\SPIRV-Headers" -DCMAKE_INSTALL_PREFIX="%llvminstloc%\spv-%abi%" -DCMAKE_PREFIX_PATH="%llvminstloc%\%abi%" -DLLVM_SPIRV_INCLUDE_TESTS=OFF
+@set buildconf=%llvmbuildconf% -DLLVM_EXTERNAL_SPIRV_HEADERS_SOURCE_DIR="%devroot%\SPIRV-Headers" -DCMAKE_INSTALL_PREFIX="%llvminstloc%\spv-%abi%" -DCMAKE_PREFIX_PATH="%llvminstloc%\%abi%" -DLLVM_SPIRV_INCLUDE_TESTS=OFF
 @FOR /F tokens^=^1^,2^ eol^= %%a IN ('type "%llvminstloc%\%abi%\lib\cmake\llvm\LLVMConfig.cmake"') DO @IF "%%a"=="set(LLVM_PACKAGE_VERSION" FOR /F tokens^=^1^ delims^=^.^ eol^= %%c IN ("%%b") DO @IF %%c GEQ 17 set buildconf=%buildconf% -DCMAKE_POLICY_DEFAULT_CMP0091=NEW
 @set buildconf=%buildconf% "%devroot%\SPIRV-LLVM-Translator"
 @echo SPIRV LLVM translator build configuration command^: %buildconf%

buildscript/build.cmd
@@ -44,9 +44,6 @@
 @rem MSVC: Select between legacy and current LLVM version
 @IF %toolchain%==msvc call "%devroot%\%projectname%\buildscript\modules\selectllvm.cmd"
 
-@rem Offer option to build with clang
-@call "%devroot%\%projectname%\buildscript\modules\clang.cmd"
-
 @rem If using MSVC search for Python. State tracking is pointless as it is loaded once and we are done. Hard fail if missing.
 @IF %toolchain%==msvc call "%devroot%\%projectname%\buildscript\modules\discoverpython.cmd"
 
@@ -59,10 +56,18 @@
 @rem Get Meson build location
 @call "%devroot%\%projectname%\buildscript\modules\locatemeson.cmd"
 
-@rem If using MSVC check for remaining dependencies: cmake, ninja, winflexbison, nuget and pkg-config if applies.
+@rem If using MSVC check for additional dependencies: cmake, ninja and nuget.
 @IF %toolchain%==msvc call "%devroot%\%projectname%\buildscript\modules\cmake.cmd"
 @IF %toolchain%==msvc call "%devroot%\%projectname%\buildscript\modules\ninja.cmd"
 @IF %toolchain%==msvc call "%devroot%\%projectname%\buildscript\modules\nuget.cmd"
+
+@rem If using MSVC do LLVM build.
+@IF %toolchain%==msvc call "%devroot%\%projectname%\buildscript\modules\llvm.cmd"
+
+@rem Offer option to build with clang
+@call "%devroot%\%projectname%\buildscript\modules\clang.cmd"
+
+@rem MSVC: Lookup/build a pkg-config implementation
 @IF %toolchain%==msvc call "%devroot%\%projectname%\buildscript\modules\pkg-config.cmd"
 
 @rem If using MSVC do CLonD3D12 build
@@ -87,8 +92,8 @@
 @rem Build zstd compressor
 @IF %toolchain%==msvc call "%devroot%\%projectname%\buildscript\modules\zstd.cmd"
 
-@rem If using MSVC do LLVM build.
-@IF %toolchain%==msvc call "%devroot%\%projectname%\buildscript\modules\llvm.cmd"
+@rem Build LLVM OpenCL stack
+@IF %toolchain%==msvc IF EXIST "%llvminstloc%\%abi%\lib\cmake\llvm\LLVMConfig.cmake" if defined llvmbuildconf call "%devroot%\%projectname%\buildscript\modules\llvmspv.cmd"
 @IF %toolchain%==msvc call "%devroot%\%projectname%\buildscript\modules\libclc.cmd"
 
 @rem If using MSYS2 Mingw-w64 select Vulkan SDK

buildscript/modules/llvm.cmd
@@ -136,8 +136,7 @@
 @IF %llvmsources% EQU 0 echo.
 @IF %llvmsources% EQU 1 IF %cmakestate% EQU 0 echo.
 @IF %llvmsources% EQU 1 IF %cmakestate% GTR 0 IF NOT EXIST "%llvminstloc%\%abi%\lib\" echo.
-@IF EXIST "%llvminstloc%\%abi%\lib\cmake\llvm\LLVMConfig.cmake" if /I "%cfgllvmbuild%"=="y" call "%devroot%\%projectname%\buildscript\modules\llvmspv.cmd"
 
 @rem Reset environment after LLVM build.
-@endlocal
+@endlocal&set llvmbuildconf=%buildconf%
 @cd "%devroot%\"

commit 02a9fbb50bdd06b1a353cb174c1ec4433318c7f3

llvmspirv build: fix potential cmake not found error regression from 13e8cc928c2f5f3f1e82521ff3c0b71418304d63

buildscript/modules/llvmspv.cmd
@@ -49,6 +49,9 @@
 @IF /I "%integratespvtools%"=="y" set PKG_CONFIG_PATH=%devroot:\=/%/spirv-tools/build/%abi%/lib/pkgconfig
 @IF /I NOT "%integratespvtools%"=="y" set PKG_CONFIG_PATH=
 
+@rem Load cmake into build environment.
+@if %cmakestate%==1 set PATH=%devroot%\cmake\bin\;%PATH%
+
 @set buildconf=%llvmbuildconf% -DLLVM_EXTERNAL_SPIRV_HEADERS_SOURCE_DIR="%devroot%\SPIRV-Headers" -DCMAKE_INSTALL_PREFIX="%llvminstloc%\spv-%abi%" -DCMAKE_PREFIX_PATH="%llvminstloc%\%abi%" -DLLVM_SPIRV_INCLUDE_TESTS=OFF
 @FOR /F tokens^=^1^,2^ eol^= %%a IN ('type "%llvminstloc%\%abi%\lib\cmake\llvm\LLVMConfig.cmake"') DO @IF "%%a"=="set(LLVM_PACKAGE_VERSION" FOR /F tokens^=^1^ delims^=^.^ eol^= %%c IN ("%%b") DO @IF %%c GEQ 17 set buildconf=%buildconf% -DCMAKE_POLICY_DEFAULT_CMP0091=NEW
 @set buildconf=%buildconf% "%devroot%\SPIRV-LLVM-Translator"